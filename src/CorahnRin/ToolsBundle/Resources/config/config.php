b8a430355d8adb24293c00c33a0e801ac01255c6
Alex Rock Ancelet <pierstoval@gmail.com>
2013-09-25 22:59:23 +0200

    Tools config, mise à jour

--- src/CorahnRin/ToolsBundle/Resources/config/config.php
+++ src/CorahnRin/ToolsBundle/Resources/config/config.php
@@ -11,9 +11,13 @@
 //date_default_timezone_set('Europe/Paris');
 //setlocale(LC_ALL, array('fr_FR', 'fr_FR.UTF-8', 'French_France', 'french'));
 
-define('CORAHNRIN_TOOLS', dirname(__FILE__));
+$dirtools = dirname(__FILE__);
+$corahn_rin_dir = str_replace('Resources'.DIRECTORY_SEPARATOR.'config', '', $dirtools);
+define('CORAHNRIN', true);
+//define('CORAHNRIN_TOOLS', CORAHNRIN.DS.'ToolsBundle');
+unset($dirtools, $corahn_rin_dir);
 
-$root = preg_replace('~(\\\\|/)vendor.*$~isUu', '', dirname(__FILE__));
+$root = preg_replace('~(\\\\|/)src.*$~isUu', '', dirname(__FILE__));
 define('ROOT', $root); //Chemin vers le dossier racine
 unset($root);
 define('DS', DIRECTORY_SEPARATOR); //Définition du séparateur dans le cas ou l'on est sur windows ou linux
@@ -27,13 +31,14 @@ define('DS', DIRECTORY_SEPARATOR); //Définition du séparateur dans le cas ou l
 //define('P_CSS',			WEBROOT.DS.'css');
 //define('P_JS',			WEBROOT.DS.'js');
 //define('CHAR_EXPORT',	WEBROOT.DS.'files'.DS.'characters_export');
-define('P_LOGS', ROOT.DS.'logs');
+define('P_LOGS', '..'.DS.'logs');
+if (!is_dir(P_LOGS)) { mkdir(P_LOGS, 0777); }
 ## Constante de stockage du fichier de temps d'exécutions
-define('P_EXECTIME_LOGFILE', P_LOGS.DS.'exectime'.DS.date('Y.m.d').'.log');
+//define('P_EXECTIME_LOGFILE', P_LOGS.DS.'exectime'.DS.date('Y.m.d').'.log');
 
 ## Constantes liées à tFPDF pour les fichiers externes
-define('P_FPDF_FONTPATH',			ROOT.DS.'vendor'.DS.'pierstools'.DS.'fpdf'.DS.'fonts');
-define('P_FPDF_SYSTEM_TTF_FONTS',	ROOT.DS.'vendor'.DS.'pierstools'.DS.'fpdf'.DS.'fonts');
+define('P_FPDF_FONTPATH',			__DIR__.'..'.DS.'fpdf'.DS.'fonts');
+define('P_FPDF_SYSTEM_TTF_FONTS',	__DIR__.'..'.DS.'fpdf'.DS.'fonts');
 
 ## On récupère le Host original pour conserver le port s'il est mentionné. Il sera ajouté à BASE_URL
 ## Utilisé surtout pour des serveurs locaux en 127.0.0.1:8888 ou 127.0.0.1:8080
@@ -71,7 +76,7 @@ define('P_LANG', isset($_SESSION['lang']) ? $_SESSION['lang'] : 'fr');
 define('P_JSON_ENCODE', JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT | JSON_NUMERIC_CHECK);
 
 ## Variable de la version issue du fichier XML
-$versions_xml = file_get_contents(ROOT.DS.'vendor'.DS.'corahnrin'.DS.'corahnrintools'.DS.'src'.DS.'versions.xml');
+$versions_xml = file_get_contents(__DIR__.DS.'versions.xml');
 $versions = new SimpleXMLElement($versions_xml);
 if (!$versions) { tr('Une erreur est survenue dans la récupération du fichier de versions');exit; }
 unset($versions_xml);
