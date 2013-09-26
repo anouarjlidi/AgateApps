d654393d6d909ef9f4e408e0b5c79d6290df865e
Alex Rock Ancelet <pierstoval@gmail.com>
2013-09-26 14:24:28 +0200

    Commentaires bundle Tools

--- src/CorahnRin/ToolsBundle/CorahnRinToolsBundle.php
+++ src/CorahnRin/ToolsBundle/CorahnRinToolsBundle.php
@@ -4,12 +4,16 @@ namespace CorahnRin\ToolsBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class CorahnRinToolsBundle extends Bundle
-{
-	
+class CorahnRinToolsBundle extends Bundle {
+
 	function __construct() {
+		//Si la constante est déjà présente, on ne la recrée pas
 		if (defined('CORAHNRIN') && CORAHNRIN === true) { return; }
+
+		//Chargement du fichier de config de CorahnRin dans config.php
 		require __DIR__.DIRECTORY_SEPARATOR.'Resources'.DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR.'config.php';
+
+		//Chargement des fonctions, vendors et outils.
 		$this->load_functions();
 		$this->load_vendors();
 		$this->load_tools();
@@ -19,8 +23,13 @@ class CorahnRinToolsBundle extends Bundle
 	 * Charge les fonctions du module Corahn-Rin
 	 */
 	public function load_functions() {
+		//Dossier des fonctions
 		$dir = __DIR__.DS.'Resources'.DS.'libs'.DS.'functions'.DS;
+
+		//Exclusion de certains fichiers
 		$excludes = array('Translate.php'=>1);
+
+		//On boucle sur les fichiers et on les charge
 		foreach (scandir($dir) as $v) {
 			if ($v !== '.' && $v !== '..' && preg_match('#\.php$#i', $v) && !isset($excludes[$v])) {
 				require $dir.$v;
@@ -32,7 +41,10 @@ class CorahnRinToolsBundle extends Bundle
 	 * Charge les librairies externes utilisées par le module Corahn-Rin
 	 */
 	public function load_vendors() {
+		//Dossier des vendors
 		$dir = __DIR__.DS.'Resources'.DS.'libs'.DS.'vendor'.DS;
+
+		//On boucle sur les fichiers et on les charge
 		foreach (scandir($dir) as $v) {
 			if ($v !== '.' && $v !== '..' && preg_match('#\.php$#i', $v)) {
 				require $dir.$v;
@@ -44,7 +56,10 @@ class CorahnRinToolsBundle extends Bundle
 	 * Charge les librairies externes utilisées par le module Corahn-Rin
 	 */
 	public function load_tools() {
+		//Dossier des outils
 		$dir = __DIR__.DS.'Resources'.DS.'libs'.DS.'CorahnRinTools'.DS;
+
+		//On boucle sur les fichiers et on les charge
 		foreach (scandir($dir) as $v) {
 			if ($v !== '.' && $v !== '..' && preg_match('#\.php$#i', $v)) {
 				require $dir.$v;
