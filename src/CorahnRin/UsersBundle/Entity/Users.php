ec64c1f8cfd8c72cbaa4c3ea8a05c3ea31dcc72b
Alex Rock Ancelet <pierstoval@gmail.com>
2013-10-03 21:54:57 +0200

    Entites Users légèrement adaptée

--- src/CorahnRin/UsersBundle/Entity/Users.php
+++ src/CorahnRin/UsersBundle/Entity/Users.php
@@ -12,11 +12,58 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\Entity
  */
 class Users extends BaseUser {
-	
+
+    /**
+     * @var integer
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Id
+     */
+    protected $id;
+
 	/**
      * @var \Doctrine\Common\Collections\Collection
      * @ORM\OneToMany(targetEntity="CorahnRin\CharactersBundle\Entity\Characters", mappedBy="user")
 	 */
 	protected $characters;
 
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->characters = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+    
+    /**
+     * Add characters
+     *
+     * @param \CorahnRin\CharactersBundle\Entity\Characters $characters
+     * @return Users
+     */
+    public function addCharacter(\CorahnRin\CharactersBundle\Entity\Characters $characters)
+    {
+        $this->characters[] = $characters;
+    
+        return $this;
+    }
+
+    /**
+     * Remove characters
+     *
+     * @param \CorahnRin\CharactersBundle\Entity\Characters $characters
+     */
+    public function removeCharacter(\CorahnRin\CharactersBundle\Entity\Characters $characters)
+    {
+        $this->characters->removeElement($characters);
+    }
+
+    /**
+     * Get characters
+     *
+     * @return \Doctrine\Common\Collections\Collection 
+     */
+    public function getCharacters()
+    {
+        return $this->characters;
+    }
 }
