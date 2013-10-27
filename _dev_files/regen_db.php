68b2fdc22520165170e1e90b936ce57c85b6ea7c
Alex Rock Ancelet <pierstoval@gmail.com>
2013-10-27 22:47:26 +0100

    Optimisation convertisseur CLI
    Changement de la mise à jour du cache

--- _dev_files/regen_db.php
+++ _dev_files/regen_db.php
@@ -2,8 +2,7 @@
 $time = microtime(true)*1000;
 $console_dir = '..'.DIRECTORY_SEPARATOR.'app'.DIRECTORY_SEPARATOR.'console';
 $cmd = array();
-$cmd[] = 'php '.$console_dir.' cache:clear';
-$cmd[] = 'php '.$console_dir.' cache:warmup';
+$cmd[] = 'php '.$console_dir.' cache:clear --no-warmup';
 $cmd[] = 'php '.$console_dir.' doctrine:database:drop --force';
 $cmd[] = 'php '.$console_dir.' doctrine:database:create';
 $cmd[] = 'php '.$console_dir.' doctrine:generate:entities CorahnRinCharactersBundle --no-backup';
@@ -11,6 +10,7 @@ $cmd[] = 'php '.$console_dir.' doctrine:generate:entities CorahnRinMapsBundle --
 //$cmd[] = 'php '.$console_dir.' doctrine:generate:entities CorahnRinPagesBundle --no-backup';
 $cmd[] = 'php '.$console_dir.' doctrine:generate:entities CorahnRinUsersBundle --no-backup';
 $cmd[] = 'php '.$console_dir.' doctrine:schema:update --force';
+$cmd[] = 'php '.$console_dir.' cache:warmup';
 
 foreach ($cmd as $c) {
 	echo "-----------------------------------------\n",
