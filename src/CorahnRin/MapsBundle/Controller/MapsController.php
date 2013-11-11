767c90f023266944989a6fc3b80bf32380d6b20d
Alex Rock Ancelet <pierstoval@gmail.com>
2013-11-11 07:59:17 +0100

    Ajout d'une vue pour la map
    En prévision des futures fonctionnalités en JS

--- src/CorahnRin/MapsBundle/Controller/MapsController.php
+++ src/CorahnRin/MapsBundle/Controller/MapsController.php
@@ -13,59 +13,13 @@ use CorahnRin\MapsBundle\Form\MapsType;
 
 class MapsController extends Controller
 {
-    
+
     /**
-     * @Route("/maps/tile/{id}/{x}/{y}", requirements={"id":"\d+","x":"\d+","y":"\d+"})
-     * @Template("CorahnRinMapsBundle:Maps:tile.json.twig")
+     * @Route("/maps/{id}-{nameSlug}", requirements={"id":"\d+"})
+     * @Template()
      */
-    public function tileAction($id, $x, $y){
-        $x = (int) $x;
-        $y = (int) $y;
-
-        //Chargement de la carte associée
-        $map = $this->getDoctrine()->getManager()->getRepository('CorahnRinMapsBundle:Maps')->findOneBy(array('id'=>$id));
-
-        $err = false;//Aucune erreur
-
-        //Récupération de la taille de l'image
-        $cmd = 'identify -format "%wx%h" "'.ROOT.'/web/'.$map->getImage().'"';
-        $size = shell_exec($cmd);
-        list($w, $h) = explode('x',$size);
-
-        $w = (int) $w;
-        $h = (int) $h;
-
-        $xmax = $w / 256;
-        $ymax = $h / 256;
-
-        if ((int)$xmax < $xmax) { $xmax = ((int) $xmax) + 1; }
-        if ((int)$ymax < $ymax) { $ymax = ((int) $ymax) + 1; }
-
-        while ($x > $xmax) { $x -= $xmax; }
-        while ($y > $ymax) { $y -= $ymax; }
-
-        while ($x < 0) { $x += $xmax; }
-        while ($y < 0) { $y += $ymax; }
-        
-        $imgname = ROOT.'/app/cache/maps_img/'.$map->getNameSlug().'_'.$x.'_'.$y.'.jpg';
-        
-        if (!file_exists($imgname)) {
-            if (!is_dir(dirname($imgname))) { mkdir(dirname($imgname), 0777, true); }
-            $x *= 256;
-            $y *= 256;
-            $cmd = 'convert '.
-                    '"'.ROOT.'/web/'.$map->getImage().
-                    '" -crop 256x256+'.$x.'+'.$y.
-                    ' -background black'.
-                    ' -extent 256x256^ '.
-                    '"'.$imgname.'"';
-            exec($cmd);
-        }
-
-        
-        $response = new Response(file_get_contents($imgname), 200, array('Content-type','image/jpeg'));
-        $response->headers->set('Content-type', 'image/jpeg');
-        return $response;
+    public function viewAction(Maps $map) {
+        return array('map'=>$map);
     }
     
     /**
@@ -98,7 +52,10 @@ class MapsController extends Controller
                 $basename = preg_replace('~\.[a-zA-Z0-9]+~isUu', '', $file->getClientOriginalName());
                 $basename = preg_replace('~[^a-zA-Z0-9]~isUu', '_', $basename);
                 $basename = preg_replace('~__+~', '_', $basename);
-                $dir = ROOT.DS.'web'.DS.'bundles'.DS.'corahnrinmaps'.DS.'img';
+                $dir = ROOT.DS.'web'.DS.'img'.DS.'maps';
+                if (!is_dir($dir)) {
+                    mkdir($dir, 0777, true);
+                }
                 $ext = $file->guessExtension();
                 $ext = $ext ? : 'bin';
                 
