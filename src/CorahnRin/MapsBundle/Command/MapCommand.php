549cc9b46693bfb73e17bd2187375679b1fb9e3d
Alex Rock Ancelet <pierstoval@gmail.com>
2013-11-11 08:02:58 +0100

    Création du générateur de tuiles
    La commande corahnrin:generate:map {id} permet de créer toutes les
    tuiles de la carte pour en faciliter le chargement.
    Pour une carte en 6000x5000 il faut environ 3h30 pour générer les
    tuiles, et environ 300Mo d'espace libre (en fonction de la carte)

--- src/CorahnRin/MapsBundle/Command/MapCommand.php
+++ src/CorahnRin/MapsBundle/Command/MapCommand.php
@@ -1,5 +1,5 @@
 <?php
-namespace CorahnRin\ToolsBundle\Command;
+namespace CorahnRin\MapsBundle\Command;
 
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputArgument;
@@ -7,223 +7,210 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class RepositoryCommand extends ContainerAwareCommand {
-
-	private $base_file = <<<'BASE_FILE'
-<?php
-namespace {namespace};
-use Doctrine\ORM\EntityRepository;
-use CorahnRin\ToolsBundle\Repository\CorahnRinRepository as CorahnRinRepository;
-
-/**
- * {name}Repository
- *
- */
-class {name}Repository extends CorahnRinRepository {
-
-}
-BASE_FILE
-;
+class MapCommand extends ContainerAwareCommand {
 
 	protected function configure() {
 
 		//public function addArgument($name, $mode = null, $description = '', $default = null)
 		$this
-		->setName('corahnrin:generate:repositories')
-		->setDescription('Generate entities repositories of a specific namespace.')
-		->addArgument(
-				'namespace',
-				InputArgument::OPTIONAL,
-				'Enter the namespace you want to scan (use "/" for compatibility).'
-			)
-		->addOption('no-backup', null, InputOption::VALUE_NONE, 'If defined, will not generate ".php~" backup files of existing entities and repositories.')
-		;
+		->setName('corahnrin:generate:map')
+		->setDescription('Generate all files for a specific map.')
+		->addArgument('id', InputArgument::OPTIONAL, 'Enter the id of the map you want to generate');
 	}
 
 	protected function execute(InputInterface $input, OutputInterface $output) {
+        $global_time = microtime(true);
 		//Récupération du service "dialog" pour les demandes à l'utilisateur
 		$dialog = $this->getHelperSet()->get('dialog');
+        
+        $repo = $this->getContainer()->get('doctrine')->getManager()->getRepository('CorahnRinMapsBundle:Maps');
 
-		//Dossiers de base et directory separator simplifié
-		$DS = DIRECTORY_SEPARATOR;
-		$global_dir = preg_replace('#(\\\|/)src(\\\|/)(.*)$#isUu', $DS, __DIR__);
+        $list = $repo->findAll();
+        
+        $maps_list = array();
+        foreach ($list as $v) {
+            $maps_list[$v->getId()] = $v->getName();
+        }
+        unset($list);
+        
+        $sleep = 50000;
+        
+		$output->writeln('Welcome to Corahn-Rin map generator !'); usleep($sleep);
+		$output->writeln('Be careful : as maps may be huge, this application can use a lot of memory and take very long to execute.');
+		$output->writeln('Be sure not to stop the process, as it starts by zero on each execution.');usleep($sleep);
+		$output->writeln('');
+        
+        $map = null;
+		$id = (int) $input->getArgument('id');
+        do {
+            $map = $repo->findOneBy(array('id'=>$id));
+            if (!$map) {
+                $output->writeln('No map with this id.');
+                $id = $dialog->select(
+                    $output,
+                    'Select a map to generate :',
+                    $maps_list,
+                    false,
+                    false,
+                    'No id "%s" in maps list.'
+                );
+            }
+        } while (!$map);
+        
+		$output->writeln('Generating map "'.$map->getName().'"');
 
-		//Demande à l'utilisateur de récupérer le namespace
-		$dir = $input->getArgument('namespace');
-		if (!$dir) {
-			$dir = $dialog->askAndValidate(
-				$output,
-				'Enter the namespace you want to scan (use "/" for compatibility) : '."\n > ",
-				function ($answer) {
-					$DS = DIRECTORY_SEPARATOR;
-					$global_dir = preg_replace('#(\\\|/)src(\\\|/)(.*)$#isUu', $DS, __DIR__);
-					$dir = trim($answer);
-					$dir = trim($dir, '/\\');
-					$dir = str_replace(array('/', '\\'), array('/', '/'), $dir);
-					$dir = preg_replace('~/?Entity/?$~isUu', '', $dir);
-					$dir = str_replace(array('/', '\\'), array($DS, $DS), $dir);
-					$namespace = $dir;
-					$dir = trim($dir, $DS);
-					$scanned_dir = $global_dir.'src'.$DS.$dir.$DS.'Entity';
-					if (!$dir) {
-						throw new \RunTimeException('Please enter a correct namespace.');
-					} elseif (!is_dir($scanned_dir)) {
-						throw new \RunTimeException(
-							'This namespace does not exist or is not located into the "'.'src'.$DS.'" folder.'.
-							PHP_EOL.'Folder calculated : '.$scanned_dir
-						);
-					}
-					return array('dir'=>$dir, 'namespace'=>$namespace);
-				},
-				false //nombre de tentatives
-			);
-		}
+        $cmd = 'identify -format "%wx%h" "'.ROOT.'/web/'.$map->getImage().'"';
+        $size = shell_exec($cmd);
+        if (!$size || !preg_match('#^[0-9]+x[0-9]+$#', $size)) {
+            throw new \RunTimeException('Error while retrieving map dimensions.');
+        }
+        list($w, $h) = explode('x',$size);
 
-		//Retraitement des variables
-		$namespace = $dir['namespace'];
-		$dir = $dir['dir'];
-		$namespace = trim($namespace);
-		$namespace = trim($namespace, '/\\');
-		$namespace = str_replace('/', '\\', $namespace);
-		$namespace .= '\\Entity';
-		$repo_dir = $global_dir.'src'.$DS.$dir.$DS.'Repository';
-		$dir = $global_dir.'src'.$DS.$dir.$DS.'Entity';
+        $img_size = (int) $this->getContainer()->getParameter('corahn_rin_maps.tile_size');;
 
-		//Affichage des données récupérées
-		$output->writeln(PHP_EOL.'Namespace : '.$namespace);
-		$output->writeln('Scanning directory : '.$dir);
-
-		//Scan des fichiers
-		$files = scandir($dir);
-		foreach ($files as $k => $v) {
-			unset($files[$k]);
-			if ($v !== '.' && $v !== '..' && preg_match('#\.php$#isUu', $v) && strpos($v, 'Repository') === false) {
-				$valid_files[] = $v;
-			}
-		}
-		$files = $valid_files;
-		$files[''] = '> Type "enter" to skip this test';
-
-		if (!count($files) || count($files) === 1) {
-			throw new \RunTimeException('The folder is empty of any php file...');
-		}
-
-		//Affichage du contenu valide du dossier
-		$selected = $dialog->select(
-			$output,
-			PHP_EOL.'If you want to ignore files you can specify them here. Separate the indexes with ",".'.PHP_EOL.'Folder contents :',
-			$files,
-			'0',
-			false,
-			'Entry "%s" does not exist',
-			true // active l'option multiselect
-		);
-
-		$selected[] = '';
-
-		foreach ($selected as $v) { unset($files[$v]); }
-
-		//Affichage du contenu valide du dossier
-		$output->writeln('Files scanned :');
-		foreach ($files as $v) { $output->writeln(' > '.str_replace('.php', '', $v)); }
-
-		//Confirmation d'exécution
-		if (!$dialog->askConfirmation($output, 'Proceed ? [yes] ', 'yes')) {
-			$output->writeln('Aborted...');
-			return;
-		}
-
-		$output->writeln('The application will create a repository for each file in the following directory :');
-		$output->writeln(' > '.$repo_dir);
-
-
-
-		$files_count = 0;
-		$files_overwritten = 0;
+        $files_written = 0;
 		$overwrite_all = 0;
-		foreach ($files as $v) {
-			$name = str_replace('.php', '', $v);
-			$dest_file = $repo_dir.$DS.$name.'Repository.php';
-			$overwrite = false;
-			$exists = file_exists($dest_file);
-			if ($exists) {
-				if ($overwrite_all === 0) {
-					$choices = array('yes','all','no','no-all','y','n','nall');
-					$answer = $dialog->select(
-						$output,
-						'Following file already exists : '.PHP_EOL.'> '. str_replace($global_dir.$DS, '', $dest_file). PHP_EOL. 'Overwrite ? [yes]',
-						$choices,
-						0,
-						false,
-						'Answer "%s" is not correct'
-					);
+       
+        $total_files = 0;
+        $current_file = 0;
+        
+        for ($zoom = 1; $zoom <= $map->getMaxZoom(); $zoom++) {
+            $ratio = $zoom / $map->getMaxZoom();
+            $_w = (int) $w * $ratio;
+            $_h = (int) $h * $ratio;
+            $xmax = $_w / $img_size;
+            $ymax = $_h / $img_size;
+            if ((int)$xmax < $xmax) { $xmax = ((int) $xmax) + 1; }
+            if ((int)$ymax < $ymax) { $ymax = ((int) $ymax) + 1; }
+            $total_files += ($xmax)*($ymax);
+        }
 
-					$answer = $choices[$answer];
-					$selected =	$answer === 'y' ? 'yes' :
-								($answer === 'n' ? 'no' :
-								($answer === 'nall' ? 'no-all' : $answer));
-					if ($selected === 'yes') {
-						$overwrite = true;
-					} elseif ($selected === 'all') {
-						$overwrite = true;
-						$overwrite_all = 1;
-					} elseif ($selected === 'no') {
-						$overwrite = false;
-					} elseif ($selected === 'no-all') {
-						$overwrite = false;
-						$overwrite_all = 2;
-					}
-				} elseif ($overwrite_all === 1) {
-					$overwrite = true;
-				} elseif ($overwrite_all === 2) {
-					$overwrite = false;
-				}
-			} else {
-				$overwrite = true;
-			}
-			$content = $this->base_file;
-			$content = str_replace('{name}', $name, $content);
-			$content = str_replace('{namespace}', str_replace('\\Entity', '\\Repository', $namespace), $content);
-			if ($overwrite) {
-				$files_count++;
-				if ($exists) {
-					$files_overwritten ++;
-					$bu = $input->getOption('no-backup');
-					if (!$bu) {
-						file_put_contents($dest_file.'~', file_get_contents($dest_file));
-					}
-				}
-				if (!is_dir($repo_dir)) { mkdir($repo_dir, 0777, true); }
-				$output->writeln(($exists ? 'Overwriting' : 'Writing into'). ' file "'. $dest_file. '". '. file_put_contents($dest_file, $content). ' octets written.');
-			} else {
-				$output->writeln('Skipping file "'. $dest_file. '"...');
-			}
-		}//end foreach
+        $times = array();
 
-		if ($dialog->askConfirmation(
-			$output,
-			'Add the parameter "ORM\\Entity(repositoryClass)" ? [y]es, [n]o (default [yes]) : ',
-			true)) {
-			foreach ($files as $file) {
-				$repositoryClass = str_replace('\\Entity', '', $namespace).'\\Repository\\'.str_replace('.php', '', $file).'Repository';
-				$file = $dir.$DS.$file;
-				$cnt = file_get_contents($file);
-				$cnt = preg_replace('~@ORM.Entity(\([^\)]*\))?(\r)?\n~Uu', '@ORM\\Entity(repositoryClass="'.$repositoryClass.'")'."\n", $cnt);
-				// echo substr($cnt, 0, 450), PHP_EOL, PHP_EOL, PHP_EOL;
-				// $handle = fopen ("php://stdin","r");
-				// $proceed = fgets($handle);
-				// fclose($handle);
-				$bu = $input->getOption('no-backup');
-				if (!$bu) {
-					file_put_contents($file.'~', file_get_contents($file));
-				}
-				file_put_contents($file, $cnt);
-				$output->writeln('Added parameter "repositoryClass" to file "'.$file.'"');
-			}
-		}
+        for ($zoom = 1; $zoom <= $map->getMaxZoom(); $zoom++) {
+            $ratio = $zoom / $map->getMaxZoom();
+            $_w = (int) $w * $ratio;
+            $_h = (int) $h * $ratio;
+
+            //Calcul du nombre maximum de vignettes
+            $xmax = $_w / $img_size;
+            $ymax = $_h / $img_size;
+
+            if ((int)$xmax < $xmax) { $xmax = ((int) $xmax) + 1; }
+            if ((int)$ymax < $ymax) { $ymax = ((int) $ymax) + 1; }
+
+            $wmax = $xmax * $img_size;
+            $hmax = $ymax * $img_size;
+
+//            $total_files = ($xmax+1)*($ymax+1);
+            for ($x = 0; $x < $xmax; $x++) {
+                for ($y = 0; $y < $ymax; $y++) {
+                    $time = microtime(true);
+                    $current_file++;
+                    //Génération du nom de la tuile finale (dans le cache)
+                    $imgname = ROOT.'/app/cache/maps_img/'.$map->getNameSlug().'_'.$zoom.'_'.$x.'_'.$y.'.jpg';
+                    if (!is_dir(dirname($imgname))) { mkdir(dirname($imgname), 0777, true); }
+        			$overwrite = false;
+//                    $overwrite_all;
+        			$exists = file_exists($imgname);
+        			if ($exists) {
+        				if ($overwrite_all === 0) {
+        					$choices = array('yes','all','no','no-all','y','n','nall');
+        					$answer = $dialog->select(
+        						$output,
+        						'Following file already exists : '."\n".'> '. str_replace(ROOT, '', $imgname). "\n". 'Overwrite ? [yes]',
+        						$choices,
+        						0,
+        						false,
+        						'Answer "%s" is not correct'
+        					);
+        
+        					$answer = $choices[$answer];
+        					$selected =	$answer === 'y' ? 'yes' :
+        								($answer === 'n' ? 'no' :
+        								($answer === 'nall' ? 'no-all' : $answer));
+        					if ($selected === 'yes') {
+        						$overwrite = true;
+        					} elseif ($selected === 'all') {
+        						$overwrite = true;
+        						$overwrite_all = 1;
+        					} elseif ($selected === 'no') {
+        						$overwrite = false;
+        					} elseif ($selected === 'no-all') {
+        						$overwrite = false;
+        						$overwrite_all = 2;
+        					}
+        				} elseif ($overwrite_all === 1) {
+        					$overwrite = true;
+        				} elseif ($overwrite_all === 2) {
+        					$overwrite = false;
+        				}
+        			} else {
+        				$overwrite = true;
+        			}
+                    
+                    $_x = $x*$img_size;
+                    $_y = $y*$img_size;
+                    if ($overwrite) {
+                        $files_written++;
+                        //Génération de l'offset à partir des X et Y demandés
+
+                        //Commande ImageMagick
+                        $cmd = 'convert'.
+                                "\t".' "'.ROOT.'/web/'.$map->getImage().'"'.
+                                "\t".($ratio < 1 ? ' -resize '.($ratio*100) .'%' : '').
+                                "\t".' -background black'.//Le "surplus" sera noirs
+                                "\t".' -extent '.$wmax.'x'.$hmax.'^'.//Redimensionne aux valeurs "width" et "height" maximales dépendant du zoom
+                                "\t".' -crop '.$img_size.'x'.$img_size.'+'.$_x.'+'.$_y.//Découpe l'image selon la taille demandée dans les paramètres
+                                "\t".' -extent '.$img_size.'x'.$img_size.'^'.//Et étend les éventuels pixels en trop ou en moins
+                                "\t".' "'.$imgname.'"';
+
+//                        $output->writeln('Executing command with values :');
+//                        $output->writeln(' zoom='.$zoom);
+//                        $output->writeln(' x='.$x);
+//                        $output->writeln(' y='.$y);
+//                        $output->writeln(' ratio='.$ratio);
+//                        $output->writeln(" ".str_replace(ROOT.DS,'',$cmd));
+            //            \CorahnRinTools\pr($cmd);exit;
+//                        if (substr(php_uname(), 0, 7) == "Windows"){ 
+//                            pclose(popen("start /B ". $cmd, "r"));  
+//                        } 
+//                        else { 
+//                            shell_exec($cmd . " > /dev/null &");   
+//                        }
+                        shell_exec($cmd);
+                    }
+
+                    $p = ($current_file * 100 / $total_files);
+                    $p = number_format($p, 2, '.', '');
+                    $str = 0;
+                    $str = '[';
+                    $p2 = (int)($p/2);
+                    for ($i = 0; $i <= 50; $i++) {
+                        $str .= $p2 < $i ? ' ' : ($p2 === $i ? '>' : '=');
+                    }
+                    $str .= ']';
+                    $time = microtime(true) - $time;
+                    if ($overwrite) {
+                        $times[] = $time;
+                    }
+                    if (count($times)) {
+                        $median = array_sum($times) / count($times);
+                    } else {
+                        $median = 60*60*24*365;
+                    }
+                    $time_remaining = gmdate("H:i:s", $median * ($total_files - $current_file));
+//                    print_r(array('sum'=>array_sum($times),'count'=>count($times),'median'=>$median));
+                    $output->write(' '.$str." ".$p.'%  File '.$current_file.'/'.$total_files.'  x='.$x.' y='.$y.' z='.$zoom.'  Remaining : '.$time_remaining.' (estimation)'." \r");
+                    
+                }
+            }
+        }
 		
 		$output->writeln('End of function !');
-		$output->writeln('Thanks for using CorahnRinTools, and see you soon !');
+        $output->writeln($files_written.' files have been written !');
+        $output->writeln('Execution time : '.gmdate('H:i:s', microtime(true) - $global_time));
+		$output->writeln('Thanks for using CorahnRinMaps, and see you soon !');
 		
 	}
 }
