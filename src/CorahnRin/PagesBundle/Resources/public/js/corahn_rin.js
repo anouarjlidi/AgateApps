ebfdaff08f005b811d12a1051480a4d3e989c231
Alex Rock Ancelet <pierstoval@gmail.com>
2013-11-18 08:03:14 +0100

    Création d'un js "corahn_rin"

---
+++ src/CorahnRin/PagesBundle/Resources/public/js/corahn_rin.js
@@ -0,0 +1,10 @@
+document.addEvent = function(elem, type, eventHandle) {
+    if (elem == null || elem == undefined) return;
+    if ( elem.addEventListener ) {
+        elem.addEventListener( type, eventHandle, false );
+    } else if ( elem.attachEvent ) {
+        elem.attachEvent( "on" + type, eventHandle );
+    } else {
+        elem["on"+type]=eventHandle;
+    }
+};
