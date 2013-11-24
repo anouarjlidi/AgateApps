84e6336ec220677ce5acf051041104f26a6a8e38
Alex Rock Ancelet <pierstoval@gmail.com>
2013-11-24 15:28:08 +0100

    Changements dans le Menu pour les langues
    Ajout des langues contenues dans la classe translator dans le menu, afin
    qu'elles soient extraites directement depuis le traducteur lui-même.

--- src/CorahnRin/PagesBundle/Controller/MenusController.php
+++ src/CorahnRin/PagesBundle/Controller/MenusController.php
@@ -16,8 +16,21 @@ class MenusController extends Controller {
 			'corahnrin_characters_viewer_list' => 'Liste des personnages',
 			'corahnrin_pages_versions_index' => 'Mises à jour',
 		);
+        $langs = $this->get('corahnrin_translate')->getLangs('array');
+        foreach($langs as $k => $lang) {
+            $lang = strtolower($lang);
+            $langs[$k] = array('locale' => $lang, 'literal' => $lang);
+                if ($lang === 'fr') { $langs[$k]['literal'] = 'Français'; }
+            elseif ($lang === 'en') { $langs[$k]['literal'] = 'Anglais'; }
+            elseif ($lang === 'de') { $langs[$k]['literal'] = 'Allemand'; }
+        }
 		$username = $this->get('fos_user.user_provider.username');
-		return array('links' => $links, 'route_name' => $route,'locale'=>strtoupper($this->get('session')->get('_locale')));
+		return array(
+            'links' => $links,
+            'route_name' => $route,
+            'locale'=>$this->get('session')->get('_locale'),
+            'langs' => $langs
+        );
 	}
 
 	/**
