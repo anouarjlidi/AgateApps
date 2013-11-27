508732e5149db7b16e9026782e855dd21446a1e8
Alex Rock Ancelet <pierstoval@gmail.com>
2013-11-27 16:42:17 +0100

    CorahnRinRepository override findAll()
    La méthode `findAll()` fait la même chose que son parent, mais si on met
    l'attribut `$sortCollection` à `true`, la liste des éléments sera triée
    en fonction de l'id de l'entité récupérée.

--- src/CorahnRin/ToolsBundle/Repository/CorahnRinRepository.php
+++ src/CorahnRin/ToolsBundle/Repository/CorahnRinRepository.php
@@ -9,4 +9,18 @@ use Doctrine\ORM\EntityRepository;
  */
 abstract class CorahnRinRepository extends EntityRepository {
 
+    public function findAll($sortCollection = false) {
+        $datas = parent::findAll();
+        $total = array();
+        if ($datas) {
+            if ($sortCollection === false) {
+                $total = $datas;
+            } else {
+                foreach ($datas as $entity) {
+                    $total[$entity->getId()] = $entity;
+                }
+            }
+        }
+        return $total;
+    }
 }
