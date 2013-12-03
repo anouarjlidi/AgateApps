6060911583dde50c1be2432786bab0c9b4c5baa2
Alex Rock Ancelet <pierstoval@gmail.com>
2013-12-03 20:45:07 +0100

    Préparation étape 2 générateur utilisant Maps

--- src/CorahnRin/CharactersBundle/Steps/_step_03_naissance.php
+++ src/CorahnRin/CharactersBundle/Steps/_step_03_naissance.php
@@ -1,3 +1,31 @@
 <?php
+/**
+ * Lieu de naissance
+ */
 
-exit('ok');
+$regions = $controller->getDoctrine()->getManager()->getRepository('CorahnRinCharactersBundle:Regions')->findAll(true);
+
+$datas['regions_list'] = $regions;
+$datas['region_value'] = '';
+
+$character_region = $session->get('character.region');
+
+if ($character_region) {
+    $datas['region_value'] = $character_region;
+}
+
+if ($request->isMethod('POST')) {
+    $region_value = $request->request->get('region_value');
+    if (isset($regions[$region_value])) {
+        $session->set('character.region', $region_value);
+        return $controller->_nextStep($session, $request);
+    } else {
+        $translator = $controller->get('corahn_rin_translate');
+        $translator->routeTemplate('corahnrin_characters_generator_step');
+        $msg = $translator->translate('Une erreur est survenue : mauvais contenu envoyé au personnage');
+        $translator->routeTemplate();
+        $session->getFlashBag()->add('error', $msg);
+    }
+
+}
+return $datas;
