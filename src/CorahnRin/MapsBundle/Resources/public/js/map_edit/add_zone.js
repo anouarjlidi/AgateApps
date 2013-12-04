c46b39e115c46114a77ffe2a501f197134671748
Alex Rock Ancelet <pierstoval@gmail.com>
2013-12-04 23:35:04 +0100

    Maps:addZone fix JS et position/offset d'un polygone

--- src/CorahnRin/MapsBundle/Resources/public/js/map_edit/add_zone.js
+++ src/CorahnRin/MapsBundle/Resources/public/js/map_edit/add_zone.js
@@ -13,7 +13,7 @@ document.getElementById('map_add_zone').onclick = function(){
     }
     this.setAttribute('data-active', attr);
 
-    
+
     if (attr === 'true') {
         document.addZoneCoordinates = [];
         document.addZoneId = document.addZoneId ? document.addZoneId + 1 : 0;
@@ -30,21 +30,31 @@ document.getElementById('map_add_zone').onclick = function(){
         $(container)
         .mousedown(function(e){
             if (this.getAttribute('data-add-zone') === 'true') {
-                this.base_coords = e.offsetX+','+e.offsetY;
+                this.base_coords = e.clientX + ',' + e.clientY;
             }
         })
         .mouseup(function(e){
             if (this.getAttribute('data-add-zone') === 'true') {
-                var coords = e.offsetX+','+e.offsetY;
-                if (coords == this.base_coords) {
-                    document.addZoneCoordinates.push(e.offsetX+','+e.offsetY);
+                var x = typeof e.offsetX !== 'undefined'
+                    ? e.offsetX
+                    : (typeof e.layerX !== 'undefined' ? e.layerX : e.clientX - $(e.target).offset().left + window.pageXOffset );
+                var y = typeof e.offsetY !== 'undefined'
+                    ? e.offsetY
+                    : (typeof e.layerY !== 'undefined' ? e.layerY : e.clientY - $(e.target).offset().top + window.pageYOffset );
+                x = parseInt(x);
+                y = parseInt(y);
+                var coords = x + ',' + y;
+                var base_coords = e.clientX+','+e.clientY;
+                if (base_coords === this.base_coords) {
+                    console.info(coords);
+                    document.addZoneCoordinates.push(x+','+y);
                     document.getElementById(document.addZoneNodeId).setAttribute('points', document.addZoneCoordinates.join(' '));
                 }
             }
         });
     }
-    
+
     container.setAttribute('data-add-zone', attr);
-    
+
     return false;
 };
