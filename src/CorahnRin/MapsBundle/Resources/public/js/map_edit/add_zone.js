93a8262dfde7645152ce69cd355256a4629de6da
Alex Rock Ancelet <pierstoval@gmail.com>
2013-12-06 16:11:53 +0100

    add_zone.js fix coordonnées sur firefox, WIP mousemove

--- src/CorahnRin/MapsBundle/Resources/public/js/map_edit/add_zone.js
+++ src/CorahnRin/MapsBundle/Resources/public/js/map_edit/add_zone.js
@@ -15,64 +15,97 @@ document.getElementById('map_dont_move').onclick = function(){
 document.getElementById('map_add_zone').onclick = function(){
     var _this = this,
         polygon,
-        attr = this.getAttribute('data-active'),
-        container = document.getElementById(this.getAttribute('data-map-container') ? this.getAttribute('data-map-container') : 'map_container'),
-        zonesContainer = document.getElementById(this.getAttribute('data-zones-container') ? this.getAttribute('data-zones-container') : 'map_zones');
+        i,l,c,
+        coordinates = [],
+        SVGcontainer = document.getElementById(this.getAttribute('data-zones-container') ? this.getAttribute('data-zones-container') : 'map_zones'),
+        mapContainer = document.getElementById(this.getAttribute('data-map-container') ? this.getAttribute('data-map-container') : 'map_container'),
+        polygonId = document.addZonePolygonId ? document.addZonePolygonId : 0,
+        polygonIdFull = document.addZonePolygonIdFull ? document.addZonePolygonIdFull : '',
+        attr = this.getAttribute('data-active');
+
     if (attr === 'true') {
+        if (document.addZoneCoordinates.length && document.addZonePolygon) {
+            document.addZonePolygon.setAttribute('points', document.addZoneCoordinates.join(' '));
+        }
+        document.addZoneCoordinates = [];
+        document.addZonePolygon = null;
+        document.addZoneMapContainerOffset = null;
         attr = 'false';
         this.parentNode.classList.remove('active');
+        return;
     } else {
         attr = 'true';
         this.parentNode.classList.add('active');
     }
     this.setAttribute('data-active', attr);
 
-    document.addZoneContainer = container;
-
     if (attr === 'true') {
-        document.addZoneCoordinates = [];
-        document.addZoneId = document.addZoneId ? document.addZoneId + 1 : 0;
-        while (document.getElementById("map_add_zone_polygon_"+document.addZoneId)) {
-            document.addZoneId ++;
+        while (document.getElementById("map_add_zone_polygon_"+polygonId)) {
+            polygonId ++;
         }
-        document.addZoneNodeId = "map_add_zone_polygon_"+document.addZoneId;
+        polygonIdFull = "map_add_zone_polygon_"+polygonId;
         polygon = document.createElementNS('http://www.w3.org/2000/svg', 'polygon');
-        polygon.id = document.addZoneNodeId;
-        zonesContainer.appendChild(polygon);
-        $(zonesContainer).height($(container).height());
+        polygon.id = polygonIdFull;
+        SVGcontainer.appendChild(polygon);
+        $(SVGcontainer).height($(mapContainer).height());
     } else {
-        document.addZoneContext.stroke();
-        document.addZoneContext.closePath();
+        l = document.querySelectorAll('polygon:not([points])');
+        c = l.length;
+        for (i = 0; i < c; i++) {
+            l[i].parentNode.removeChild(l[i]);
+        }
     }
 
-    if (!container.getAttribute('data-add-zone')){
-        document.addZoneContainer.onmousedown = function(e){
-            if (document.addZoneContainer.getAttribute('data-add-zone') === 'true') {
-                document.addZoneContainer.base_coords = e.clientX + ',' + e.clientY;
+    document.addZoneCoordinates = coordinates;
+    document.addZonePolygon = polygon;
+    document.addZoneMapContainerOffset = $(mapContainer).offset();
+
+    if (!mapContainer.getAttribute('data-add-zone')){
+        SVGcontainer.onmousedown = function(e){
+            if (mapContainer.getAttribute('data-add-zone') === 'true') {
+                mapContainer.base_coords = e.clientX + ',' + e.clientY;
             }
         };
-        document.addZoneContainer.onmouseup = function(e){
-            if (document.addZoneContainer.getAttribute('data-add-zone') === 'true') {
-                var x = typeof e.offsetX !== 'undefined'
-                    ? e.offsetX
-                    : (typeof e.layerX !== 'undefined' ? e.layerX : e.clientX - $(document.addZoneContainer).offset().left + window.pageXOffset );
-                var y = typeof e.offsetY !== 'undefined'
-                    ? e.offsetY
-                    : (typeof e.layerY !== 'undefined' ? e.layerY : e.clientY - $(document.addZoneContainer).offset().top + window.pageYOffset );
-                x = parseInt(x);
-                y = parseInt(y);
-                console.info(e);
-                console.info(x,y);
+        mapContainer.onmousemove = function(e){
+            if (mapContainer.getAttribute('data-add-zone') === 'true') {
+                if (mapContainer.getAttribute('data-add-zone') === 'true' && document.addZoneCoordinates.length) {
+                    var x = typeof e.offsetX !== 'undefined'
+                        ? e.offsetX
+                        : e.clientX - document.addZoneMapContainerOffset.left + window.pageXOffset ;
+                    var y = typeof e.offsetY !== 'undefined'
+                        ? e.offsetY
+                        : e.clientY - document.addZoneMapContainerOffset.top + window.pageYOffset;
+                    x = parseInt(x);
+                    y = parseInt(y);
+                    var coordinatesTemp = document.addZoneCoordinates.concat([x+','+y]);
+                    document.addZonePolygon.setAttribute('points', coordinatesTemp.join(' '));
+                }
+            }
+        };
+        SVGcontainer.onmouseup = function(e){
+            if (mapContainer.getAttribute('data-add-zone') === 'true') {
                 var base_coords = e.clientX+','+e.clientY;
-                if (base_coords === document.addZoneContainer.base_coords) {
+                if (base_coords === mapContainer.base_coords) {
+                    var x = typeof e.offsetX !== 'undefined'
+                        ? e.offsetX
+                        : /*(typeof e.layerX !== 'undefined' ? e.layerX :*/ e.clientX - document.addZoneMapContainerOffset.left + window.pageXOffset /*)*/;
+                    var y = typeof e.offsetY !== 'undefined'
+                        ? e.offsetY
+                        : /*(typeof e.layerY !== 'undefined' ? e.layerY :*/ e.clientY - document.addZoneMapContainerOffset.top + window.pageYOffset /*)*/;
+                    x = parseInt(x);
+                    y = parseInt(y);
+                    if (isNaN(x) || isNaN(y)) {
+                        console.error('Error with points '+(isNaN(x) ? "x" : "y")+"\n event : ", e);
+                        return false;
+                    }
                     document.addZoneCoordinates.push(x+','+y);
-                    document.getElementById(document.addZoneNodeId).setAttribute('points', document.addZoneCoordinates.join(' '));
+                    document.addZonePolygon.setAttribute('points', document.addZoneCoordinates.join(' '));
                 }
             }
         };
     }
 
-    document.addZoneContainer.setAttribute('data-add-zone', attr);
+    mapContainer.setAttribute('data-add-zone', attr);
 
     return false;
 };
