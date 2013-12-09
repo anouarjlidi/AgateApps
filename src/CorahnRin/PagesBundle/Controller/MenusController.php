10098463f21d7568acfcacef9af5b5ec056e37f1
Alex Rock Ancelet <pierstoval@gmail.com>
2013-12-09 18:13:26 +0100

    Refonte du système de menus
    Désormais, on peut avoir plusieurs menus. Chacun est déterminé par un
    tableau de valeurs, chaque valeur pouvant être une chaîne de caractères
    (on a donc une combinaison route>titre), ou un tableau. Si c'est un
    tableau, il faudra mentionner la clé initiale comme "dropdown_list_x" où
    "x" est un nombre (pour permettre de multiples dropdowns), avec un
    attribut "name" pour le titre du dropdown, et "list" pour la liste. La
    liste devra contenir un attribut "name" pour le titre du lien, un
    attribut "route" pour le chemin à récupérer dans le routeur, et
    éventuellement un tableau "attributes" pour les éventuelles variables de
    route.
    Une liste de liens pour le menu a été faite pour CorahnRin et une autre
    pour EsterenMaps (qui récupère la liste des maps dans le dropdown).
    Ce système va peut-être changer, ou du moins s'optimiser.

--- src/CorahnRin/PagesBundle/Controller/MenusController.php
+++ src/CorahnRin/PagesBundle/Controller/MenusController.php
@@ -11,11 +11,16 @@ class MenusController extends Controller {
 	 * @Template()
 	 */
 	public function menuAction($route = '') {
-		$links = array(
-			'corahnrin_characters_generator_index' => 'Générateur de personnage',
-			'corahnrin_characters_viewer_list' => 'Liste des personnages',
-			'corahnrin_pages_versions_index' => 'Mises à jour',
-		);
+        if (preg_match('#^corahnrin_maps#isUu', $route)) {
+            $links_func = 'EsterenMaps';
+            $brandTitle = 'Esteren Maps';
+            $brandRoute = 'corahnrin_maps_maps_index';
+        } else {
+            $links_func = 'CorahnRin';
+            $brandTitle = 'Corahn-Rin';
+            $brandRoute = 'corahnrin_pages_pages_index';
+        }
+        $links = $this->{'menu'.$links_func}();
         $langs = $this->get('corahnrin_translate')->getLangs('array');
         foreach($langs as $k => $lang) {
             $lang = strtolower($lang);
@@ -27,6 +32,8 @@ class MenusController extends Controller {
 		$username = $this->get('fos_user.user_provider.username');
 		return array(
             'links' => $links,
+            'brandTitle' => $brandTitle,
+            'brandRoute' => $brandRoute,
             'route_name' => $route,
             'locale'=>$this->get('session')->get('_locale'),
             'langs' => $langs
@@ -40,4 +47,34 @@ class MenusController extends Controller {
 		return new Response('menu 1');
 	}
 
+    protected function menuCorahnRin(){
+		return array(
+			'corahnrin_characters_generator_index' => 'Générateur de personnage',
+			'corahnrin_characters_viewer_list' => 'Liste des personnages',
+			'corahnrin_maps_maps_index' => 'Esteren Maps',
+		);
+    }
+    
+    protected function menuEsterenMaps(){
+        $maps = $this->getDoctrine()->getManager()->getRepository('CorahnRinMapsBundle:Maps')->findAll(true);
+        $maps_list = array();
+        foreach ($maps as $id => $map) {
+            $maps_list[$id] = array(
+                'route' => 'corahnrin_maps_maps_view',
+                'name' => $map->getName(),
+                'attributes' => array(
+                    'id' => $map->getId(),
+                    'nameSlug' => $map->getNameSlug(),
+                ),
+            );
+        }
+		$links = array(
+			'dropdown_list_1' => array(
+                'name' => 'Voir une carte',
+                'list' => $maps_list,
+            ),
+			'corahnrin_pages_pages_index' => 'Corahn-Rin',
+		);
+        return $links;
+    }
 }
