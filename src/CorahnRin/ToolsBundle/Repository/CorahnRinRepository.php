2feeaee00d6efe1d9b381efeb4338015515b1637
Alex Rock Ancelet <pierstoval@gmail.com>
2013-12-10 12:03:00 +0100

    CorahnRinRepository : ajout de la méthode getMax
    Cette méthode récupère la valeur de l'AUTO_INCREMENT pour la table
    demandée

--- src/CorahnRin/ToolsBundle/Repository/CorahnRinRepository.php
+++ src/CorahnRin/ToolsBundle/Repository/CorahnRinRepository.php
@@ -2,6 +2,7 @@
 
 namespace CorahnRin\ToolsBundle\Repository;
 use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\Query\ResultSetMapping;
 
 /**
  * CorahnRinRepository
@@ -22,5 +23,22 @@ abstract class CorahnRinRepository extends EntityRepository {
             }
         }
         return $total;
+    }
+    
+    /**
+     * Récupère la valeur de l'AUTO_INCREMENT de la table de l'entité
+     * @return int 
+     */
+    public function getMax() {
+        $table = $this->getClassMetadata()->getTableName();
+        
+        $connection = $this->getEntityManager()->getConnection();
+        $statement = $connection->prepare('SHOW TABLE STATUS LIKE "'.$table.'" ');
+        $statement->execute();
+        $datas = $statement->fetch();
+        
+        $max = (int) $datas['Auto_increment'];
+        
+        return $max;
     }
 }
