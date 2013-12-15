968ff2d882c34e08d514d779f95207aad401cff6
Alex Rock Ancelet <pierstoval@gmail.com>
2013-12-15 21:35:59 +0100

    Maps : 1_stop_movement.js
    Déplacé la fonctionnalité "stop mouvement" dans un fichier indépendant.

---
+++ src/CorahnRin/MapsBundle/Resources/public/js/map_edit/1_stop_movement.js
@@ -0,0 +1,24 @@
+// Désactivation du mouvement
+document.getElementById('map_dont_move').onclick = function(){
+    var active = this.getAttribute('data-active');
+    if (active === 'true') {
+        // Réactivation du mouvement de la map
+        active = 'false';
+        document.corahn_rin_map.allowMove(true);
+        this.classList.remove('active');
+        this.parentNode.classList.remove('active');
+        this.children[0].classList.remove('text-danger');
+        this.children[0].classList.add('text-success');
+    } else {
+        // Désactivation du mouvement de la map
+        active = 'true';
+        document.corahn_rin_map.allowMove(false);
+        this.classList.add('active');
+        this.parentNode.classList.add('active');
+        this.children[0].classList.add('text-danger');
+        this.children[0].classList.remove('text-success');
+    }
+    this.onblur();
+    this.setAttribute('data-active', active);
+    return false;
+};
