8317d56c24dd85e6e2f7e81e0dfc4c69a91f5b8c
Alex Rock Ancelet <pierstoval@gmail.com>
2014-01-12 11:53:43 +0100

    Ajout de fichier suite au commit 00f126404a18d72199af935897fb8728181b1a73
    Fichier "HelperExtension"

---
+++ src/CorahnRin/ToolsBundle/Twig/HelperExtension.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace CorahnRin\ToolsBundle\Twig;
+
+/**
+ * Class HelperExtension
+ * Project corahn_rin
+ *
+ * @author Pierstoval
+ * @version 1.0 08/01/2014
+ */
+class HelperExtension extends \Twig_Extension {
+
+    public function getName() {
+        return 'corahnrin_helper_extension';
+    }
+
+    public function getFilters() {
+        return array(
+            'pr' => new \Twig_Filter_Method($this, 'prFilter'),
+            'datetime' => new \Twig_Filter_Method($this, 'datetimeFilter')
+        );
+    }
+
+    /**
+     * Fonctions Twig
+     */
+    public function getFunctions() {
+        return array(
+            'is_array' => new \Twig_Function_Method($this, 'is_arrayFunction'),
+        );
+    }
+
+    public function is_arrayFunction($element) {
+        return is_array($element);
+    }
+
+    public function datetimeFilter($d, $format = null) {
+        if (!$format) { $format = '%A %d %B %Y %H:%M'; }
+        if ($d instanceof \DateTime) { $d = $d->getTimestamp(); }
+        return strftime($format, $d);
+    }
+
+    public function prFilter($var, $return = false) {
+		return \CorahnRinTools\pr($var, $return);
+    }
+
+}
