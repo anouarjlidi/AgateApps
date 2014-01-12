b609f8a6e89222a9b439363d8183a054980e8d09
Alex Rock Ancelet <pierstoval@gmail.com>
2014-01-12 11:55:11 +0100

    Repository de base : correction d'un bug énorme
    La méthode `findBy()` a été corrigée, elle ne renvoyait aucune donnée,
    ce qui causait l'échec systématique des autres méthodes et de la méthode
    `findAll()`.
    Externalisé la méthode "sortCollection"

--- src/CorahnRin/ToolsBundle/Repository/CorahnRinRepository.php
+++ src/CorahnRin/ToolsBundle/Repository/CorahnRinRepository.php
@@ -1,8 +1,7 @@
 <?php
 
 namespace CorahnRin\ToolsBundle\Repository;
-use Doctrine\ORM\EntityRepository;
-use Doctrine\ORM\Query\ResultSetMapping;
+use Doctrine\ORM\EntityRepository as EntityRepository;
 
 /**
  * CorahnRinRepository
@@ -10,35 +9,52 @@ use Doctrine\ORM\Query\ResultSetMapping;
  */
 abstract class CorahnRinRepository extends EntityRepository {
 
-    public function findAll($sortCollection = false) {
-        $datas = parent::findAll();
-        $total = array();
-        if ($datas) {
-            if ($sortCollection === false) {
-                $total = $datas;
-            } else {
-                foreach ($datas as $entity) {
-                    $total[$entity->getId()] = $entity;
-                }
-            }
+    public function findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null, $sortCollection = false) {
+        $datas = parent::findBy($criteria, $orderBy, $limit, $offset);
+        if ($datas && $sortCollection === true) {
+            $datas = $this->sortCollection($datas);
         }
-        return $total;
+        return $datas;
     }
-    
+
+    public function findAll($sortCollection = false) {
+        return $this->findBy(array(), null, null, null, $sortCollection);
+    }
+
+    /**
+     * Alias de getMax()
+     * @see getMax()
+     * @return int
+     */
+    public function getAutoIncrement() {
+        return $this->getMax();
+    }
+
     /**
      * Récupère la valeur de l'AUTO_INCREMENT de la table de l'entité
-     * @return int 
+     * @return int
      */
     public function getMax() {
         $table = $this->getClassMetadata()->getTableName();
-        
+
         $connection = $this->getEntityManager()->getConnection();
         $statement = $connection->prepare('SHOW TABLE STATUS LIKE "'.$table.'" ');
         $statement->execute();
         $datas = $statement->fetch();
-        
+
         $max = (int) $datas['Auto_increment'];
-        
+
         return $max;
+    }
+
+    public function sortCollection($collection){
+        $total = array();
+        $current = current($collection);
+        if (method_exists($current, 'getId')) {
+            foreach ($collection as $entity) {
+                $total[$entity->getId()] = $entity;
+            }
+        }
+        return $total ?: $collection;
     }
 }
