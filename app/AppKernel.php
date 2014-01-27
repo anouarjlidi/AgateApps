a1c682ebbc7a44cbe7eb559eb3b1c33def901612
Alex Rock Ancelet <pierstoval@gmail.com>
2014-01-27 14:04:33 +0100

    Kernel : créé un nouveau workspace
    Un environnement "dev_fast" a été créé pour éviter les problèmes de
    temps de chargement liés aux assets, relativement longs...

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -29,7 +29,7 @@ class AppKernel extends Kernel
             new CorahnRin\ImagickBundle\CorahnRinImagickBundle(),
         );
 
-        if (in_array($this->getEnvironment(), array('dev', 'test'))) {
+        if (in_array($this->getEnvironment(), array('dev', 'test','dev_fast'))) {
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
