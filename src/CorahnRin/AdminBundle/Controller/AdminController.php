86bc15be4c2b28cdd7faabd7546f77df031cd4ad
Alex Rock Ancelet <pierstoval@gmail.com>
2014-01-29 16:05:21 +0100

    AdminBundle:AdminController
    Désormais le menu est géré par le contrôleur, il renvoie donc un tableau
    de données avec un format particulier qui permet au template Twig
    d'afficher un menu très facilement.
    L'index va devoir être modifié pour afficher plus tard un tableau de
    bord. À décider avec l'équipe du contenu de ce tableau de bord.

--- src/CorahnRin/AdminBundle/Controller/AdminController.php
+++ src/CorahnRin/AdminBundle/Controller/AdminController.php
@@ -10,10 +10,54 @@ class AdminController extends Controller
 {
     /**
      * @Route("/")
-     * @Template()
+     * @Template("CorahnRinAdminBundle:Admin:layout.html.twig")
      */
-    public function indexAction()
-    {
-        return array();
+    public function indexAction() {
+        $controller = '';
+        $params = array();
+        return array(
+            'controller' => $controller,
+            '_params' => $params,
+        );
     }
+
+	/**
+	 * @Template()
+	 */
+	public function menuAction($route) {
+        $links = array(
+            'Tableau de bord' => array(
+                '_route' => 'corahnrin_admin_admin_index',
+                '_params'=>array(),
+            ),
+            'Profil' => array(
+                'Éditer' => array(
+                    '_route' => 'fos_user_profile_edit',
+                    '_params' => array(),
+                ),
+                'Voir' => array(
+                    '_route' => 'fos_user_profile_show',
+                    '_params' => array(),
+                ),
+            ),
+        );
+        $context = $securityContext = $this->container->get('security.context');
+        if ($context->isGranted('ROLE_ADMIN_MAPS')) {
+            $links['Esteren Maps'] = array(
+                'Cartes' => array(
+                    '_route' => 'corahnrin_maps_maps_adminlist',
+                ),
+                'Marqueurs' => array(
+                    '_route' => 'corahnrin_maps_markers_adminlist',
+                ),
+                'Factions' => array(
+                    '_route' => 'corahnrin_maps_factions_adminlist',
+                ),
+            );
+        }
+		return array(
+            'links' => $links,
+            'route' => $route,
+        );
+	}
 }
