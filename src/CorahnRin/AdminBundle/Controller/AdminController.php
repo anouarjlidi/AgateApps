01d9ef823544e96aa5951862cbbb8a5cfd2cc155
Alex Rock Ancelet <pierstoval@gmail.com>
2014-02-01 12:26:27 +0100

    Optimisation convertisseur CLI
    Fix 2e7d89b495c94f084d7e86c9a30c0d6f9fd6c9d4 . Le menu d'administration
    était incorrect. Suppression de la précédente méthode d'injection de
    menus dans l'administration.

--- src/CorahnRin/AdminBundle/Controller/AdminController.php
+++ src/CorahnRin/AdminBundle/Controller/AdminController.php
@@ -26,32 +26,6 @@ class AdminController extends Controller
 	 */
 	public function menuAction($route) {
 
-        $links = array(
-            'Tableau de bord' => array(
-                '_route' => 'corahnrin_admin_admin_index',
-            ),
-            'Profil' => array(
-                'Éditer' => array(
-                    '_route' => 'fos_user_profile_edit',
-                ),
-                'Voir' => array(
-                    '_route' => 'fos_user_profile_show',
-                ),
-            ),
-        );
-        if ($this->get('security.context')->isGranted('ROLE_ADMIN_MAPS')) {
-            $links['Esteren Maps'] = array(
-                'Cartes' => array(
-                    '_route' => 'corahnrin_maps_maps_adminlist',
-                ),
-                'Marqueurs' => array(
-                    '_route' => 'corahnrin_maps_markers_adminlist',
-                ),
-                'Factions' => array(
-                    '_route' => 'corahnrin_maps_factions_adminlist',
-                ),
-            );
-        }
         $links = $this->getDoctrine()->getManager()->getRepository('CorahnRinPagesBundle:Menus')->findTree('Administration');
 		return array(
             'links' => $links,
