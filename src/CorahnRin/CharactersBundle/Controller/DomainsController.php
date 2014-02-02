166dba6078ee8e2c1d05c1d103dd452e8c19d836
Alex Rock Ancelet <pierstoval@gmail.com>
2014-02-02 17:29:50 +0100

    Ajouté la gestion des Domaines dans le backoffice

---
+++ src/CorahnRin/CharactersBundle/Controller/DomainsController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace CorahnRin\CharactersBundle\Controller;
+
+use CorahnRin\CharactersBundle\Entity\Domains;
+use CorahnRin\CharactersBundle\Form\DomainsType;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class DomainsController extends Controller
+{
+    /**
+     * @Route("/admin/generator/domains/")
+     * @Template()
+     */
+    public function adminListAction() {
+        $name = str_replace('Controller','',preg_replace('#^([a-zA-Z]+\\\)*#isu', '', __CLASS__));
+        return array(
+            strtolower($name) => $this->getDoctrine()->getManager()->getRepository('CorahnRinCharactersBundle:'.$name)->findAll(),
+        );
+    }
+
+    /**
+     * @Route("/admin/generator/domains/edit/{id}")
+     * @Template("CorahnRinAdminBundle:Form:add.html.twig")
+     */
+    public function editAction(Domains $domain)
+    {
+        return $this->handle_request($domain);
+    }
+
+    private function handle_request(Domains $element) {
+        $method = preg_replace('#^'.str_replace('\\','\\\\',__CLASS__).'::([a-zA-Z]+)Action$#isUu', '$1', $this->getRequest()->get('_controller'));
+
+        $form = $this->createForm(new \CorahnRin\CharactersBundle\Form\DomainsType(), $element);
+
+        $request = $this->get('request');
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($element);
+            $em->flush();
+            $this->get('session')->getFlashBag()->add('success', 'Domaine modifié : <strong>'.$element->getName().'</strong>');
+            return $this->redirect($this->generateUrl('corahnrin_characters_domains_adminlist'));
+        }
+
+        return array(
+            'form' => $form->createView(),
+            'title' => 'Modifier un domaine',
+            'breadcrumbs' => array(
+                'Accueil' => array('route' => 'corahnrin_admin_admin_index',),
+                'Domaines' => array('route'=>'corahnrin_characters_domains_adminlist'),
+            ),
+        );
+    }
+}
