728d93c2c4897fcd9cb8aa5eda2830a5a1228fec
Alex Rock Ancelet <pierstoval@gmail.com>
2014-02-02 17:30:22 +0100

    Ajouté la gestion des Disciplines dans le backoffice

---
+++ src/CorahnRin/CharactersBundle/Controller/DisciplinesController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace CorahnRin\CharactersBundle\Controller;
+
+use CorahnRin\CharactersBundle\Entity\Disciplines;
+use CorahnRin\CharactersBundle\Form\DisciplinesType;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class DisciplinesController extends Controller
+{
+    /**
+     * @Route("/admin/generator/disciplines/")
+     * @Template()
+     */
+    public function adminListAction() {
+        $name = str_replace('Controller','',preg_replace('#^([a-zA-Z]+\\\)*#isu', '', __CLASS__));
+        return array(
+            strtolower($name) => $this->getDoctrine()->getManager()->getRepository('CorahnRinCharactersBundle:'.$name)->findAll(),
+        );
+    }
+
+    /**
+     * @Route("/admin/generator/disciplines/add/")
+     * @Template("CorahnRinAdminBundle:Form:add.html.twig")
+     */
+    public function addAction()
+    {
+        if (false === $this->get('security.context')->isGranted('ROLE_ADMIN_GENERATOR_SUPER')) {
+            throw new AccessDeniedException();
+        }
+        return $this->handle_request(new Disciplines);
+    }
+
+    /**
+     * @Route("/admin/generator/disciplines/edit/{id}")
+     * @Template("CorahnRinAdminBundle:Form:add.html.twig")
+     */
+    public function editAction(Disciplines $discipline)
+    {
+        return $this->handle_request($discipline);
+    }
+
+    /**
+     * @Route("/admin/generator/disciplines/delete/{id}")
+     * @Template()
+     */
+    public function deleteAction(Disciplines $element)
+    {
+        if (false === $this->get('security.context')->isGranted('ROLE_ADMIN_GENERATOR_SUPER')) {
+            throw new AccessDeniedException();
+        }
+
+        $em = $this->getDoctrine()->getManager();
+        $element->setDeleted(1);
+        $em->persist($element);
+        $em->flush();
+        $this->get('session')->getFlashBag()->add('success', 'Discipline supprimé : <strong>'.$element->getName().'</strong>');
+        return $this->redirect($this->generateUrl('corahnrin_characters_disciplines_adminlist'));
+    }
+
+    private function handle_request(Disciplines $element) {
+        $method = preg_replace('#^'.str_replace('\\','\\\\',__CLASS__).'::([a-zA-Z]+)Action$#isUu', '$1', $this->getRequest()->get('_controller'));
+
+        $form = $this->createForm(new \CorahnRin\CharactersBundle\Form\DisciplinesType(), $element);
+
+        $request = $this->get('request');
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($element);
+            $em->flush();
+            $this->get('session')->getFlashBag()->add('success', 'Discipline '.($method=='add'?'ajouté':'modifié').' : <strong>'.$element->getName().'</strong>');
+            return $this->redirect($this->generateUrl('corahnrin_characters_disciplines_adminlist'));
+        }
+
+        return array(
+            'form' => $form->createView(),
+            'title' => ($method=='add'?'Ajouter':'Modifier').' un Discipline',
+            'breadcrumbs' => array(
+                'Accueil' => array('route' => 'corahnrin_admin_admin_index',),
+                'Disciplines' => array('route'=>'corahnrin_characters_disciplines_adminlist'),
+            ),
+        );
+    }
+}
