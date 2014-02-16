da5d8e1d3795d0cd9efdf94bd339345916b6b1a1
Alex Rock Ancelet <pierstoval@gmail.com>
2014-02-16 14:04:31 +0100

    Ajout des Ogham et des Types d'Ogham au backoffice

---
+++ src/CorahnRin/CharactersBundle/Controller/OghamController.php
@@ -0,0 +1,139 @@
+<?php
+
+namespace CorahnRin\CharactersBundle\Controller;
+
+use CorahnRin\CharactersBundle\Entity\Ogham;
+use CorahnRin\CharactersBundle\Entity\OghamTypes;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class OghamController extends Controller
+{
+    /**
+     * @Route("/admin/generator/ogham/")
+     * @Template()
+     */
+    public function adminListAction() {
+        return array(
+            'ogham_list' => $this->getDoctrine()->getManager()->getRepository('CorahnRinCharactersBundle:Ogham')->findAll(),
+            'oghamTypes' => $this->getDoctrine()->getManager()->getRepository('CorahnRinCharactersBundle:OghamTypes')->findAll(),
+        );
+    }
+
+    /**
+     * @Route("/admin/generator/ogham/add/")
+     * @Template("CorahnRinAdminBundle:Form:add.html.twig")
+     */
+    public function addAction()
+    {
+        if (false === $this->get('security.context')->isGranted('ROLE_ADMIN_GENERATOR_SUPER')) {
+            throw new AccessDeniedException();
+        }
+        return $this->handle_request(new Ogham);
+    }
+
+    /**
+     * @Route("/admin/generator/ogham/addtype/")
+     * @Template("CorahnRinAdminBundle:Form:add.html.twig")
+     */
+    public function addTypeAction()
+    {
+        if (false === $this->get('security.context')->isGranted('ROLE_ADMIN_GENERATOR_SUPER')) {
+            throw new AccessDeniedException();
+        }
+        return $this->handle_request(new OghamTypes);
+    }
+
+    /**
+     * @Route("/admin/generator/ogham/edit/{id}")
+     * @Template("CorahnRinAdminBundle:Form:add.html.twig")
+     */
+    public function editAction(Ogham $ogham)
+    {
+        return $this->handle_request($ogham);
+    }
+
+    /**
+     * @Route("/admin/generator/ogham/edittype/{id}")
+     * @Template("CorahnRinAdminBundle:Form:add.html.twig")
+     */
+    public function editTypeAction(OghamTypes $oghamType)
+    {
+        return $this->handle_request($oghamType);
+    }
+
+    /**
+     * @Route("/admin/generator/ogham/delete/{id}")
+     * @Template()
+     */
+    public function deleteAction(Ogham $element)
+    {
+        if (false === $this->get('security.context')->isGranted('ROLE_ADMIN_GENERATOR_SUPER')) {
+            throw new AccessDeniedException();
+        }
+
+        $em = $this->getDoctrine()->getManager();
+        $element->setDeleted(1);
+        $em->persist($element);
+        $em->flush();
+        $this->get('session')->getFlashBag()->add('success', 'Ogham supprimé : <strong>'.$element->getName().'</strong>');
+        return $this->redirect($this->generateUrl('corahnrin_characters_ogham_adminlist'));
+    }
+
+    /**
+     * @Route("/admin/generator/ogham/deletetype/{id}")
+     * @Template()
+     */
+    public function deleteTypeAction(OghamTypes $element)
+    {
+        if (false === $this->get('security.context')->isGranted('ROLE_ADMIN_GENERATOR_SUPER')) {
+            throw new AccessDeniedException();
+        }
+
+        $em = $this->getDoctrine()->getManager();
+        $element->setDeleted(1);
+        $em->persist($element);
+        $em->flush();
+        $this->get('session')->getFlashBag()->add('success', 'Type d\'Ogham supprimé : <strong>'.$element->getName().'</strong>');
+        return $this->redirect($this->generateUrl('corahnrin_characters_ogham_adminlist'));
+    }
+
+    private function handle_request($element) {
+        $method = preg_replace('#^'.str_replace('\\','\\\\',__CLASS__).'::([a-zA-Z]+)Action$#isUu', '$1', $this->getRequest()->get('_controller'));
+        $method = str_replace('Type','',$method);
+
+
+        if (is_a($element, 'CorahnRin\CharactersBundle\Entity\Ogham')) {
+            $type = false;
+            $form = $this->createForm(new \CorahnRin\CharactersBundle\Form\OghamType(), $element);
+        } elseif (is_a($element, 'CorahnRin\CharactersBundle\Entity\OghamTypes')) {
+            $type = true;
+            $form = $this->createForm(new \CorahnRin\CharactersBundle\Form\OghamTypesType(), $element);
+        }
+
+        $request = $this->get('request');
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($element);
+            $em->flush();
+
+            $this->get('session')->getFlashBag()->add('success', ($type?'Type d\'':'').'Ogham '.($method=='add'?'ajouté':'modifié').' : <strong>'.$element->getName().'</strong>');
+            return $this->redirect($this->generateUrl('corahnrin_characters_ogham_adminlist'));
+        }
+
+        return array(
+            'form' => $form->createView(),
+            'title' => ($method=='add'?'Ajouter':'Modifier').' un '.($type?'type d\'':'').'Ogham',
+            'breadcrumbs' => array(
+                'Accueil' => array('route' => 'corahnrin_admin_admin_index',),
+                'Ogham' => array('route'=>'corahnrin_characters_ogham_adminlist'),
+            ),
+        );
+    }
+}
