4287a6fd49eea02cf29687ae0c2f85628fcaebcd
Alex Rock Ancelet <pierstoval@gmail.com>
2014-02-16 14:05:58 +0100

    Ajout des Peuples au backoffice

---
+++ src/CorahnRin/CharactersBundle/Controller/PeoplesController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace CorahnRin\CharactersBundle\Controller;
+
+use CorahnRin\CharactersBundle\Entity\Peoples;
+use CorahnRin\CharactersBundle\Form\PeoplesType;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class PeoplesController extends Controller
+{
+    /**
+     * @Route("/admin/generator/peoples/")
+     * @Template()
+     */
+    public function adminListAction() {
+        $name = str_replace('Controller','',preg_replace('#^([a-zA-Z]+\\\)*#isu', '', __CLASS__));
+        return array(
+            strtolower($name) => $this->getDoctrine()->getManager()->getRepository('CorahnRinCharactersBundle:'.$name)->findAll(),
+        );
+    }
+
+    /**
+     * @Route("/admin/generator/peoples/add/")
+     * @Template("CorahnRinAdminBundle:Form:add.html.twig")
+     */
+    public function addAction()
+    {
+        if (false === $this->get('security.context')->isGranted('ROLE_ADMIN_GENERATOR_SUPER')) {
+            throw new AccessDeniedException();
+        }
+        return $this->handle_request(new Peoples);
+    }
+
+    /**
+     * @Route("/admin/generator/peoples/edit/{id}")
+     * @Template("CorahnRinAdminBundle:Form:add.html.twig")
+     */
+    public function editAction(Peoples $people)
+    {
+        return $this->handle_request($people);
+    }
+
+    /**
+     * @Route("/admin/generator/peoples/delete/{id}")
+     * @Template()
+     */
+    public function deleteAction(Peoples $element)
+    {
+        if (false === $this->get('security.context')->isGranted('ROLE_ADMIN_GENERATOR_SUPER')) {
+            throw new AccessDeniedException();
+        }
+
+        $em = $this->getDoctrine()->getManager();
+        $element->setDeleted(1);
+        $em->persist($element);
+        $em->flush();
+        $this->get('session')->getFlashBag()->add('success', 'Peuple supprimé : <strong>'.$element->getName().'</strong>');
+        return $this->redirect($this->generateUrl('corahnrin_characters_peoples_adminlist'));
+    }
+
+    private function handle_request(Peoples $element) {
+        $method = preg_replace('#^'.str_replace('\\','\\\\',__CLASS__).'::([a-zA-Z]+)Action$#isUu', '$1', $this->getRequest()->get('_controller'));
+
+        $form = $this->createForm(new \CorahnRin\CharactersBundle\Form\PeoplesType(), $element);
+
+        $request = $this->get('request');
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($element);
+            $em->flush();
+            $this->get('session')->getFlashBag()->add('success', 'Peuple '.($method=='add'?'ajouté':'modifié').' : <strong>'.$element->getName().'</strong>');
+            return $this->redirect($this->generateUrl('corahnrin_characters_peoples_adminlist'));
+        }
+
+        return array(
+            'form' => $form->createView(),
+            'title' => ($method=='add'?'Ajouter':'Modifier').' un peuple',
+            'breadcrumbs' => array(
+                'Accueil' => array('route' => 'corahnrin_admin_admin_index',),
+                'Peuples' => array('route'=>'corahnrin_characters_peoples_adminlist'),
+            ),
+        );
+    }
+}
---
+++ src/CorahnRin/CharactersBundle/Form/PeoplesType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace CorahnRin\CharactersBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class PeoplesType extends AbstractType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', 'text', array('label' => 'Nom'))
+            ->add('description', 'textarea', array('label' => 'Description', 'required'=>false))
+            ->add('book', null, array('label'=>'Livre', 'property'=>'name'))
+            ->add('save','submit',array('label'=>'Enregistrer'))
+        ;
+    }
+
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'CorahnRin\CharactersBundle\Entity\Peoples'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'corahnrin_charactersbundle_peoples';
+    }
+}
