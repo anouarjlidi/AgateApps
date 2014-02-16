d5931195dab2e5b1cf9d38702f397246dba3bc88
Alex Rock Ancelet <pierstoval@gmail.com>
2014-02-16 14:02:40 +0100

    Ajout des Miracles au backoffice

---
+++ src/CorahnRin/CharactersBundle/Controller/MiraclesController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace CorahnRin\CharactersBundle\Controller;
+
+use CorahnRin\CharactersBundle\Entity\Miracles;
+use CorahnRin\CharactersBundle\Form\MiraclesType;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class MiraclesController extends Controller
+{
+    /**
+     * @Route("/admin/generator/miracles/")
+     * @Template()
+     */
+    public function adminListAction() {
+        $name = str_replace('Controller','',preg_replace('#^([a-zA-Z]+\\\)*#isu', '', __CLASS__));
+        return array(
+            strtolower($name) => $this->getDoctrine()->getManager()->getRepository('CorahnRinCharactersBundle:'.$name)->findAll(),
+        );
+    }
+
+    /**
+     * @Route("/admin/generator/miracles/add/")
+     * @Template("CorahnRinAdminBundle:Form:add.html.twig")
+     */
+    public function addAction()
+    {
+        if (false === $this->get('security.context')->isGranted('ROLE_ADMIN_GENERATOR_SUPER')) {
+            throw new AccessDeniedException();
+        }
+        return $this->handle_request(new Miracles);
+    }
+
+    /**
+     * @Route("/admin/generator/miracles/edit/{id}")
+     * @Template("CorahnRinAdminBundle:Form:add.html.twig")
+     */
+    public function editAction(Miracles $miracle)
+    {
+        return $this->handle_request($miracle);
+    }
+
+    /**
+     * @Route("/admin/generator/miracles/delete/{id}")
+     * @Template()
+     */
+    public function deleteAction(Miracles $element)
+    {
+        if (false === $this->get('security.context')->isGranted('ROLE_ADMIN_GENERATOR_SUPER')) {
+            throw new AccessDeniedException();
+        }
+
+        $em = $this->getDoctrine()->getManager();
+        $element->setDeleted(1);
+        $em->persist($element);
+        $em->flush();
+        $this->get('session')->getFlashBag()->add('success', 'Miracle supprimé : <strong>'.$element->getName().'</strong>');
+        return $this->redirect($this->generateUrl('corahnrin_characters_miracles_adminlist'));
+    }
+
+    private function handle_request(Miracles $element) {
+        $method = preg_replace('#^'.str_replace('\\','\\\\',__CLASS__).'::([a-zA-Z]+)Action$#isUu', '$1', $this->getRequest()->get('_controller'));
+
+        $form = $this->createForm(new \CorahnRin\CharactersBundle\Form\MiraclesType(), $element);
+
+        $request = $this->get('request');
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($element);
+            $em->flush();
+            $this->get('session')->getFlashBag()->add('success', 'Miracle '.($method=='add'?'ajouté':'modifié').' : <strong>'.$element->getName().'</strong>');
+            return $this->redirect($this->generateUrl('corahnrin_characters_miracles_adminlist'));
+        }
+
+        return array(
+            'form' => $form->createView(),
+            'title' => ($method=='add'?'Ajouter':'Modifier').' un Miracle',
+            'breadcrumbs' => array(
+                'Accueil' => array('route' => 'corahnrin_admin_admin_index',),
+                'Miracles' => array('route'=>'corahnrin_characters_miracles_adminlist'),
+            ),
+        );
+    }
+}
---
+++ src/CorahnRin/CharactersBundle/Tests/Controller/MiraclesControllerTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace CorahnRin\CharactersBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class MiraclesControllerTest extends WebTestCase
+{
+    public function testAdminlist()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/admin/generator/miracles/');
+    }
+
+    public function testAdd()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/admin/generator/miracles/add/');
+    }
+
+    public function testEdit()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/admin/generator/miracles/edit/{id}');
+    }
+
+    public function testDelete()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/admin/generator/miracles/delete/{id}');
+    }
+
+}
