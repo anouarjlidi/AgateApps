d2cf3644ebe4a59fb5ec9b314e2308ed142c7d6d
Alex Rock Ancelet <pierstoval@gmail.com>
2014-06-20 23:36:48 +0200

    Création d'un script bash qui permet de simplifier le déploiement. (et mise à jour d'un fichier relatif à PHPStorm)

---
+++ deploy_dev.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+echo " > Suppression du cache"
+php app/console cache:clear --env=dev
+php app/console cache:clear --env=dev_fast
+
+echo " > Mise à jour des dépendances"
+php composer.phar update -v
+bower update
+
+echo " > Installation des assets "
+php app/console assets:install
+php app/console assetic:dump --env=dev_fast
