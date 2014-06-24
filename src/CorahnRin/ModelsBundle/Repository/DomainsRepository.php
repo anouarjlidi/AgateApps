635f1533289bb18f6c516c44735d28722de7c2db
Alex Rock Ancelet <pierstoval@gmail.com>
2014-06-24 21:07:16 +0200

    Fix d'une erreur reçue en prod empêchant l'affichage du formulaire "Avantages"

--- src/CorahnRin/ModelsBundle/Repository/DomainsRepository.php
+++ src/CorahnRin/ModelsBundle/Repository/DomainsRepository.php
@@ -9,9 +9,8 @@ use CorahnRin\ToolsBundle\Repository\CorahnRinRepository as CorahnRinRepository;
  */
 class DomainsRepository extends CorahnRinRepository {
 
-
     public function findAllSortedByName(array $criteria = array(), array $orderBy = null, $limit = null, $offset = null, $sortCollection = true) {
-        $orderBy = array('p.name' => 'asc');
+        $orderBy = array('name' => 'asc');
         $datas = $this->findBy($criteria, $orderBy, $limit, $offset, $sortCollection);
         foreach ($datas as $id => $element) {
             $datas[$id] = $element->getName();
