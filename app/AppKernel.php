b703fbb550084e9aaa8f34ec2fb459918c0c617b
Alex Rock Ancelet <pierstoval@gmail.com>
2014-09-14 23:19:30 +0200

    Mise à jour deps composer + modification comportement de composer Ajout du bundle DoctrineFixtures, suppression des bundles de Sonata qui sont désormais abandonnés. Modifications dans composer.json : après un update, composer ne supprime plus le cache et n'installe plus les assets, cela pour éviter les problèmes de "cache:clear" défectueux, ou les erreurs d'application/configuration de bundles  après la construction du fichier bootstrap de SF2.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -3,9 +3,11 @@
 use Symfony\Component\HttpKernel\Kernel;
 use Symfony\Component\Config\Loader\LoaderInterface;
 
-class AppKernel extends Kernel {
+class AppKernel extends Kernel
+{
 
-    public function registerBundles() {
+    public function registerBundles()
+    {
 
         $bundles = array(
 
@@ -17,18 +19,14 @@ class AppKernel extends Kernel {
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-
             // Doctrine
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
-
             // JMS
             new JMS\SerializerBundle\JMSSerializerBundle($this),
-
             // FOS
             new FOS\UserBundle\FOSUserBundle(),
             new FOS\RestBundle\FOSRestBundle(),
-
             // Sonata et dépendances
             new Sonata\SeoBundle\SonataSeoBundle(),
             new Sonata\CoreBundle\SonataCoreBundle(),
@@ -38,25 +36,20 @@ class AppKernel extends Kernel {
             new Sonata\BlockBundle\SonataBlockBundle(),
             new Sonata\AdminBundle\SonataAdminBundle(),
             new Sonata\MediaBundle\SonataMediaBundle(),
-            new Sonata\jQueryBundle\SonatajQueryBundle(),
-            new Sonata\MarkItUpBundle\SonataMarkItUpBundle(),
             new Sonata\FormatterBundle\SonataFormatterBundle(),
             new Sonata\EasyExtendsBundle\SonataEasyExtendsBundle(),
             new Sonata\UserBundle\SonataUserBundle('FOSUserBundle'),
             new Sonata\NotificationBundle\SonataNotificationBundle(),
             new Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle(),
-
             new Knp\Bundle\MenuBundle\KnpMenuBundle(),
             new Knp\Bundle\MarkdownBundle\KnpMarkdownBundle(),
             new Ivory\CKEditorBundle\IvoryCKEditorBundle(),
             new Symfony\Cmf\Bundle\RoutingBundle\CmfRoutingBundle(),
-
             // Local Sonata generated bundles
             new Application\Sonata\PageBundle\ApplicationSonataPageBundle(),
             new Application\Sonata\UserBundle\ApplicationSonataUserBundle(),
             new Application\Sonata\AdminBundle\ApplicationSonataAdminBundle(),
             new Application\Sonata\MediaBundle\ApplicationSonataMediaBundle(),
-
             // Corahn-Rin
             new CorahnRin\AdminBundle\CorahnRinAdminBundle(),
             new CorahnRin\CharactersBundle\CorahnRinCharactersBundle(),
@@ -64,13 +57,10 @@ class AppKernel extends Kernel {
             new CorahnRin\ModelsBundle\CorahnRinModelsBundle(),
             new CorahnRin\ToolsBundle\CorahnRinToolsBundle(),
             new CorahnRin\GeneratorBundle\CorahnRinGeneratorBundle(),
-
             // Esteren général
             new Esteren\PagesBundle\EsterenPagesBundle(),
-
             // Esteren Maps
             new EsterenMaps\MapsBundle\EsterenMapsBundle(),
-
             // Pierstoval's tools
             new Pierstoval\Bundle\ApiBundle\PierstovalApiBundle(),
             new Pierstoval\Bundle\ToolsBundle\PierstovalToolsBundle(),
@@ -83,12 +73,14 @@ class AppKernel extends Kernel {
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+            $bundles[] = new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle();
         }
 
         return $bundles;
     }
 
-    public function registerContainerConfiguration(LoaderInterface $loader) {
+    public function registerContainerConfiguration(LoaderInterface $loader)
+    {
         $loader->load(__DIR__.'/config/config_'.$this->getEnvironment().'.yml');
     }
 }
