3dacbc2dc08cd94da97539f2afd29e5e5528fd1e
Alex Rock Ancelet <pierstoval@gmail.com>
2014-09-21 19:37:54 +0200

    Ajout de la shebang line sur la commande d'init ACL

--- init_acl.sh
+++ init_acl.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 setfacl -R -m u:www-data:rwX -m u:`whoami`:rwX app/cache app/logs var/cache var/logs
 setfacl -dR -m u:www-data:rwx -m u:`whoami`:rwx app/cache app/logs var/cache var/logs
 
