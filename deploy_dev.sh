443e9edb32e74425ca0c8eee7839cd207c8ccc93
Alex Rock Ancelet <pierstoval@gmail.com>
2015-01-07 09:45:27 +0100

    Updated deploy dev too

--- deploy_dev.sh
+++ deploy_dev.sh
@@ -1,14 +1,23 @@
 #!/bin/sh
 
-echo " > Suppression du cache"
-php bin/console cache:clear --env=dev
-php bin/console cache:clear --env=dev_fast
+echo " > Mise à jour de Composer "
+php composer.phar selfupdate
 
 echo " > Mise à jour des dépendances"
-php composer.phar selfupdate
-php composer.phar update -v
-bower update
+php composer.phar install
+bower install
+
+echo " > Suppression du cache"
+php bin/console cache:clear --env=dev
+php bin/console cache:clear --env=prod
 
 echo " > Installation des assets "
 php bin/console assets:install
-php bin/console assetic:dump --env=dev_fast
+php bin/console assetic:dump --env=prod
+
+echo " > Insertion des fixtures "
+php bin/console doctrine:fixtures:load --append
+
+echo " > Intégration des snapshots de Sonata dans l'application."
+php bin/console sonata:page:update-core-routes --site=1
+php bin/console sonata:page:create-snapshots --site=1
