c25af992036dd9fd3e86af194b3d7a5c819eb6ec
Alex Rock Ancelet <pierstoval@gmail.com>
2015-01-19 10:40:20 +0100

    Evolution #808 @0.05 Deleted unused bundles

--- src/Application/Sonata/UserBundle/Repository/GroupRepository.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Application\Sonata\UserBundle\Repository;
-
-use Doctrine\ORM\EntityRepository;
-
-class GroupRepository extends EntityRepository {
-
-} 
--- src/Application/Sonata/UserBundle/Repository/UserRepository.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Application\Sonata\UserBundle\Repository;
-
-use Doctrine\ORM\EntityRepository;
-
-class UserRepository extends EntityRepository {
-
-} 
