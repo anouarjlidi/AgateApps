66a69e3ffea28a9f90b55696afe6af5cc0502cb0
Alex Rock Ancelet <pierstoval@gmail.com>
2015-02-22 17:45:21 +0100

    Optim AdminMapsController

--- src/EsterenMaps/AdminBundle/Controller/AdminMapsController.php
+++ src/EsterenMaps/AdminBundle/Controller/AdminMapsController.php
@@ -2,28 +2,17 @@
 
 namespace EsterenMaps\AdminBundle\Controller;
 
-use EsterenMaps\MapsBundle\Entity\Factions;
 use EsterenMaps\MapsBundle\Entity\Maps;
-use EsterenMaps\MapsBundle\Entity\Markers;
-use EsterenMaps\MapsBundle\Entity\MarkersTypes;
-use EsterenMaps\MapsBundle\Entity\Routes;
-use EsterenMaps\MapsBundle\Entity\RoutesTypes;
-use EsterenMaps\MapsBundle\Entity\Zones;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
+/**
+ * @Security("has_role('ROLE_MANAGER')")
+ */
 class AdminMapsController extends Controller {
-
-    /** @var MarkersTypes[] */
-    private $markersTypes;
-
-    /** @var RoutesTypes[] */
-    private $routesTypes;
-
-    /** @var Factions[] */
-    private $factions;
 
     /**
      * @Route("/maps/edit-interactive/{id}", name="admin_esterenmaps_maps_maps_editInteractive")
@@ -34,30 +23,12 @@ class AdminMapsController extends Controller {
      */
     public function editAction(Maps $map, Request $request) {
 
-        $this->denyAccessUnlessGranted('ROLE_MANAGER');
-
-        $em = $this->getDoctrine()->getManager();
-
-        $routesTypes = $em->getRepository('EsterenMapsBundle:RoutesTypes')->findAll(true);
-        $markersTypes = $em->getRepository('EsterenMapsBundle:MarkersTypes')->findAll(true);
-        $factions = $em->getRepository('EsterenMapsBundle:Factions')->findAll(true);
-
-        $this->routesTypes = $routesTypes;
-        $this->markersTypes = $markersTypes;
-        $this->factions = $factions;
-
-        $tilesUrl = $this->generateUrl('esterenmaps_api_tiles_tile_distant', array('id'=>0,'x'=>0,'y'=>0,'zoom'=>0), true);
-        $tilesUrl = str_replace('0/0/0/0','{id}/{z}/{x}/{y}', $tilesUrl);
-        $tilesUrl = preg_replace('~app_dev(_fast)\.php/~isUu', '', $tilesUrl);
+        $request->query->set('entity', 'Maps');
 
         return array(
-            'config' => $this->container->getParameter('easy_admin.config'),
+            'config' => $this->container->getParameter('easyadmin.config'),
             'map' => $map,
-            'tilesUrl' => $tilesUrl,
             'tile_size' => $this->container->getParameter('esterenmaps.tile_size'),
-            'routesTypes' => $routesTypes,
-            'markersTypes' => $markersTypes,
-            'factions' => $factions,
         );
     }
 
