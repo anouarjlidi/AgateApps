a4e44bc2aa1176c779f27ebf0762f363d11b4e70
Alex Rock Ancelet <pierstoval@gmail.com>
2015-05-11 18:39:19 +0200

    Process reference datas first (types..)

--- src/EsterenMaps/MapsBundle/Command/ImportTiddlyWikiCommand.php
+++ src/EsterenMaps/MapsBundle/Command/ImportTiddlyWikiCommand.php
@@ -11,6 +11,7 @@ use EsterenMaps\MapsBundle\Entity\RoutesTypes;
 use EsterenMaps\MapsBundle\Entity\Zones;
 use EsterenMaps\MapsBundle\Entity\Routes;
 use EsterenMaps\MapsBundle\Entity\ZonesTypes;
+use Orbitale\Component\DoctrineTools\BaseEntityRepository;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
@@ -19,6 +20,21 @@ use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 
 class ImportTiddlyWikiCommand extends ContainerAwareCommand
 {
+
+    /**
+     * @var array
+     */
+    protected $datas = array();
+
+    /**
+     * @var EntityManager
+     */
+    protected $em;
+
+    /**
+     * @var OutputInterface
+     */
+    protected $output;
 
     protected function configure()
     {
@@ -33,6 +49,8 @@ class ImportTiddlyWikiCommand extends ContainerAwareCommand
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $this->output = $output;
+
         $file = $input->getArgument('file');
 
         $datas = file_get_contents($file);
@@ -48,10 +66,14 @@ class ImportTiddlyWikiCommand extends ContainerAwareCommand
         }
 
         /**
-         * @var EntityManager $em
+         * @var EntityManager $this->em
          */
-        $em = $this->getContainer()->get('doctrine')->getManager();
+        $this->em = $this->getContainer()->get('doctrine')->getManager();
 
+        // Setting all datas in the class for we can use it in the other methods
+        $this->datas = $datas;
+
+        $output->writeln('Processing reference datas');
         /** @var Markers[] $markers */
         /** @var Zones[] $zones */
         /** @var Routes[] $routes */
@@ -60,19 +82,22 @@ class ImportTiddlyWikiCommand extends ContainerAwareCommand
         /** @var MarkersTypes[] $markersTypes */
         /** @var ZonesTypes[] $zonesTypes */
         /** @var RoutesTypes[] $routesTypes */
-        $markers = $em->getRepository('EsterenMapsBundle:Markers')->findAllRoot('id');
-        $zones = $em->getRepository('EsterenMapsBundle:Zones')->findAllRoot('id');
-        $routes = $em->getRepository('EsterenMapsBundle:Routes')->findAllRoot('id');
+        $markers = $this->em->getRepository('EsterenMapsBundle:Markers')->findAllRoot('id');
+        $zones = $this->em->getRepository('EsterenMapsBundle:Zones')->findAllRoot('id');
+        $routes = $this->em->getRepository('EsterenMapsBundle:Routes')->findAllRoot('id');
 
-        $maps = $em->getRepository('EsterenMapsBundle:Maps')->findAllRoot('id');
-        $factions = $em->getRepository('EsterenMapsBundle:Factions')->findAllRoot('id');
-        $markersTypes = $em->getRepository('EsterenMapsBundle:MarkersTypes')->findAllRoot('id');
-        $zonesTypes = $em->getRepository('EsterenMapsBundle:ZonesTypes')->findAllRoot('id');
-        $routesTypes = $em->getRepository('EsterenMapsBundle:RoutesTypes')->findAllRoot('id');
+        $maps = $this->em->getRepository('EsterenMapsBundle:Maps')->findAllRoot('id');
+        $factions = $this->em->getRepository('EsterenMapsBundle:Factions')->findAllRoot('id');
+        $markersTypes = $this->getReferenceObjects('markertype', 'EsterenMapsBundle:MarkersTypes', 'id_marker');
+        $zonesTypes = $this->getReferenceObjects('zonetype', 'EsterenMapsBundle:ZonesTypes', 'id_zonetype');
+        $routesTypes = $this->getReferenceObjects('routetype', 'EsterenMapsBundle:RoutesTypes', 'id_route');
 
         $inputMarkers = array();
+        $inputMarkersTypes = array();
         $inputZones = array();
         $inputRoutes = array();
+
+        $unknownTypes = array();
 
         $numb = 0;
         $total = count($datas);
@@ -80,15 +105,30 @@ class ImportTiddlyWikiCommand extends ContainerAwareCommand
         $exceptionMessages = '';
 
         $output->writeln('Found <info>'.$total.'</info> items to check for import.');
+
+        if ($output->getVerbosity() >= OutputInterface::VERBOSITY_VERBOSE) {
+            $output->writeln('First character:');
+            $output->writeln(' <comment>M</comment>: Markers');
+            $output->writeln(' <comment>R</comment>: Routes');
+            $output->writeln(' <comment>Z</comment>: Zones');
+            $output->writeln(' <comment>E</comment>: An error occurred');
+            $output->writeln(' <comment>_</comment>: Unknown type');
+            $output->writeln('Second character:');
+            $output->writeln(' <comment>i</comment>: Insert');
+            $output->writeln(' <comment>u</comment>: Update');
+        }
+
+        $output->writeln('Processing...');
+
         foreach ($datas as $data) {
             $numb++;
 
             $status = ' ';
 
             $type = $data['tags'];
-            $id = $data['id'];
-            $created = DateTime::createFromFormat('YmdHisu', $data['created']);
-            $modified = DateTime::createFromFormat('YmdHisu', $data['modified']);
+            $id = isset($data['id']) ? $data['id'] : null;
+            $created = DateTime::createFromFormat('YmdHisu', isset($data['created']) ? $data['created'] : date('YmdHisu'));
+            $modified = DateTime::createFromFormat('YmdHisu', isset($data['modified']) ? $data['modified'] : date('YmdHisu'));
             $title = $data['title'];
             $text = $data['text'];
 
@@ -99,12 +139,22 @@ class ImportTiddlyWikiCommand extends ContainerAwareCommand
                     case 'marqueurs':
                         $status .= 'M';
                         $object = isset($dbMarkers[$id]) ? $dbMarkers[$id] : new Markers;
+
+                        if (isset($markersTypes[$data['markertype_id']])) {
+                            $markerType = $markersTypes[$data['markertype_id']];
+                        } else {
+                            $markerType = new MarkersTypes();
+                            $markerType->setName(str_replace('id_marker', '', $data['markertype_id']));
+                            $markerType->setDescription('');
+                            $inputMarkersTypes[] = $markerType;
+                        }
+
                         $object
                             ->setName($title)
                             ->setDescription($text)
                             ->setFaction($factions[$data['faction_id']])
                             ->setMap($maps[$data['map_id']])
-                            ->setMarkerType($markersTypes[$data['markertype_id']])
+                            ->setMarkerType($markerType)
                             ->setCreatedAt($created)
                             ->setUpdatedAt($modified)
                         ;
@@ -114,25 +164,25 @@ class ImportTiddlyWikiCommand extends ContainerAwareCommand
                         if (!$object->getId()) {
                             $inputMarkers[$id] = $object;
                         }
-                        $em->persist($object);
+                        $this->em->persist($object);
                         break;
                     case 'zones':
                         $status .= 'Z';
                         $object = isset($dbZones[$id]) ? $dbZones[$id] : new Zones;
                         break;
                     case 'routes':
-                        $status .= 'Z';
+                        $status .= 'R';
                         $object = isset($dbRoutes[$id]) ? $dbRoutes[$id] : new Routes;
                         break;
                     default:
-                        $output->writeln('<error>Unknown type: '.$type.'</error>');
+                        $unknownTypes[$type] = null;
                         $status .= '_';
-                        continue;
+                        break;
                 }
-                $status .= $object->getId() ? 'u' : 'i';
+                $status .= $object && $object->getId() ? 'u' : 'i';
             } catch (\Exception $e) {
                 $status .= 'E';
-                $exceptionMessages .= "\n\t".get_class($e)."\t".(isset($object) && $object ? get_class($object) : '')."\t".$e->getLine().':'.$e->getMessage();
+                $exceptionMessages .= "\n".$e->getFile().':'.$e->getLine()."    ".$e->getMessage();
             }
             $output->write($status);
             if (($numb % 20 === 0 || $numb === $total) && $numb) {
@@ -143,11 +193,52 @@ class ImportTiddlyWikiCommand extends ContainerAwareCommand
                 $output->write(' '.$percent."\n");
             }
 
+        } // end foreach
+
+        if (count($unknownTypes)) {
+            $exceptionMessages .= "\nUnknown Types:\n".(implode(', ', array_keys($unknownTypes)));
         }
 
         if ($exceptionMessages) {
-            $output->writeln("<error>\n$exceptionMessages\n</error>");
+            throw new \RuntimeException($exceptionMessages);
         }
+    }
+
+    /**
+     * @param string $dataTag
+     * @param string $entity
+     * @param string $idToReplace
+     * @param string $nameProperty
+     *
+     * @return \object[]
+     */
+    protected function getReferenceObjects($dataTag, $entity, $idToReplace, $nameProperty = 'name')
+    {
+        $datas = array_filter($this->datas, function($element) use ($dataTag) {
+            return isset($element['tags']) && $element['tags'] === $dataTag;
+        });
+
+        /** @var BaseEntityRepository $repo */
+        $repo = $this->em->getRepository($entity);
+
+        $objects = $repo->findAllRoot('id');
+
+        foreach ($datas as $data) {
+            if (strpos($data['id'], $idToReplace) === 0) {
+                $exists = $repo->findOneBy(array($nameProperty => $data['title']));
+                if (!$exists) {
+                    dump('does not exist', $data);
+                    $object = new MarkersTypes();
+                    $object->{'set'.ucfirst($nameProperty)}($data['title']);
+                    $this->em->persist($object);
+                    $this->em->flush($object);
+                    $objects[$object->getId()] = $object;
+                    $this->output->writeln('Added new object of type <comment>'.$entity.'</comment> with name <comment>'.$object->getName().'</comment>');
+                }
+            }
+        }
+
+        return $objects;
     }
 
 }
