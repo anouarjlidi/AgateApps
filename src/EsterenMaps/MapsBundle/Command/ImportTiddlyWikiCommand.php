cdf6b205d58ffe57dc902d03d7d6d0c9368e5abf
Alex Rock Ancelet <pierstoval@gmail.com>
2015-06-06 23:24:05 +0200

    Finish issue #957 @4.5

--- src/EsterenMaps/MapsBundle/Command/ImportTiddlyWikiCommand.php
+++ src/EsterenMaps/MapsBundle/Command/ImportTiddlyWikiCommand.php
@@ -107,6 +107,10 @@ class ImportTiddlyWikiCommand extends ContainerAwareCommand
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $code = 100;
+
+        $time = microtime(true);
+
         $this->output = $output;
 
         $this->dryRun = !$input->getOption('force');
@@ -153,32 +157,62 @@ class ImportTiddlyWikiCommand extends ContainerAwareCommand
         $this->zonesTypes   = $this->getReferenceObjects('zonetype', ZonesTypes::class, 'id_zone');
         $this->routesTypes  = $this->getReferenceObjects('routetype', RoutesTypes::class, 'id_route');
 
-        $this->processObjects('marqueurs', Markers::class, 'id_');
-        $this->processObjects('zones', Zones::class, 'id_');
-        $this->processObjects('routes', Routes::class, 'id_');
-//
-//        // Edit ALL entities before processing flush
-//        $this->em->getUnitOfWork()->computeChangeSets();
-//
-//        $updates = $this->em->getUnitOfWork()->getScheduledEntityUpdates();
-//        $updatesCollec = $this->em->getUnitOfWork()->getScheduledCollectionUpdates();
-//        $this->showProcessed('Update', array_merge($updates, $updatesCollec));
-//        $inserts = $this->em->getUnitOfWork()->getScheduledEntityInsertions();
-//        $this->showProcessed('Add', $inserts);
+        $this->markers = $this->processObjects('marqueurs', Markers::class, 'id_');
+        $this->zones   = $this->processObjects('zones', Zones::class, 'id_');
+        $this->routes  = $this->processObjects('routes', Routes::class, 'id_');
 
         if ($this->dryRun) {
             $output->writeln('Finished processing dry run.');
-            return 1;
+            $code = 1;
+        } else {
+            try {
+                $output->write('Attempting to flush all datas...');
+                $this->em->flush();
+                $output->writeln(' <info>Ok!</info>');
+                $code = 0;
+            } catch (\Exception $e) {
+                throw new \RuntimeException('An error occured when trying to update the database.', null, $e);
+            }
         }
 
-        try {
-            $this->em->flush();
-        } catch (\Exception $e) {
-            throw new \RuntimeException('An error occured when trying to update the database.', null, $e);
+        $idsUpdated = 0;
+
+        foreach ($this->datas as $k => $data) {
+            $id = $data['id'];
+
+            if (is_numeric($id)) {
+                continue;
+            }
+
+            $newId = null;
+
+            switch ($data['tags']) {
+                case 'marqueurs':
+                    $newId = $this->markers['new'][$id]->getId();
+                    break;
+                case 'zones':
+                    $newId = $this->zones['new'][$id]->getId();
+                    break;
+                case 'routes':
+                    $newId = $this->routes['new'][$id]->getId();
+                    break;
+                case 'factions':
+                    $newId = $this->factions['new'][$id]->getId();
+                    break;
+            }
+
+            if ($newId) {
+                $this->datas[$k]['id'] = $newId;
+                $idsUpdated++;
+            }
         }
 
-        //TODO: Update the whole TiddlyWiki file to manage updates
+        $json = json_encode($this->datas, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_BIGINT_AS_STRING);
+        file_put_contents($file, $json);
+        $output->writeln('Updated <info>'.$idsUpdated.'</info> identifiers in the JSON file.');
 
+        $output->writeln('Executed in '.number_format(microtime(true) - $time, 4).' seconds');
+        return $code;
     }
 
     /**
@@ -187,7 +221,7 @@ class ImportTiddlyWikiCommand extends ContainerAwareCommand
      * @param string $idToReplace
      * @param string $nameProperty
      *
-     * @return object[]
+     * @return array
      */
     private function getReferenceObjects($tag, $entity, $idToReplace, $nameProperty = 'name')
     {
@@ -216,10 +250,10 @@ class ImportTiddlyWikiCommand extends ContainerAwareCommand
 
             if ($object) {
                 $existing[$object->getId()] = $object;
-                $type = 'Update';
+                $type = '~ Update ';
             } else {
                 $object = new $entity();
-                $type = 'Add   ';
+                $type = '+ Add    ';
                 $new[$id] = $object;
             }
 
@@ -246,9 +280,10 @@ class ImportTiddlyWikiCommand extends ContainerAwareCommand
     /**
      * @param string $tag
      * @param string $entity
-     * @param string $idToReplace
+     *
+     * @return array
      */
-    private function processObjects($tag, $entity, $idToReplace)
+    private function processObjects($tag, $entity)
     {
         $datas = array_filter($this->datas, function ($element) use ($tag) {
             return isset($element['tags']) && $element['tags'] === $tag;
@@ -275,10 +310,10 @@ class ImportTiddlyWikiCommand extends ContainerAwareCommand
 
             if ($object) {
                 $existing[$object->getId()] = $object;
-                $type = 'Update';
+                $type = '~ Update ';
             } else {
                 $object = new $entity();
-                $type = 'Add   ';
+                $type = '+ Add    ';
                 $new[$id] = $object;
             }
 
@@ -286,152 +321,22 @@ class ImportTiddlyWikiCommand extends ContainerAwareCommand
 
             $this->showOneProcessed('object', $type, $object);
 
+            $this->updateOneObject($object, $data);
+
             $this->em->persist($object);
         }
 
         foreach ($objects as $object) {
             if (!isset($existing[$object->getId()])) {
                 $existing[$object->getId()] = $object;
-                $this->showOneProcessed('Nothing done on object', '', $object);
+                $this->showOneProcessed('object', '= Nothing', $object);
             }
         }
 
-
-
-        return;
-        $datas = array_filter($this->datas, function ($element) use ($tag) {
-            return isset($element['tags']) && $element['tags'] === $tag;
-        });
-
-        $repo = $this->getRepository($entity);
-
-        // Fetch base objects
-        $objects = $repo->findAllRoot('id');
-        $finalObjects = array();
-
-        foreach ($datas as $data) {
-
-            /** @var Routes|Markers|Zones $object */
-            $object = null;
-
-            $type = $data['tags'];
-            $id   = isset($data['id']) ? $data['id'] : null;
-
-            if (strpos($data['id'], $idToReplace) === 0) {
-                $object = $repo->findOneBy(array('name' => $data['title']));
-                if (!$object) {
-                    $object = new $entity();
-                }
-            } else {
-                if (isset($objects[$id])) {
-                    $object = $objects[$id];
-                } else {
-                    $this->output->writeln('<error>Did not find "'.$tag.'" object with id "'.$id.'".</error>');
-                    continue;
-                }
-            }
-
-            if ($this->dryRun && !$object->getId()) {
-                $object->setId($id);
-            }
-
-            dump($object);
-//            $data = $this->normalizeData($data);
-            continue;
-
-            switch ($type) {
-                case 'marqueurs':
-                    $object
-                        ->setId($data['id'])
-                        ->setName($data['title'])
-                        ->setDescription($data['text'])
-                        ->setMap($data['map_id'])
-                        ->setFaction($data['faction_id'] ?: null)
-                        ->setMarkerType($data['markertype_id'])
-                    ;
-                    $object->setCreatedAt($data['created']);
-                    $object->setUpdatedAt($data['modified']);
-                    if (!$object->isLocalized()) {
-                        $object->setLatitude(0)->setLongitude(0);
-                    }
-                    break;
-                case 'routes':
-                    $object
-                        ->setId($data['id'])
-                        ->setName($data['title'])
-                        ->setDescription($data['text'])
-                        ->setMap($data['map_id'])
-                        ->setFaction($data['faction_id'] ?: null)
-                        ->setRouteType($data['routetype_id'])
-                        ->setMarkerStart($data['markerstart_id'] ?: null)
-                        ->setMarkerEnd($data['markerend_id'] ?: null)
-                    ;
-                    $object->setCreatedAt($data['created']);
-                    $object->setUpdatedAt($data['modified']);
-                    if (!$object->isLocalized()) {
-                        $object->setCoordinates(array(
-                            array('lat' => 0, 'lng' => 0),
-                            array('lat' => 1, 'lng' => 1),
-                        ));
-                    }
-                    break;
-                case 'zones':
-                    $object
-                        ->setId($data['id'])
-                        ->setName($data['title'])
-                        ->setDescription($data['text'])
-                        ->setMap($data['map_id'])
-                        ->setFaction($data['faction_id'] ?: null)
-                        ->setZoneType($data['zonetype_id'])
-                    ;
-                    $object->setCreatedAt($data['created']);
-                    $object->setUpdatedAt($data['modified']);
-                    if (!$object->isLocalized()) {
-                        $object->setCoordinates(array(
-                            array('lat' => 0, 'lng' => 0),
-                            array('lat' => 0, 'lng' => 1),
-                            array('lat' => 1, 'lng' => 0),
-                        ));
-                    }
-                    break;
-                default:
-                    throw new \RuntimeException('Unknown type "'.$type.'".');
-            }
-
-            if (method_exists($object, 'setUpdatedAt')) {
-                $object->setUpdatedAt(new DateTime());
-            }
-
-            $finalObjects[$object->getId()] = $object;
-
-            switch ($tag) {
-                case 'zones':
-                    $this->zones = $finalObjects;
-                    break;
-                case 'marqueurs':
-                    $this->markers = $finalObjects;
-                    break;
-                case 'routes':
-                    $this->routes = $finalObjects;
-                    break;
-            }
-
-            $this->em->persist($object);
-            usleep(31250*3);
-        }
-
-        return;
-        switch ($tag) {
-            case 'zones':
-                $this->zones = $finalObjects;
-                break;
-            case 'marqueurs':
-                $this->markers = $finalObjects;
-                break;
-            case 'routes':
-                $this->routes = $finalObjects;
-                break;
-        }
+        return array(
+            'new' => $new,
+            'existing' => $existing,
+        );
     }
 
     /**
@@ -445,71 +350,7 @@ class ImportTiddlyWikiCommand extends ContainerAwareCommand
         $class = array_pop($class);
         $ref = (string) $object;
         $processType = $processType ? '<comment>'.$processType.'</comment>' : '';
-        $this->output->writeln(' > '.$processType.'</comment> '.$objectType.' of type <comment>'.$class.'</comment>: <info>'.$ref.'</info>');
-        usleep(31250);
-    }
-
-    /**
-     * @param string   $processType
-     * @param object[] $objects
-     */
-    private function showProcessed($processType, array $objects = array())
-    {
-        foreach ($objects as $object) {
-            $class = explode('\\', get_class($object));
-            $class = array_pop($class);
-            if (in_array($class, array('Markers', 'Zones', 'Routes'))) {
-                $type = 'object';
-                $refType = 'id';
-                if ($processType === 'Update') {
-                    $ref = (string) $object;
-                } else {
-                    $ref = $object->getId();
-                }
-            } else {
-                $type = 'reference';
-                $refType = 'name';
-                $ref = $object->getName();
-            }
-            $this->output->writeln(' > <comment>'.$processType.'</comment> '.$type.' of type <comment>'.$class.'</comment> with '.$refType.' <info>'.$ref.'</info>');
-            usleep(31250);
-        }
-    }
-
-    /**
-     * @param array $data
-     *
-     * @return array
-     */
-    private function normalizeData(array $data)
-    {
-        $data['created']  = DateTime::createFromFormat('YmdHisu', isset($data['created']) ? $data['created'] : date('YmdHisu'));
-        $data['modified'] = DateTime::createFromFormat('YmdHisu', isset($data['modified']) ? $data['modified'] : date('YmdHisu'));
-
-        $fieldsToNormalize = array(
-            'map_id'         => array('property' => 'maps'),
-            'markertype_id'  => array('property' => 'markersTypes'),
-            'routetype_id'   => array('property' => 'routesTypes'),
-            'zonetype_id'    => array('property' => 'zonesTypes'),
-            'markerend_id'   => array('property' => 'markers'),
-            'markerstart_id' => array('property' => 'markers'),
-            'faction_id'     => array('property' => 'factions'),
-        );
-
-        foreach ($fieldsToNormalize as $field => $attr) {
-            $property = $attr['property'];
-            if (!isset($this->$property)) {
-                throw new \InvalidArgumentException('Property '.$property.' does not exist as reference.');
-            }
-            $list = $this->$property;
-            if (isset($data[$field]) && isset($list[$data[$field]])) {
-                $data[$field] = $list[$data[$field]];
-            } elseif (isset($data[$field]) && !isset($list[$data[$field]]) && !in_array($field, array('faction_id', 'markerend_id', 'markerstart_id'))) {
-                throw new \RuntimeException('<error>Could not find "'.$property.'" reference object with id "'.$data[$field].'".</error>');
-            }
-        }
-
-        return $data;
+        $this->output->writeln(' '.$processType.'</comment> '.$objectType.' of type <comment>'.$class.'</comment>: <info>'.$ref.'</info>');
     }
 
     /**
@@ -523,6 +364,72 @@ class ImportTiddlyWikiCommand extends ContainerAwareCommand
             $this->repos[$entityName] = $this->em->getRepository($entityName);
         }
         return $this->repos[$entityName];
+    }
+
+    private function updateOneObject($object, $data)
+    {
+        $data['created']  = DateTime::createFromFormat('YmdHisu', isset($data['created']) ? $data['created'] : date('YmdHisu'));
+
+        $object
+            ->setId(is_numeric($data['id']) ? $data['id'] : null)
+            ->setName($data['title'])
+            ->setDescription($data['text'])
+            ->setMap($this->maps[$data['map_id']])
+            ->setFaction($this->getOneReferencedObject('factions', $data['faction_id']))
+            ->setCreatedAt($data['created'])
+        ;
+
+        if (method_exists($object, 'setUpdatedAt')) {
+            $object->setUpdatedAt(new DateTime());
+        }
+
+        if ($object instanceof Markers) {
+            $object
+                ->setMarkerType($this->getOneReferencedObject('markersTypes', $data['markertype_id']))
+            ;
+            if (!$object->isLocalized()) {
+                $object->setLatitude(0)->setLongitude(0);
+            }
+        } elseif ($object instanceof Routes) {
+            $object
+                ->setRouteType($this->getOneReferencedObject('routesTypes', $data['routetype_id']))
+                ->setMarkerStart($this->getOneReferencedObject('markers', $data['markerstart_id']))
+                ->setMarkerEnd($this->getOneReferencedObject('markers', $data['markerend_id']))
+            ;
+            if (!$object->isLocalized()) {
+                $object->setCoordinates(array(
+                    array('lat' => 0, 'lng' => 0),
+                    array('lat' => 1, 'lng' => 1),
+                ));
+            }
+        } elseif ($object instanceof Zones) {
+            $object
+                ->setZoneType($this->getOneReferencedObject('zonesTypes', $data['zonetype_id']))
+            ;
+            if (!$object->isLocalized()) {
+                $object->setCoordinates(array(
+                    array('lat' => 0, 'lng' => 0),
+                    array('lat' => 0, 'lng' => 1),
+                    array('lat' => 1, 'lng' => 0),
+                ));
+            }
+        } else {
+            throw new \RuntimeException('Unknown type "'.$data['tags'].'".');
+        }
+    }
+
+    private function getOneReferencedObject($type, $id)
+    {
+        $object = null;
+        if ($id) {
+            $datas = $this->$type;
+            if (isset($datas['new'][$id])) {
+                $object = $datas['new'][$id];
+            } elseif (isset($datas['existing'][$id])) {
+                $object = $datas['existing'][$id];
+            }
+        }
+        return $object;
     }
 
 }
