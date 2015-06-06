dbcbdc0f23f4b276ca541a06a9bbbcb1b8cba7e1
Alex Rock Ancelet <pierstoval@gmail.com>
2015-06-06 16:48:51 +0200

    Update reset script to fit new version (and fix bash shell)

--- reset.bash
+++ reset.bash
@@ -4,10 +4,16 @@
 # Under MIT license. Feel free to re-use, but don't forget credits ;)
 # Original file: https://gist.github.com/Pierstoval/88650af7942ab381c893
 
-SFENV=$1
-SFENV=${SFENV:=dev}
+NL=$'\n'
 
-if [[ "$SFENV" =~ ^dev|prod|test$ ]]; then
+SFENV=$1
+if [[ -z "$SFENV" ]]
+then
+    SFENV="dev"
+fi
+
+if echo "$SFENV" | grep -E '^dev|prod|test$' > /dev/null
+then
         echo "Check env $SFENV"
 else
         echo "First argument must be a valid environment: dev, prod or test"
@@ -17,13 +23,16 @@ fi
 
 CONSOLECMD="php app/console --env=$SFENV "
 
-echo "\nWelcome to Symfony's application resetter!"
+echo $NL
+echo "Welcome to Symfony's application resetter!"
 
-echo "\n================"
+echo $NL
+echo "================"
 echo "Remove cache and assets"
 rm -rf app/cache/* web/bundles/*
 
-echo "\n================"
+echo $NL
+echo "================"
 echo "> Delete and recreate the whole database"
 ${CONSOLECMD} doctrine:database:drop --force
 ${CONSOLECMD} doctrine:database:create
@@ -32,7 +41,8 @@ ${CONSOLECMD} doctrine:schema:create
 checkFixturesEnabled=`${CONSOLECMD} list doctrine -n --no-ansi | grep "doctrine:fixtures:load"`
 if [ -n "checkFixturesEnabled" ]
 then
-    echo "\n================"
+    echo $NL
+    echo "================"
     echo "> Load fixtures"
     ${CONSOLECMD} doctrine:fixtures:load --append
 fi
@@ -44,19 +54,24 @@ then
     ${CONSOLECMD} fos:user:create --super-admin admin admin@localhost admin
 fi
 
-echo "\n================"
+echo $NL
+echo "================"
 echo "> Force recreation of the assets in relative mode"
 ${CONSOLECMD} assets:i --symlink --relative
 
-echo "\n================"
+echo $NL
+echo "================"
 echo "> Compile assetic files"
 ${CONSOLECMD} assetic:dump
 
-echo "\n================"
-echo "Finished !\n"
+echo $NL
+echo "================"
+echo "Finished !"
+echo $NL
 
 if [ -n "$checkFosUserEnabled" ]
 then
     echo "INFO: If you used this function in production, please note that you must change the user password by executing this command:"
-    echo "${CONSOLECMD} fos:user:change-password admin newPassword\n"
+    echo "${CONSOLECMD} fos:user:change-password admin newPassword"
+    echo $NL
 fi
