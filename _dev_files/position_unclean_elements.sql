6f985d279df70ce9f39a7d2834b8a73b1b4b9c80
Alex Rock Ancelet <pierstoval@gmail.com>
2015-06-15 18:33:27 +0200

    Update dev queries to update routes and zones

--- _dev_files/position_unclean_elements.sql
+++ _dev_files/position_unclean_elements.sql
@@ -1,1 +1,48 @@
-update maps_markers set latitude = (80 + (id/50) + (id/250)), longitude = (-177 + (-1*id/50) + (-1*id/250)) where latitude = 0;
+update maps_markers set
+    latitude = (80 + (id/50) + (id/250)),
+    longitude = (-177 + (-1*id/50) + (-1*id/250))
+    where latitude = 0
+;
+
+update maps_routes set
+    coordinates =
+    CONCAT(
+        "[",
+            '{',
+                '"lat":', ( 50 + (id/50) + (id/250) ),
+                ',',
+                '"lng":', (-170 + (-1*id/50) + (-1*id/250) ),
+            '},',
+            '{',
+                '"lat":', ( 51 + (id/50) + (id/250) ),
+                ',',
+                '"lng":', (-167.5 + (-1*id/50) + (-1*id/250) ),
+            '}',
+        "]"
+    )
+    where coordinates = '[{"lat":"0","lng":"0"}]'
+;
+
+update maps_zones set
+    coordinates =
+    CONCAT(
+        "[",
+            '{',
+                '"lat":', ( 60 + (id/50) + (id/250) ),
+                ',',
+                '"lng":', (-170 + (-1*id/50) + (-1*id/250) ),
+            '},',
+            '{',
+                '"lat":', ( 61 + (id/50) + (id/250) ),
+                ',',
+                '"lng":', (-167.5 + (-1*id/50) + (-1*id/250) ),
+            '},',
+            '{',
+                '"lat":', ( 62 + (id/50) + (id/250) ),
+                ',',
+                '"lng":', (-170 + (-1*id/50) + (-1*id/250) ),
+            '}',
+        "]"
+    )
+    where coordinates = ''
+;
