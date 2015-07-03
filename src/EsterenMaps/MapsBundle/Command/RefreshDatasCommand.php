bc2931503513063f3d16488fc536e80c1600d387
Alex Rock Ancelet <pierstoval@gmail.com>
2015-07-03 15:04:13 +0200

    Added "refresh-datas" command to Maps, to update routes #966 @0.75

---
+++ src/EsterenMaps/MapsBundle/Command/RefreshDatasCommand.php
@@ -0,0 +1,91 @@
+<?php
+namespace EsterenMaps\MapsBundle\Command;
+
+use Doctrine\ORM\EntityManager;
+use EsterenMaps\MapsBundle\Entity\Maps;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class RefreshDatasCommand extends ContainerAwareCommand {
+
+	protected function configure() {
+
+		//public function addArgument($name, $mode = null, $description = '', $default = null)
+		$this
+            ->setName('esterenmaps:refresh-datas')
+            ->setDescription('Refresh all dynamic datas (routes distances, etc.)')
+            ->setHelp('This command is here to update all datas that may be dynamic.'."\n"
+              .'For example, the start and end marker of each route may change its coordinates,'."\n"
+              .'and the distance depends on the coordinates, so both datas are updated with this command.'."\n\n"
+              .'This command runs on every data for every map in the database.'
+            )
+        ;
+	}
+
+	protected function execute(InputInterface $input, OutputInterface $output)
+    {
+
+        /** @var EntityManager $em */
+        $em = $this->getContainer()->get('doctrine')->getManager();
+
+        $maps = $em->getRepository('EsterenMapsBundle:Maps')->findAll();
+
+        $numberTotal = 0;
+
+        foreach ($maps as $map) {
+
+            foreach ($map->getRoutes() as $route) {
+                $route->refresh();
+                $em->persist($route);
+                $numberTotal++;
+            }
+
+        }
+
+        $uow = $em->getUnitOfWork();
+
+        $uow->computeChangeSets();
+
+        $updates = $uow->getScheduledEntityUpdates();
+
+        $numberModified = 0;
+
+        foreach ($updates as $entity) {
+            $changesets = $uow->getEntityChangeSet($entity);;
+
+            if (isset($changesets['distance'])) {
+                $changesets['distance'] = array_map(function($e) {
+                    // This trick truncates the number
+                    // Else, a number like 219.664402090055 would have been treated as 219.664402090060
+                    $shift = pow(10, 10);
+                    $e = ((floor($e * $shift)) / $shift);
+                    return number_format($e, 11, '.', '');
+                }, $changesets['distance']);
+                if ($changesets['distance'][0] === $changesets['distance'][1]) {
+                    continue;
+                }
+            }
+
+            $numberModified++;
+        }
+
+        if ($numberModified === 0) {
+            $output->writeln('Nothing to be updated.');
+            return 1;
+        }
+
+        $output->writeln('Elements updated: <info>'.$numberModified.' / '.$numberTotal.'</info>');
+
+        try {
+            $em->flush();
+        } catch (\Exception $e) {
+            throw $e;
+        }
+
+        $output->writeln('Finished!');
+
+	}
+}
