238f2d17e9139af32611009a9b79b4a488c0c1b0
Alex Rock Ancelet <pierstoval@gmail.com>
2015-07-25 17:23:01 +0200

    Refactor config tree and add better support for new portable app #699 @1.25

---
+++ app/AppPortableKernel.php
@@ -0,0 +1,64 @@
+<?php
+
+use Symfony\Component\HttpKernel\Kernel;
+use Symfony\Component\Config\Loader\LoaderInterface;
+
+class AppPortableKernel extends Kernel
+{
+    public function registerBundles()
+    {
+        $bundles = array(
+            new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+            new Symfony\Bundle\SecurityBundle\SecurityBundle(),
+            new Symfony\Bundle\TwigBundle\TwigBundle(),
+            new Symfony\Bundle\MonologBundle\MonologBundle(),
+            new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
+            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
+            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
+            new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+
+            new FOS\UserBundle\FOSUserBundle(),
+            new FOS\RestBundle\FOSRestBundle(),
+//            new Nelmio\CorsBundle\NelmioCorsBundle(),
+            new Knp\Bundle\MenuBundle\KnpMenuBundle(),
+            new JMS\SerializerBundle\JMSSerializerBundle(),
+            //new JavierEguiluz\Bundle\EasyAdminBundle\EasyAdminBundle(),
+            new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
+
+            // Application global
+            new UserBundle\UserBundle(),
+            //new AdminBundle\AdminBundle(),
+
+            // Corahn-Rin
+//            new CorahnRin\CorahnRinBundle\CorahnRinBundle(),
+
+            // Esteren
+            new Esteren\PortalBundle\EsterenPortalBundle(),
+
+            // Esteren Maps
+            new EsterenMaps\MapsBundle\EsterenMapsBundle(),
+            new EsterenMaps\ApiBundle\EsterenMapsApiBundle(),
+
+            // Pierstoval's tools
+            new Pierstoval\Bundle\ApiBundle\PierstovalApiBundle(),
+            new Pierstoval\Bundle\ToolsBundle\PierstovalToolsBundle(),
+//            new Orbitale\Bundle\CmsBundle\OrbitaleCmsBundle(),
+            new Orbitale\Bundle\TranslationBundle\OrbitaleTranslationBundle(),
+        );
+
+        if (in_array($this->getEnvironment(), array('dev', 'test'))) {
+            $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
+            $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
+            $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
+            $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+            $bundles[] = new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle();
+        }
+
+        return $bundles;
+    }
+
+    public function registerContainerConfiguration(LoaderInterface $loader)
+    {
+        $loader->load(__DIR__.'/config/config_'.$this->getEnvironment().'.yml');
+    }
+}
---
+++ setup_portable.php
@@ -0,0 +1,71 @@
+<?php
+
+use Symfony\Component\Finder\Finder;
+
+define('DS', DIRECTORY_SEPARATOR);
+
+$loader = include __DIR__.DS.'app'.DS.'bootstrap.php.cache';
+
+$fs = new Symfony\Component\Filesystem\Filesystem();
+
+$remove = array(
+    '_dev_files',
+    'app/config/_app_web.yml',
+    'app/config/config_prod.yml',
+    'app/config/config_dev.yml',
+    'app/config/config_test.yml',
+    'app/config/routing_dev.yml',
+    'app/config/routing__corahnrin.yml',
+    'app/config/routing__fos.yml',
+    'app/config/routing__orbitale.yml',
+    'app/config/routing__portal.yml',
+    'app/cache/dev',
+    'app/cache/prod',
+    'app/cache/portable',
+    'app/check.php',
+    'app/SymfonyRequirements.php',
+    'app/phpunit.xml.dist',
+    'node_modules',
+    'web/app.php',
+    'web/app_dev.php',
+    'web/config.php',
+    'web/robots.txt',
+    'web/uploads/maps',
+    'web/components/bootstrap',
+    'web/components/jquery/src',
+    'web/components/jquery/bower.json',
+    'web/components/jquery/.bower.json',
+    'web/components/jquery/MIT-LICENSE.txt',
+    'web/components/jquery.colorpicker',
+    'web/components/jquery-ui',
+    'web/components/leaflet',
+    'web/components/leaflet-draw',
+    'web/components/leaflet-sidebar',
+    'src/AdminBundle',
+    'src/Tests',
+    'src/CorahnRin',
+    'vendor/javiereguiluz/easyadmin-bundle',
+    'vendor/orbitale/cms-bundle',
+    'vendor/nelmio/cors-bundle',
+    '.bowerrc',
+    '.gitattributes',
+    '.gitignore',
+    '.travis.yml',
+    'bower.json',
+    'composer.json',
+    'composer.lock',
+    'cc.cmd',
+    'cs',
+    'cs.bat',
+    'init_acl.sh',
+    'package.json',
+    'reset.bash',
+    'README.md',
+);
+
+foreach ($remove as $path) {
+    $path = str_replace('/', DS, $path);
+    $fs->remove(__DIR__.DS.$path);
+}
+
+
