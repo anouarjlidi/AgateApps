820a3017be1b480ef01f8912794007ed3807c9ad
Alex Rock Ancelet <pierstoval@gmail.com>
2015-07-25 22:08:01 +0200

    Added command to manually insert fixtures from array "sql-natively" #966 @3.5

---
+++ _dev_files/insert_fixtures_manually.php
@@ -0,0 +1,53 @@
+<?php
+
+require __DIR__.'/../app/bootstrap.php.cache';
+
+$params = Symfony\Component\Yaml\Yaml::parse(file_get_contents(__DIR__.'/../app/config/parameters.yml'));
+$params = $params['parameters'];
+
+$config = new Doctrine\DBAL\Configuration();
+$dbParams = array(
+    'driver' => $params['database_driver'],
+    'host' => $params['database_host'],
+    'port' => $params['database_port'],
+    'dbname' => $params['database_name'],
+    'user' => $params['database_user'],
+    'password' => $params['database_password'],
+    'charset' => 'UTF8',
+    'path' => $params['database_path'],
+    'driverOptions' => array(),
+);
+
+if ($dbParams['path']) {
+    $dbParams['path'] = str_replace('%kernel.root_dir%', __DIR__.'/../app', $dbParams['path']);
+    $dbParams['path'] = realpath($dbParams['path']);
+}
+
+$dbal = Doctrine\DBAL\DriverManager::getConnection($dbParams, $config);
+$dbal->connect();
+
+$isSqlite = $dbal->getDatabasePlatform() instanceof Doctrine\DBAL\Platforms\SqlitePlatform;
+
+$fixtures = require 'fixtures.php';
+
+foreach ($fixtures as $table => $lines) {
+    if (!count($lines)) { continue; }
+
+    try {
+        $dbal->query('delete from '.$table)->execute();
+    } catch (Doctrine\DBAL\DBALException $e) {
+        if (strpos($e->getMessage(), 'no such table')) {
+            echo 'No table '.$table.PHP_EOL;
+        }
+    }
+
+    $sqls = array();
+
+    foreach ($lines as $line) {
+        $line = array_map(function ($e) { return !is_numeric($e) ? '"'.str_replace('"', '\\"', $e).'"' : $e; }, $line);
+        $dbal->insert($table, $line);
+        echo "Inserted into $table values with id $id.\n";
+    }
+}
+
+$dbal->close();
