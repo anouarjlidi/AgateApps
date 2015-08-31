694dd02b1220fad3c8ca376f995289f78c9d6f56
Alex Rock Ancelet <pierstoval@gmail.com>
2015-08-31 11:11:36 +0200

    Lighten the map TilesController waiting for refactoring

--- src/EsterenMaps/ApiBundle/Controller/TilesController.php
+++ src/EsterenMaps/ApiBundle/Controller/TilesController.php
@@ -3,8 +3,6 @@
 namespace EsterenMaps\ApiBundle\Controller;
 
 use EsterenMaps\MapsBundle\Entity\Maps;
-use EsterenMaps\MapsBundle\Services\MapsTilesManager;
-use Exception;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Cache;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
@@ -23,91 +21,43 @@ class TilesController extends Controller
      *
      * @param Request $request
      * @param Maps $map
-     * @throws Exception
      * @return Response
      */
     public function generateMapImageAction(Request $request, Maps $map)
     {
-
-        $response = new Response();
-
-        // TODO: Refactor this method
-        $response->setContent('Coming soon...');
-        $response->headers->add(array('Content-Type' => 'text/plain'));
-        return $response;
-
-        $this->init();
-
-        /** @var MapsTilesManager $tilesManager */
-        $tilesManager = $this->container->get('esterenmaps.tiles_manager');
-
-        $tilesManager->setMap($map);
-
-        $get_vars = $request->query;
-        $dimensionsType = $get_vars->get('dimensions-type');
-        $positionType = $get_vars->get('position-type');
-
-        if (!$dimensionsType) { $dimensionsType = 'tile'; }
-        if (preg_match('~^(t(iles?)?)$~isUu', $dimensionsType)) { $dimensionsType = 'tile'; }
-        if (preg_match('~^(p(ixels?)?)$~isUu', $dimensionsType)) { $dimensionsType = 'pixel'; }
-
-        if (!$positionType) { $positionType = 'tile'; }
-        if (preg_match('~^(t(iles?)?)$~isUu', $positionType)) { $positionType = 'tile'; }
-        if (preg_match('~^(p(ixels?)?)$~isUu', $positionType)) { $positionType = 'pixel'; }
-
-        $x = (int) $get_vars->get('x') * ($positionType === 'tile' ? $this->img_size : 1);
-        $y = (int) $get_vars->get('y') * ($positionType === 'tile' ? $this->img_size : 1);
-        $z = (int) $get_vars->get('zoom');
-        $w = (int) $get_vars->get('width') * ($dimensionsType === 'tile' ? $this->img_size : 1);
-        $h = (int) $get_vars->get('height') * ($dimensionsType === 'tile' ? $this->img_size : 1);
-
-        $identification = $tilesManager->identifyImage($z);
-
-        if (!$w) { $w = $this->img_size; }
-        if ($w > $identification['wmax']) { $w = $identification['wmax']; }
-        if (!$h) { $h = $this->img_size; }
-        if ($h > $identification['hmax']) { $h = $identification['hmax']; }
-
-
-        if ($w > 1000) { $w = 1000; }
-        if ($h > 1000) { $h = 1000; }
-
-        $imgname = $tilesManager->mapDestinationName($z, $x, $y, $w, $h);
-
-        // Création de l'image personnalisée
-        if (!file_exists($imgname)) { $tilesManager->createImage($z, $x, $y, $w, $h); }
-
-        // Si l'image n'existe pas à ce stade c'est que la création a échoué
-        if (!file_exists($imgname)) { return $this->quit('Erreur dans la création de l\'image.'); }
-
-        $response->setContent(file_get_contents($imgname));
-
-        $response->headers->set('Content-type', 'image/jpeg');
-        return $response;
+        return new BinaryFileResponse(
+            $this->get('esterenmaps.tiles_manager')
+                ->setMap($map)
+                ->createImage(
+                    (int) $request->query->get('zoom'),
+                    (int) $request->query->get('x'),
+                    (int) $request->query->get('y'),
+                    (int) $request->query->get('width'),
+                    (int) $request->query->get('height')
+                ),
+            200,
+            array('Content-Type' => 'image/jpeg')
+        );
     }
 
     /**
      * @Route("/maps/tile/{id}/{zoom}/{x}/{y}.jpg", requirements={"id":"\d+"}, host="%esteren_domains.api%", name="esterenmaps_api_tiles")
      * @Cache(maxage="864000", expires="+10 days")
+     *
      * @param Maps $map
      * @param integer $zoom
      * @param integer $x
      * @param integer $y
-     * @throws \Exception
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function tileAction(Maps $map, $zoom, $x, $y) {
+    public function tileAction(Maps $map, $zoom, $x, $y)
+    {
+        $outputDirectory = $this->container->getParameter('esterenmaps.output_directory');
 
-        $zoom = (int) $zoom;
-        $x = (int) $x;
-        $y = (int) $y;
-
-        $directory = $this->container->getParameter('esterenmaps.output_directory');
-
-        $file = $directory.$map->getId().'/'.$zoom.'/'.$x.'/'.$y.'.jpg';
+        $file = $outputDirectory.$map->getId().'/'.(int) $zoom.'/'.(int) $x.'/'.(int) $y.'.jpg';
 
         if (!file_exists($file)) {
-            $file = $directory.'/empty.jpg';
+            $file = $outputDirectory.'/empty.jpg';
         }
 
         return new BinaryFileResponse($file, 200, array('Content-Type' => 'image/jpeg'));
