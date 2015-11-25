4f3130a80bb6a33839137845cb4f62879853b56a
Alex Rock Ancelet <pierstoval@gmail.com>
2015-11-25 14:20:48 +0100

    Fixed testing, update dependencies @0.20

--- tests/phpunit/bootstrap.php
+++ tests/phpunit/bootstrap.php
@@ -18,10 +18,6 @@ if (true === $input->hasParameterOption('--no-db')) {
     return;
 }
 
-if (!is_dir($rootDir.'/build')) {
-    mkdir($rootDir.'/build');
-}
-
 // Remove build dir files
 if (is_dir($rootDir.'/build')) {
     echo "Removing files in the build directory.\n".__DIR__."\n";
@@ -33,6 +29,10 @@ if (is_dir($rootDir.'/build')) {
         $todo = ($fileinfo->isDir() ? 'rmdir' : 'unlink');
         $todo($fileinfo->getRealPath());
     }
+}
+
+if (!is_dir($rootDir.'/build')) {
+    mkdir($rootDir.'/build');
 }
 
 if (file_exists($rootDir.'/build/database_test.db')) {
