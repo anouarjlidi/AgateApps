0eb927a6e3b10f0c8e05c8e50ca44cf6dd2243ea
Alex Rock Ancelet <pierstoval@gmail.com>
2015-12-04 16:23:10 +0100

    Still WIP on #1230 's command...

--- src/EsterenMaps/MapsBundle/Command/ChangeCoordinatesSystemCommand.php
+++ src/EsterenMaps/MapsBundle/Command/ChangeCoordinatesSystemCommand.php
@@ -2,8 +2,11 @@
 
 namespace EsterenMaps\MapsBundle\Command;
 
+use Doctrine\ORM\EntityManager;
 use EsterenMaps\MapsBundle\Entity\Maps;
+use Orbitale\Component\DoctrineTools\BaseEntityRepository;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Helper\DialogHelper;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
@@ -22,7 +25,7 @@ class ChangeCoordinatesSystemCommand extends ContainerAwareCommand
                       ."\n".'infinite { x, y } canvas that will allow both negative and positive coordinates.'
                       ."\n\n".'<info>Be very careful</info> because it can change a lot of things, and can also change the map representation!')
             ->addArgument('system', InputArgument::REQUIRED, 'The system you want to use. Can be one of "latlng" or "xy".')
-            ->addArgument('id', InputArgument::OPTIONAL, 'The ID of the map to manage..', null)
+            ->addArgument('id', InputArgument::OPTIONAL, 'The ID of the map to manage.', null)
             ->addOption('--force', null, InputOption::VALUE_NONE, 'By default, this command executes as dry run. This option will update the whole database.')
         ;
     }
@@ -35,15 +38,53 @@ class ChangeCoordinatesSystemCommand extends ContainerAwareCommand
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        /** @var EntityManager $em */
         $em = $this->getContainer()->get('doctrine')->getManager();
 
-        $markersRepo = $em->getRepository('EsterenMapsBundle:Markers');
+        /** @var DialogHelper $dialog */
+        $dialog = $this->getHelperSet()->get('dialog');
 
-        $marker = $markersRepo->find(68);
+        /** @var BaseEntityRepository $mapsRepo */
+        $mapsRepo = $em->getRepository('EsterenMapsBundle:Maps');
+
+        /** @var Maps $map */
+        if ($input->getArgument('id')) {
+            $map = $mapsRepo->find($input->getArgument('id'));
+        } else {
+            $maps = $mapsRepo->findAllRoot('id');
+
+            if (!count($maps)) {
+                $output->writeln('<comment>There is no map in the database.</comment>');
+                return 1;
+            }
+
+            do {
+                $id = $dialog->select(
+                    $output,
+                    'Select a map:',
+                    $maps,
+                    false,
+                    false,
+                    'Invalid id: "%s"'
+                );
+                $map = isset($maps[$id]) ? $maps[$id] : null;
+            } while (!$map);
+        }
+
+        $output->writeln('Taking care of map <info>'.$map.'</info>');
+
+        $marker = $map->getMarkers()[0];
 
         dump($marker->getName());
 
-        <<<JSCODE
+        $xy = $this->transformLatLngToXY($map, (float) $marker->getLatitude(), (float) $marker->getLongitude());
+
+        dump($marker->getLatitude(), $marker->getLongitude());
+
+        dump($xy);
+
+/*
+        $s = <<<JS
 
 var p = document.map._map.project(document.map._markers[68].getLatLng());
 document.map.addMarker(p);
@@ -52,15 +93,6 @@ document.map._map.options.crs.latLngToPoint;
 // Function()
 // Code > EPSG:3857
 // Projection: L.Projection.SphericalMercator
-
-JSCODE;
-
-        $marker = $map->getMarkers()[0];
-
-        $latlng = array((float) $marker->getLatitude(), (float) $marker->getLongitude());
-        dump($latlng);
-
-        $s = <<<JS
 
 $.each(document.map._markers, function(i,e) {
     e._icon.style.outline="solid 3px red";
@@ -147,13 +179,34 @@ var a = {
     z:0
 };
 JS;
-
+*/
         exit;
 
+    }
 
+    /**
+     * Transforms a latlng point to x,y
+     *
+     * @param Maps  $map
+     * @param float $lat
+     * @param float $lng
+     *
+     * @return array
+     */
+    private function transformLatLngToXY(Maps $map, $lat, $lng)
+    {
+        $manager = $this->getContainer()->get('esteren_maps')->getTilesManager();
 
+        $manager->setMap($map);
 
+        $identification = $manager->identifyImage(1);
 
+        $width = $identification->getGlobalWidth();
+        $height = $identification->getGlobalHeight();
 
+        return [
+            'x' => ( $lng + 180 ) * ($width / 360),
+            'y' => ( ( -1 * $lat ) + 90 ) * ( $height / 180 ),
+        ];
     }
 }
