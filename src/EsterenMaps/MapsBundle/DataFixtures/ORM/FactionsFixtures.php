7fb0aef788da796724da5f87b9bbd8fa2d52adf7
Alex Rock Ancelet <pierstoval@gmail.com>
2016-03-01 16:45:24 +0100

    Update Factions fixtures #1278 @0.5

--- src/EsterenMaps/MapsBundle/DataFixtures/ORM/FactionsFixtures.php
+++ src/EsterenMaps/MapsBundle/DataFixtures/ORM/FactionsFixtures.php
@@ -3,95 +3,164 @@
 namespace EsterenMaps\MapsBundle\DataFixtures\ORM;
 
 use CorahnRin\CorahnRinBundle\Entity\Books;
-use Doctrine\Common\DataFixtures\AbstractFixture;
-use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
-use Doctrine\Common\Persistence\ObjectManager;
-use Doctrine\ORM\EntityRepository;
-use Doctrine\ORM\Mapping\ClassMetadata;
 use EsterenMaps\MapsBundle\Entity\Factions;
+use Orbitale\Component\DoctrineTools\AbstractFixture;
 
-class FactionsFixtures extends AbstractFixture implements OrderedFixtureInterface
+class FactionsFixtures extends AbstractFixture
 {
 
     /**
-     * @var ObjectManager
-     */
-    private $manager;
-
-    /**
      * Get the order of this fixture
+     *
      * @return integer
      */
-    function getOrder()
+    public function getOrder()
     {
         return 2;
     }
 
     /**
-     * Load data fixtures with the passed EntityManager
-     *
-     * @param ObjectManager $manager
+     * {@inheritdoc}
      */
-    function load(ObjectManager $manager)
+    protected function getReferencePrefix()
     {
-        $this->manager = $manager;
-
-        $repo = $this->manager->getRepository('EsterenMapsBundle:Factions');
-
-        /** @var Books $book */
-        $book1 = $this->getReference('corahnrin-book-2');
-
-        $this->fixtureObject($repo, 1, $book1, 'Temple', ' Les adeptes de la religion de l\'Unique.');
-        $this->fixtureObject($repo, 2, $book1, 'Magience', 'Les partisans d\'une société régie par des principes académiques & scientifiques.');
-        $this->fixtureObject($repo, 3, $book1, 'Démorthèn', 'Les populations honorant les cultes et traditions ancestrales de Tri-Kazel. ');
-        $this->fixtureObject($repo, 4, $book1, 'Neutre', 'Aucun des grands courants idéologiques ne dominent ce lieu.');
-        $this->fixtureObject($repo, 5, $book1, 'Osags', 'Rattachés au culte demorthèn, les Osags en sont peut-être l\'expression la plus radicale.');
-        $this->fixtureObject($repo, 6, $book1, 'Tarish', 'Peuple nomade par excellence, ses communautés sont en mouvement constant.');
-        //$this->fixtureObject($repo, 7, $book1, 'Loge botaniste', 'L\'école magientiste appliquée aux plantes, herbes et végétaux en général.');
-        //$this->fixtureObject($repo, 8, $book1, 'Loge minéraliste', 'L\'école magientiste appliquée aux pierres, métaux et minéraux en général.');
-        $this->fixtureObject($repo, 9, $book1, 'Noblesse', 'La noblesse défend des valeurs de tradition, hiérarchie, autorité. Certains nobles sont très dévoués à leur souverain, d\'autres estiment qu\'ils sont seuls maîtres sur leurs terres.');
-        $this->fixtureObject($repo, 10, $book1, 'Marchand', 'Les marchands sont essentiellement pragmatiques : partisans de la circulation des biens et des personnes, et de législations qui leur permettent de s\'enrichir. Ils peuvent s\'accommoder d\'un peu de corruption, mais veulent des routes sûres.');
-        $this->fixtureObject($repo, 11, $book1, 'Mixte', 'Plusieurs courants se partagent ou se disputent les lieux sans qu\'il soit véritablement possible de discerner la domination de l\'un d\'eux.');
-        $this->fixtureObject($repo, 12, $book1, 'Rebelles & Criminels', 'Les individus qui dominent sont en rupture avec la société, pour des raisons d\'intérêt ou d\'opinion. Le territoire est une zone grise, où la violence est endémique.');
-        $this->fixtureObject($repo, 13, $book1, 'Chevaliers Ronce', 'Les Ronces sont un ordre très ancien dont les activités sont partagées entre la lutte contre les feondas et la gestion d\'organismes bancaires.');
-        $this->fixtureObject($repo, 14, $book1, 'Chevaliers Hilderins', 'Cet ordre de chevalerie, très lié à la couronne talkéride, est voué à la lutte contre les feondas et peu à l\'occasion assurer la régence d\'un domaine.');
-        $this->fixtureObject($repo, 15, $book1, 'Varigaux', 'Ces guides et messagers arpentent les chemins de la péninsule, assurant le lien entre les communautés les plus isolées. Ils connaissent souvent des raccourcis.');
-
-        $this->manager->flush();
+        return 'esterenmaps-factions';
     }
 
-    public function fixtureObject(EntityRepository $repo, $id, $book, $name, $description)
+    /**
+     * {@inheritdoc}
+     */
+    protected function getEntityClass()
     {
-        $obj       = null;
-        $newObject = false;
-        $addRef    = false;
-        if ($id) {
-            $obj = $repo->find($id);
-            if ($obj) {
-                $addRef = true;
-            } else {
-                $newObject = true;
-            }
-        } else {
-            $newObject = true;
-        }
-        if ($newObject === true) {
-            $obj = new Factions();
-            $obj->setId($id)
-                ->setName($name)
-                ->setDescription($description)
-                ->setBook($book)
-            ;
-            if ($id) {
-                /** @var ClassMetadata $metadata */
-                $metadata = $this->manager->getClassMetaData(get_class($obj));
-                $metadata->setIdGeneratorType(ClassMetadata::GENERATOR_TYPE_NONE);
-            }
-            $this->manager->persist($obj);
-            $addRef = true;
-        }
-        if ($addRef === true && $obj) {
-            $this->addReference('esterenmaps-factions-'.$id, $obj);
-        }
+        return Factions::class;
+    }
+
+    /**
+     * @return array
+     */
+    public function getObjects()
+    {
+        /** @var Books $book2 */
+        $book2 = $this->getReference('corahnrin-book-2');
+
+        return [
+            [
+                'id'          => '1',
+                'book'        => $book2,
+                'name'        => 'Temple',
+                'description' => 'Les adeptes de la religion de l\'Unique.',
+                'createdAt'   => \DateTime::createFromFormat('Y-m-d H:i:s', '2015-07-10 20:49:05'),
+                'updatedAt'   => \DateTime::createFromFormat('Y-m-d H:i:s', '2015-07-10 20:49:05'),
+                'deletedAt'   => null,
+            ],
+            [
+                'id'          => '2',
+                'book'        => $book2,
+                'name'        => 'Magience',
+                'description' => 'Les partisans d\'une société régie par des principes académiques & scientifiques.',
+                'createdAt'   => \DateTime::createFromFormat('Y-m-d H:i:s', '2015-07-10 20:49:05'),
+                'updatedAt'   => \DateTime::createFromFormat('Y-m-d H:i:s', '2015-07-10 20:49:05'),
+                'deletedAt'   => null,
+            ],
+            [
+                'id'          => '3',
+                'book'        => $book2,
+                'name'        => 'Demorthèn',
+                'description' => 'Les populations honorant les cultes et traditions ancestrales de Tri-Kazel. ',
+                'createdAt'   => \DateTime::createFromFormat('Y-m-d H:i:s', '2015-07-10 20:49:05'),
+                'updatedAt'   => \DateTime::createFromFormat('Y-m-d H:i:s', '2015-07-17 22:51:49'),
+                'deletedAt'   => null,
+            ],
+            [
+                'id'          => '4',
+                'book'        => $book2,
+                'name'        => 'Neutre',
+                'description' => 'Aucun des grands courants idéologiques ne dominent ce lieu.',
+                'createdAt'   => \DateTime::createFromFormat('Y-m-d H:i:s', '2015-07-10 20:49:05'),
+                'updatedAt'   => \DateTime::createFromFormat('Y-m-d H:i:s', '2015-07-10 20:49:05'),
+                'deletedAt'   => null,
+            ],
+            [
+                'id'          => '5',
+                'book'        => $book2,
+                'name'        => 'Osags',
+                'description' => 'Rattachés au culte demorthèn, les Osags en sont peut-être l\'expression la plus radicale.',
+                'createdAt'   => \DateTime::createFromFormat('Y-m-d H:i:s', '2015-07-10 20:49:05'),
+                'updatedAt'   => \DateTime::createFromFormat('Y-m-d H:i:s', '2015-07-10 20:49:05'),
+                'deletedAt'   => null,
+            ],
+            [
+                'id'          => '6',
+                'book'        => $book2,
+                'name'        => 'Tarish',
+                'description' => 'Peuple nomade par excellence, ses communautés sont en mouvement constant.',
+                'createdAt'   => \DateTime::createFromFormat('Y-m-d H:i:s', '2015-07-10 20:49:05'),
+                'updatedAt'   => \DateTime::createFromFormat('Y-m-d H:i:s', '2015-07-10 20:49:05'),
+                'deletedAt'   => null,
+            ],
+            [
+                'id'          => '9',
+                'book'        => $book2,
+                'name'        => 'Noblesse',
+                'description' => 'La noblesse défend des valeurs de tradition, hiérarchie, autorité. Certains nobles sont très dévoués à leur souverain, d\'autres estiment qu\'ils sont seuls maîtres sur leurs terres.',
+                'createdAt'   => \DateTime::createFromFormat('Y-m-d H:i:s', '2015-07-10 20:49:05'),
+                'updatedAt'   => \DateTime::createFromFormat('Y-m-d H:i:s', '2015-07-10 20:49:05'),
+                'deletedAt'   => null,
+            ],
+            [
+                'id'          => '10',
+                'book'        => $book2,
+                'name'        => 'Marchand',
+                'description' => 'Les marchands sont essentiellement pragmatiques : partisans de la circulation des biens et des personnes, et de législations qui leur permettent de s\'enrichir. Ils peuvent s\'accommoder d\'un peu de corruption, mais veulent des routes sûres.',
+                'createdAt'   => \DateTime::createFromFormat('Y-m-d H:i:s', '2015-07-10 20:49:05'),
+                'updatedAt'   => \DateTime::createFromFormat('Y-m-d H:i:s', '2015-07-10 20:49:05'),
+                'deletedAt'   => null,
+            ],
+            [
+                'id'          => '11',
+                'book'        => $book2,
+                'name'        => 'Mixte',
+                'description' => 'Plusieurs courants se partagent ou se disputent les lieux sans qu\'il soit véritablement possible de discerner la domination de l\'un d\'eux.',
+                'createdAt'   => \DateTime::createFromFormat('Y-m-d H:i:s', '2015-07-10 20:49:05'),
+                'updatedAt'   => \DateTime::createFromFormat('Y-m-d H:i:s', '2015-07-10 20:49:05'),
+                'deletedAt'   => null,
+            ],
+            [
+                'id'          => '12',
+                'book'        => $book2,
+                'name'        => 'Rebelles & Criminels',
+                'description' => 'Les individus qui dominent sont en rupture avec la société, pour des raisons d\'intérêt ou d\'opinion. Le territoire est une zone grise, où la violence est endémique.',
+                'createdAt'   => \DateTime::createFromFormat('Y-m-d H:i:s', '2015-07-10 20:49:05'),
+                'updatedAt'   => \DateTime::createFromFormat('Y-m-d H:i:s', '2015-07-10 20:49:05'),
+                'deletedAt'   => null,
+            ],
+            [
+                'id'          => '13',
+                'book'        => $book2,
+                'name'        => 'Chevaliers Ronces',
+                'description' => 'Les Ronces sont un ordre très ancien dont les activités sont partagées entre la lutte contre les feondas et la gestion d\'organismes bancaires.',
+                'createdAt'   => \DateTime::createFromFormat('Y-m-d H:i:s', '2015-07-10 20:49:05'),
+                'updatedAt'   => \DateTime::createFromFormat('Y-m-d H:i:s', '2015-07-10 20:49:22'),
+                'deletedAt'   => null,
+            ],
+            [
+                'id'          => '14',
+                'book'        => $book2,
+                'name'        => 'Chevaliers Hilderins',
+                'description' => 'Cet ordre de chevalerie, très lié à la couronne talkéride, est voué à la lutte contre les feondas et peu à l\'occasion assurer la régence d\'un domaine.',
+                'createdAt'   => \DateTime::createFromFormat('Y-m-d H:i:s', '2015-07-10 20:49:05'),
+                'updatedAt'   => \DateTime::createFromFormat('Y-m-d H:i:s', '2015-07-10 20:49:05'),
+                'deletedAt'   => null,
+            ],
+            [
+                'id'          => '15',
+                'book'        => $book2,
+                'name'        => 'Varigaux',
+                'description' => 'Ces guides et messagers arpentent les chemins de la péninsule, assurant le lien entre les communautés les plus isolées. Ils connaissent souvent des raccourcis.',
+                'createdAt'   => \DateTime::createFromFormat('Y-m-d H:i:s', '2015-07-10 20:49:05'),
+                'updatedAt'   => \DateTime::createFromFormat('Y-m-d H:i:s', '2015-07-10 20:49:05'),
+                'deletedAt'   => null,
+            ],
+        ];
     }
 }
