5006cd0e848fabd788be03ef09881bc106543ed0
Alex Rock Ancelet <pierstoval@gmail.com>
2016-03-08 11:38:13 +0100

    Minor code style in factions fixture #1278 @0.05

--- src/EsterenMaps/MapsBundle/DataFixtures/ORM/FactionsFixtures.php
+++ src/EsterenMaps/MapsBundle/DataFixtures/ORM/FactionsFixtures.php
@@ -45,7 +45,7 @@ class FactionsFixtures extends AbstractFixture
 
         return [
             [
-                'id'          => '1',
+                'id'          => 1,
                 'book'        => $book2,
                 'name'        => 'Temple',
                 'description' => 'Les adeptes de la religion de l\'Unique.',
@@ -54,7 +54,7 @@ class FactionsFixtures extends AbstractFixture
                 'deletedAt'   => null,
             ],
             [
-                'id'          => '2',
+                'id'          => 2,
                 'book'        => $book2,
                 'name'        => 'Magience',
                 'description' => 'Les partisans d\'une société régie par des principes académiques & scientifiques.',
@@ -63,7 +63,7 @@ class FactionsFixtures extends AbstractFixture
                 'deletedAt'   => null,
             ],
             [
-                'id'          => '3',
+                'id'          => 3,
                 'book'        => $book2,
                 'name'        => 'Demorthèn',
                 'description' => 'Les populations honorant les cultes et traditions ancestrales de Tri-Kazel. ',
@@ -72,7 +72,7 @@ class FactionsFixtures extends AbstractFixture
                 'deletedAt'   => null,
             ],
             [
-                'id'          => '4',
+                'id'          => 4,
                 'book'        => $book2,
                 'name'        => 'Neutre',
                 'description' => 'Aucun des grands courants idéologiques ne dominent ce lieu.',
@@ -81,7 +81,7 @@ class FactionsFixtures extends AbstractFixture
                 'deletedAt'   => null,
             ],
             [
-                'id'          => '5',
+                'id'          => 5,
                 'book'        => $book2,
                 'name'        => 'Osags',
                 'description' => 'Rattachés au culte demorthèn, les Osags en sont peut-être l\'expression la plus radicale.',
@@ -90,7 +90,7 @@ class FactionsFixtures extends AbstractFixture
                 'deletedAt'   => null,
             ],
             [
-                'id'          => '6',
+                'id'          => 6,
                 'book'        => $book2,
                 'name'        => 'Tarish',
                 'description' => 'Peuple nomade par excellence, ses communautés sont en mouvement constant.',
@@ -99,7 +99,7 @@ class FactionsFixtures extends AbstractFixture
                 'deletedAt'   => null,
             ],
             [
-                'id'          => '9',
+                'id'          => 9,
                 'book'        => $book2,
                 'name'        => 'Noblesse',
                 'description' => 'La noblesse défend des valeurs de tradition, hiérarchie, autorité. Certains nobles sont très dévoués à leur souverain, d\'autres estiment qu\'ils sont seuls maîtres sur leurs terres.',
@@ -108,7 +108,7 @@ class FactionsFixtures extends AbstractFixture
                 'deletedAt'   => null,
             ],
             [
-                'id'          => '10',
+                'id'          => 10,
                 'book'        => $book2,
                 'name'        => 'Marchand',
                 'description' => 'Les marchands sont essentiellement pragmatiques : partisans de la circulation des biens et des personnes, et de législations qui leur permettent de s\'enrichir. Ils peuvent s\'accommoder d\'un peu de corruption, mais veulent des routes sûres.',
@@ -117,7 +117,7 @@ class FactionsFixtures extends AbstractFixture
                 'deletedAt'   => null,
             ],
             [
-                'id'          => '11',
+                'id'          => 11,
                 'book'        => $book2,
                 'name'        => 'Mixte',
                 'description' => 'Plusieurs courants se partagent ou se disputent les lieux sans qu\'il soit véritablement possible de discerner la domination de l\'un d\'eux.',
@@ -126,7 +126,7 @@ class FactionsFixtures extends AbstractFixture
                 'deletedAt'   => null,
             ],
             [
-                'id'          => '12',
+                'id'          => 12,
                 'book'        => $book2,
                 'name'        => 'Rebelles & Criminels',
                 'description' => 'Les individus qui dominent sont en rupture avec la société, pour des raisons d\'intérêt ou d\'opinion. Le territoire est une zone grise, où la violence est endémique.',
@@ -135,7 +135,7 @@ class FactionsFixtures extends AbstractFixture
                 'deletedAt'   => null,
             ],
             [
-                'id'          => '13',
+                'id'          => 13,
                 'book'        => $book2,
                 'name'        => 'Chevaliers Ronces',
                 'description' => 'Les Ronces sont un ordre très ancien dont les activités sont partagées entre la lutte contre les feondas et la gestion d\'organismes bancaires.',
@@ -144,7 +144,7 @@ class FactionsFixtures extends AbstractFixture
                 'deletedAt'   => null,
             ],
             [
-                'id'          => '14',
+                'id'          => 14,
                 'book'        => $book2,
                 'name'        => 'Chevaliers Hilderins',
                 'description' => 'Cet ordre de chevalerie, très lié à la couronne talkéride, est voué à la lutte contre les feondas et peu à l\'occasion assurer la régence d\'un domaine.',
@@ -153,7 +153,7 @@ class FactionsFixtures extends AbstractFixture
                 'deletedAt'   => null,
             ],
             [
-                'id'          => '15',
+                'id'          => 15,
                 'book'        => $book2,
                 'name'        => 'Varigaux',
                 'description' => 'Ces guides et messagers arpentent les chemins de la péninsule, assurant le lien entre les communautés les plus isolées. Ils connaissent souvent des raccourcis.',
