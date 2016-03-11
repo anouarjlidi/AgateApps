54cdf396a3fe1a6f4d848c69bee66d3ee961837c
Alex Rock Ancelet <pierstoval@gmail.com>
2016-03-11 10:37:44 +0100

    Fix tests for the portal #939 @0.75

--- tests/phpunit/Portal/PortalControllerTest.php
+++ tests/phpunit/Portal/PortalControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Portal;
 
+use Doctrine\ORM\EntityManager;
 use Orbitale\Bundle\CmsBundle\Entity\Page;
 use Tests\WebTestCase;
 
@@ -22,27 +23,67 @@ class PortalControllerTest extends WebTestCase
     /**
      * @see GeneratorController::indexAction
      */
-    public function testIndexWithOneHomePage()
+    public function testIndexWithHomepage()
     {
-        $client = static::getClient('portal.esteren.dev');
+        $client = static::getClient('www.esteren.dev');
 
         $page = new Page();
         $page
             ->setHomepage(true)
-            ->setHost('portal.esteren.dev')
+            ->setHost('www.esteren.dev')
+            ->setTitle('Homepage test')
+            ->setContent('<h2>This tag is only here for testing</h2>')
+            ->setSlug('homepage-test')
+            ->setLocale('fr')
+            ->setEnabled(true)
+        ;
+
+        /** @var EntityManager $em */
+        $em = static::$kernel->getContainer()->get('doctrine')->getManager();
+        $em->persist($page);
+        $em->flush();
+
+        $client->restart();
+
+        $crawler = $client->request('GET', '/fr/');
+        $this->assertEquals(200, $client->getResponse()->getStatusCode(), $client->getResponse()->getContent());
+
+        $this->assertEquals($page->getTitle(), trim($crawler->filter('#content section article h1')->html()));
+        $this->assertContains($page->getContent(), trim($crawler->filter('#content section article')->html()));
+    }
+
+    /**
+     * @see GeneratorController::indexAction
+     */
+    public function testIndexWithOnePage()
+    {
+        $client = static::getClient('www.esteren.dev');
+
+        $page = new Page();
+        $page
+            ->setHost('www.esteren.dev')
             ->setTitle('Page test')
             ->setContent('<h2>This tag is only here for testing</h2>')
             ->setSlug('page-test')
             ->setLocale('fr')
         ;
 
+        /** @var EntityManager $em */
         $em = static::$kernel->getContainer()->get('doctrine')->getManager();
         $em->persist($page);
         $em->flush();
 
-        $crawler = $client->request('GET', '/fr/page-test');
+        // First, we check that "enabled" is false by default
+        $client->request('GET', '/fr/page-test');
+        $this->assertEquals(404, $client->getResponse()->getStatusCode());
 
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $page->setEnabled(true);
+        $em->flush($page);
+
+        $client->restart();
+
+        $crawler = $client->request('GET', '/fr/page-test');
+        $this->assertEquals(200, $client->getResponse()->getStatusCode(), $client->getResponse()->getContent());
 
         $this->assertEquals($page->getTitle(), trim($crawler->filter('#content section article h1')->html()));
         $this->assertContains($page->getContent(), trim($crawler->filter('#content section article')->html()));
