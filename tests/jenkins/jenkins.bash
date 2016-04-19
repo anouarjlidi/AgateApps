dc754b76956b1c15919b8830baf5ee2f0af50141
Alex Rock Ancelet <pierstoval@gmail.com>
2016-04-19 16:07:02 +0200

    Update jenkins and phpunit config

--- tests/jenkins/jenkins.bash
+++ tests/jenkins/jenkins.bash
@@ -7,12 +7,15 @@ pwd
 curl -sS https://getcomposer.org/installer | php
 
 cp tests/jenkins/parameters.yml app/config/parameters.yml
+cp tests/jenkins/phpunit.xml tests/phpunit.xml
 
 export SYMFONY_ENV='test'
 
 # Install dependencies
 php composer.phar install -o
 
+echo "Execute tests"
+
 # Execute tests
-vendor/bin/phpunit -c tests --coverage-text --coverage-clover build/logs/clover.xml
+vendor/bin/phpunit -c tests/phpunit.xml --coverage-text --coverage-clover build/logs/clover.xml
 
