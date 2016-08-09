27454e0e29a0b5c4c8ec6d2ad08d7dadb0d1af7d
Alex Rock Ancelet <pierstoval@gmail.com>
2016-08-09 17:28:01 +0200

    Update bootstrap file so database reset can be faster

--- tests/phpunit/bootstrap.php
+++ tests/phpunit/bootstrap.php
@@ -26,18 +26,22 @@ if (getenv('TESTS_NO_DB')) {
 $fs = new Filesystem();
 
 // Remove build dir files
-if (is_dir($rootDir.'/build')) {
-    echo "Removing files in the build directory.\n".__DIR__."\n";
-    $fs->remove($rootDir.'/build');
+if (!is_dir($rootDir.'/build')) {
+    $fs->mkdir($rootDir.'/build');
 }
-
-$fs->mkdir($rootDir.'/build');
 
 if ($fs->exists($rootDir.'/build/database_test.db')) {
     $fs->remove($rootDir.'/build/database_test.db');
+}
+
+if ($fs->exists($rootDir.'/build/database_reference.db')) {
+    $fs->copy($rootDir.'/build/database_reference.db', $rootDir.'/build/database_test.db');
+    return;
 }
 
 system('php '.$rootDir.'/bin/console cache:clear --no-warmup');
 system('php '.$rootDir.'/bin/console doctrine:database:create');
 system('php '.$rootDir.'/bin/console doctrine:schema:create');
 system('php '.$rootDir.'/bin/console doctrine:fixtures:load --append');
+
+$fs->copy($rootDir.'/build/database_test.db', $rootDir.'/build/database_reference.db');
