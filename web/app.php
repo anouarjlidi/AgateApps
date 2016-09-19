b5a97157d0611550d383b918003c1cb1231f25fa
Alex Rock Ancelet <pierstoval@gmail.com>
2016-09-19 16:37:05 +0200

    Update app/app_dev config to have one single end point

--- web/app.php
+++ web/app.php
@@ -1,19 +1,33 @@
 <?php
 
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Debug\Debug;
+
+umask(0002);
+
+$environment = (getenv('APP_ENVIRONMENT') !== false) ? getenv('APP_ENVIRONMENT') : 'dev';
+$debug       = (getenv('APP_DEBUG') !== false) ? (bool) getenv('APP_DEBUG') : true;
 
 /**
  * @var Composer\Autoload\ClassLoader
  */
 $loader = require __DIR__.'/../app/autoload.php';
-include_once __DIR__.'/../var/bootstrap.php.cache';
 
-$kernel = new AppKernel('prod', false);
+if (true === $debug) {
+    Debug::enable();
+} else {
+    include_once __DIR__.'/../var/bootstrap.php.cache';
+}
+
+$kernel = new AppKernel($environment, $debug);
 $kernel->loadClassCache();
-$kernel = new AppCache($kernel);
 
-// When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
-Request::enableHttpMethodParameterOverride();
+if (false === $debug && 'prod' === $environment) {
+    $kernel = new AppCache($kernel);
+    // When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
+    Request::enableHttpMethodParameterOverride();
+}
+
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
