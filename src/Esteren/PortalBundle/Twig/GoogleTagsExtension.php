24500ceda0244fdf3fc76c9d8e34ae152508e99f
Alex Rock Ancelet <pierstoval@gmail.com>
2016-09-19 16:19:25 +0200

    Add google tags / analytics system #1811 @1.25

---
+++ src/Esteren/PortalBundle/Twig/GoogleTagsExtension.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Esteren\PortalBundle\Twig;
+
+class GoogleTagsExtension extends \Twig_Extension
+{
+    /**
+     * @var array
+     */
+    private $googleTags;
+
+    /**
+     * @var bool
+     */
+    private $debug;
+
+    public function __construct(array $googleTags, $debug)
+    {
+        $this->googleTags = $googleTags;
+        $this->debug      = $debug;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getFunctions()
+    {
+        return [
+            new \Twig_SimpleFunction('get_gtm', [$this, 'getGoogleTagManager']),
+            new \Twig_SimpleFunction('get_ga', [$this, 'getGoogleAnalytics']),
+        ];
+    }
+
+    /**
+     * @return string
+     */
+    public function getGoogleTagManager()
+    {
+        return $this->googleTags['tag_manager'];
+    }
+
+    /**
+     * @return string
+     */
+    public function getGoogleAnalytics()
+    {
+        return $this->googleTags['analytics'];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'google_tags';
+    }
+}
