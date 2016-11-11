f692e93a3b195b932b4f4eda61af149586fd379c
Alex Rock Ancelet <pierstoval@gmail.com>
2016-11-11 20:21:09 +0100

    Fix portal tests #939 @0.1

--- src/Esteren/PortalBundle/Tests/PortalControllerTest.php
+++ src/Esteren/PortalBundle/Tests/PortalControllerTest.php
@@ -2,8 +2,6 @@
 
 namespace Esteren\PortalBundle\Tests;
 
-use Doctrine\ORM\EntityManager;
-use Esteren\PortalBundle\Entity\Page;
 use Tests\WebTestCase;
 
 class PortalControllerTest extends WebTestCase
@@ -15,25 +13,13 @@ class PortalControllerTest extends WebTestCase
         $client = $this->getClient('portal.esteren.dev');
 
         $crawler = $client->request('GET', '/fr/');
-        static::assertEquals(200, $client->getResponse()->getStatusCode(), $client->getResponse()->getContent());
-        static::assertEquals('Homepage', trim($crawler->filter('#content h1')->html()));
-        static::assertContains('This this a default home page.', trim($crawler->filter('#content')->html()));
-    }
 
-    /**
-     * @see GeneratorController::indexAction
-     */
-    public function testIndexWithOnePage()
-    {
-        parent::resetDatabase();
+        // Ensures that portal homepage is managed in a controller and not in the CMS
+        static::assertEquals('portal_home', $client->getRequest()->attributes->get('_route'));
 
-        $client = $this->getClient('portal.esteren.dev');
-
-        $crawler = $client->request('GET', '/fr/page-test');
         static::assertEquals(200, $client->getResponse()->getStatusCode());
 
-        static::assertEquals('Static page', trim($crawler->filter('#content h1')->html()));
-        static::assertContains('This this a default static page.', trim($crawler->filter('#content')->html()));
+        // Check <h1> content only, this will be our "regression point" for homepage (now that it's static and no more in the CMS)
+        static::assertEquals('Best Cartography', trim($crawler->filter('#content h1')->text()));
     }
-
 }
