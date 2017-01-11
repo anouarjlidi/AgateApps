6db756981dacc2e477e8a53b64571d4a5a03d66b
Alex Rock Ancelet <pierstoval@gmail.com>
2017-01-11 09:57:00 +0100

    Add more verbosity to CI script #939 @0.05

--- tests/ci/ci.bash
+++ tests/ci/ci.bash
@@ -9,32 +9,42 @@ DIR=`readlink -m "${DIR}/../../"`
 # Change directory to root directory so all commands are executed from there
 cd ${DIR} || exit 100
 
-echo "Working directory:"
+echoPrefix="[CI SCRIPT] -"
+
+echo ""
+echo "$echoPrefix Working directory:"
 pwd
 
-echo "Installing composer"
+echo ""
+echo "$echoPrefix Installing composer"
 curl -sS https://getcomposer.org/installer | php || exit 110
 
-echo "Backup any existing parameters file."
+echo ""
+echo "$echoPrefix Backup any existing parameters file."
 if [ -f app/config/parameters.yml ]; then
     if ! grep -q "# CI file" "app/config/parameters.yml"; then
-        echo "Backing up parameters.yml file"
+        echo ""
+echo "$echoPrefix Backing up parameters.yml file"
         mv app/config/parameters.yml app/config/parameters.yml.backup
     fi
 fi
 
-echo "Update parameters and phpunit file for CI"
+echo ""
+echo "$echoPrefix Update parameters and phpunit file for CI"
 cp tests/ci/parameters.yml app/config/parameters.yml || exit 120
 
-echo "Setup environment variables"
+echo ""
+echo "$echoPrefix Setup environment variables"
 export SYMFONY_ENV='test'
 export SYMFONY_DEBUG=1
 export RECREATE_DB=1
 
-echo "Install Composer dependencies"
+echo ""
+echo "$echoPrefix Install Composer dependencies"
 php composer.phar install -o --no-interaction --no-scripts || exit 130
 
-echo "Testing environment capabilities and Symfony requirements"
+echo ""
+echo "$echoPrefix Testing environment capabilities and Symfony requirements"
 php bin/symfony_requirements || exit 140
 
 if [[ -z "$PHPUNIT_PARAMETERS" ]]; then
@@ -47,10 +57,12 @@ else
     phpunit_script="./vendor/bin/simple-phpunit"
 fi
 
-echo "Execute tests"
+echo ""
+echo "$echoPrefix Execute tests"
 ${phpunit_script} ${PHPUNIT_PARAMETERS}
 
 if [ -f app/config/parameters.yml.backup ]; then
-    echo "Retrieve backed up parameters file"
+    echo ""
+    echo "$echoPrefix Retrieve backed up parameters file"
     cp app/config/parameters.yml.backup app/config/parameters.yml
 fi
