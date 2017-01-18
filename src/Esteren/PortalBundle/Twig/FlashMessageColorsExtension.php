cc6151b91ea1f476bb3af23995ada9c3dff897b0
Alex Rock Ancelet <pierstoval@gmail.com>
2017-01-18 22:41:27 +0100

    Added twig extension to manage materialized colors for flash messages #879 @0.25

---
+++ src/Esteren/PortalBundle/Twig/FlashMessageColorsExtension.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Esteren\PortalBundle\Twig;
+
+class FlashMessageColorsExtension extends \Twig_Extension
+{
+    public function getFunctions()
+    {
+        return [
+            new \Twig_SimpleFunction('get_flash_class', [$this, 'getFlashClass']),
+        ];
+    }
+
+    public function getFlashClass($initialClass)
+    {
+        switch ($initialClass) {
+            case 'alert':
+            case 'error':
+            case 'danger':
+                return 'red lighten-4';
+            case 'warning':
+                return 'orange lighten-3';
+            default:
+                return '';
+        }
+    }
+}
