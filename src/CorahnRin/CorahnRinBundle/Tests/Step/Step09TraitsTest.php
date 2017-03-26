b46eb5084ed249a3480bc23c308ddf196e1185f8
Alex Rock Ancelet <pierstoval@gmail.com>
2017-03-26 23:36:24 +0200

    Add all next generator steps to be added to test suite #1953

---
+++ src/CorahnRin/CorahnRinBundle/Tests/Step/Step09TraitsTest.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * This file is part of the corahn_rin package.
+ *
+ * (c) Alexandre Rock Ancelet <alex@orbitale.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace CorahnRin\CorahnRinBundle\Tests\Step;
+
+class Step09TraitsTest extends AbstractStepTest
+{
+    public function testStep()
+    {
+        static::markTestIncomplete();
+    }
+}
