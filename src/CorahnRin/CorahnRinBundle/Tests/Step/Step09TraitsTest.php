1bc94da87c354c888f4b390292e3bca932a48c83
Alex Rock Ancelet <pierstoval@gmail.com>
2017-03-31 13:53:01 +0200

    Add tests for step 9 #1953 @0.5

--- src/CorahnRin/CorahnRinBundle/Tests/Step/Step09TraitsTest.php
+++ src/CorahnRin/CorahnRinBundle/Tests/Step/Step09TraitsTest.php
@@ -13,8 +13,53 @@ namespace CorahnRin\CorahnRinBundle\Tests\Step;
 
 class Step09TraitsTest extends AbstractStepTest
 {
-    public function testStep()
+    public function testWaysDependency()
     {
-        static::markTestIncomplete();
+        $client = $this->getClient();
+
+        $client->request('GET', '/fr/character/generate/'.$this->getStepName());
+
+        static::assertSame(302, $client->getResponse()->getStatusCode());
+        static::assertTrue($client->getResponse()->isRedirect('/fr/character/generate'));
+    }
+
+    public function testValidSetbacks()
+    {
+        $result = $this->submitAction([
+            '08_ways' => [
+                1 => 1,
+                2 => 2,
+                3 => 3,
+                4 => 4,
+                5 => 5,
+            ]
+        ], $values = [
+            'quality' => 6,
+            'flaw' => 64,
+        ]);
+
+        static::assertSame(302, $result->getResponse()->getStatusCode());
+        static::assertTrue($result->getResponse()->isRedirect('/fr/character/generate/10_orientation'));
+        static::assertSame($values, $result->getSession()->get('character')[$this->getStepName()]);
+    }
+
+    public function testInValidSetbacks()
+    {
+        $result = $this->submitAction([
+            '08_ways' => [
+                1 => 1,
+                2 => 2,
+                3 => 3,
+                4 => 4,
+                5 => 5,
+            ]
+        ], $values = [
+            'quality' => 0,
+            'flaw' => 0,
+        ]);
+
+        static::assertSame(200, $result->getResponse()->getStatusCode());
+        static::assertSame(1, $result->getCrawler()->filter('#flash-messages > .card-panel.error')->count());
+        static::assertEquals('Les traits de caractère choisis sont incorrects.', trim($result->getCrawler()->filter('#flash-messages > .card-panel.error')->text()));
     }
 }
