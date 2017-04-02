2c5d0882a1096d3ecae9dd31292feec2133d83c3
Alex Rock Ancelet <pierstoval@gmail.com>
2017-04-02 21:58:15 +0200

    Add tests for step 12 #1953 @0.15

--- src/CorahnRin/CorahnRinBundle/Tests/Step/Step12MentalDisorderTest.php
+++ src/CorahnRin/CorahnRinBundle/Tests/Step/Step12MentalDisorderTest.php
@@ -13,8 +13,74 @@ namespace CorahnRin\CorahnRinBundle\Tests\Step;
 
 class Step12MentalDisorderTest extends AbstractStepTest
 {
-    public function testStep()
+    public function testWaysDependency()
     {
-        static::markTestIncomplete();
+        $client = $this->getClient();
+
+        $client->request('GET', '/fr/character/generate/'.$this->getStepName());
+
+        static::assertSame(302, $client->getResponse()->getStatusCode());
+        static::assertTrue($client->getResponse()->isRedirect('/fr/character/generate'));
+    }
+
+    public function testValidMentalDisorder()
+    {
+        $result = $this->submitAction([
+            '08_ways' => [
+                1 => 5,
+                2 => 4,
+                3 => 3,
+                4 => 2,
+                5 => 1,
+            ]
+        ], [
+            'gen-div-choice' => 1,
+        ]);
+
+        static::assertSame(302, $result->getResponse()->getStatusCode());
+        static::assertTrue($result->getResponse()->isRedirect('/fr/character/generate/13_primary_domains'));
+        static::assertSame(1, $result->getSession()->get('character')[$this->getStepName()]);
+    }
+
+    public function testEmptyValue()
+    {
+        $result = $this->submitAction([
+            '08_ways' => [
+                1 => 5,
+                2 => 4,
+                3 => 3,
+                4 => 2,
+                5 => 1,
+            ]
+        ], [
+            'gen-div-choice' => 0,
+        ]);
+
+        $crawler = $result->getCrawler();
+
+        static::assertSame(200, $result->getResponse()->getStatusCode());
+        static::assertSame(1, $crawler->filter('#flash-messages > .card-panel.error')->count());
+        static::assertEquals('Veuillez choisir un désordre mental.', trim($crawler->filter('#flash-messages > .card-panel.error')->text()));
+    }
+
+    public function testInvalidMentalDisorder()
+    {
+        $result = $this->submitAction([
+            '08_ways' => [
+                1 => 5,
+                2 => 4,
+                3 => 3,
+                4 => 2,
+                5 => 1,
+            ]
+        ], [
+            'gen-div-choice' => 999999,
+        ]);
+
+        $crawler = $result->getCrawler();
+
+        static::assertSame(200, $result->getResponse()->getStatusCode());
+        static::assertSame(1, $crawler->filter('#flash-messages > .card-panel.error')->count());
+        static::assertEquals('Le désordre mental choisi n\'existe pas.', trim($crawler->filter('#flash-messages > .card-panel.error')->text()));
     }
 }
