7d2728552b49427fbd6337029ccbf39caa38a66c
Alex Rock Ancelet <pierstoval@gmail.com>
2017-04-25 00:05:35 +0200

    Add first tests for step 13 #1953 @0.05

--- src/CorahnRin/CorahnRinBundle/Tests/Step/Step13PrimaryDomainsTest.php
+++ src/CorahnRin/CorahnRinBundle/Tests/Step/Step13PrimaryDomainsTest.php
@@ -11,10 +11,71 @@
 
 namespace CorahnRin\CorahnRinBundle\Tests\Step;
 
+use Symfony\Bundle\FrameworkBundle\Client;
+
 class Step13PrimaryDomainsTest extends AbstractStepTest
 {
-    public function testStep()
+    /**
+     * @dataProvider provideInvalidDependencies
+     */
+    public function testStepDependency($dependencies)
     {
-        static::markTestIncomplete();
+        $client = parent::getClient();
+
+        $session = $client->getContainer()->get('session');
+        $session->set('character', $dependencies); // Varigal
+        $session->save();
+
+        $client->request('GET', '/fr/character/generate/'.$this->getStepName());
+
+        static::assertSame(302, $client->getResponse()->getStatusCode());
+        static::assertTrue($client->getResponse()->isRedirect('/fr/character/generate'));
+    }
+
+    public function provideInvalidDependencies()
+    {
+        return [
+            [[]],
+            [['02_job' => 1]],
+            [['08_ways' => [1, 2, 3, 4, 5]]],
+            [['11_advantages' => []]],
+            [['02_job' => 1, '08_ways' => [1, 2, 3, 4, 5]]],
+            [['11_advantages' => [], '08_ways' => [1, 2, 3, 4, 5]]],
+        ];
+    }
+
+    public function testVarigal()
+    {
+        $client = $this->getStepClient(18); // Varigal id in fixtures
+
+        $crawler = $client->request('GET', '/fr/character/generate/'.$this->getStepName());
+
+        static::assertCount(2, $crawler->filter('[data-change="3"].domain-change'));
+    }
+
+    /**
+     * @param int   $jobId
+     * @param int[] $ways
+     * @param int[] $advantages
+     * @param int[] $disadvantages
+     *
+     * @return Client
+     */
+    protected function getStepClient($jobId = 1, array $ways = [1, 2, 3, 4, 5], array $advantages = [], array $disadvantages = [])
+    {
+        $client = $this->getClient();
+
+        $session = $client->getContainer()->get('session');
+        $session->set('character', [
+            '02_job' => $jobId,
+            '08_ways' => $ways,
+            '11_advantages' => [
+                'advantages' => $advantages,
+                'disadvantages' => $disadvantages,
+            ],
+        ]);
+        $session->save();
+
+        return $client;
     }
 }
