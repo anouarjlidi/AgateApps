8066d715240d286acd15ac6b5c220ef0d8cca6f7
Alex Rock Ancelet <pierstoval@gmail.com>
2017-05-22 00:06:44 +0200

    Enhance frontend build script

--- bin/build_frontend.js
+++ bin/build_frontend.js
@@ -1,9 +1,35 @@
-var http = require('http');
-var path = require('path');
-var spawn = require('child_process').spawn;
+const http = require('http');
+const path = require('path');
+const spawn = require('child_process').spawn;
+
+const port = 9999;
+
+console.log('Listening to port '+9999);
 
 http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    spawn('node', [path.dirname(__dirname)+'/node_modules/gulp4/bin/gulp.js', 'dump', '--prod'], { stdio: 'inherit' });
-    res.end('');
-}).listen(9999);
+    if (req.url !== '/') {
+        res.writeHead(400);
+        res.end();
+
+        return;
+    }
+
+    // First, update all project
+    let cmd = spawn('git', ['pull', 'origin', 'master'], { stdio: 'inherit' });
+
+    cmd.on('close', () => {
+        // Then dump assets
+        let dump = spawn('node', [path.dirname(__dirname)+'/node_modules/gulp4/bin/gulp.js', 'dump', '--prod'], { stdio: 'inherit' });
+
+        dump.on('close', (code) => {
+            if (0 !== code) {
+                res.writeHead(500, {'Content-Type': 'text/plain'});
+            } else {
+                res.writeHead(200, {'Content-Type': 'text/plain'});
+            }
+
+            res.end(code.toString());
+        });
+    });
+
+}).listen(port);
