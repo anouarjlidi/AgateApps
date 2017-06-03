83cae9ba42a9829a55ca60356da41a60b871b8ed
Alex Rock Ancelet <pierstoval@gmail.com>
2017-06-03 14:29:34 +0200

    Fix a missing errored fixture

--- src/CorahnRin/CorahnRinBundle/DataFixtures/ORM/CombatArtsFixtures.php
+++ src/CorahnRin/CorahnRinBundle/DataFixtures/ORM/CombatArtsFixtures.php
@@ -50,16 +50,16 @@ class CombatArtsFixtures extends AbstractFixture implements OrderedFixtureInterf
         /** @var Books $book */
         $book = $this->getReference('corahnrin-book-2');
 
-        $this->fixtureObject($repo, 1, 1, 0, 'Attaque sournoise', 'Si le personnage tient un ennemi en embuscade et qu\'il touche sa cible, il inflige +5 dégâts. Il ne doit utiliser qu\'une arme courte (dague, épée courte, couteau...)', '2014-04-09 08:56:43', '2014-04-09 08:56:43', null, $book);
-        $this->fixtureObject($repo, 2, 1, 1, 'Combat à 2 armes', 'La deuxième arme doit être courte ou de petite taille. Il ajoute +2 au bonus de son attitude de combat. Le bonus est au choix s\'il est en atittude standard. Sinon, le bonus s\'ajoute à l\'attaque en posture offensive, et en défense en posture défensive.', '2014-04-09 08:56:43', '2014-04-09 08:56:43', null, $book);
-        $this->fixtureObject($repo, 3, 1, 0, 'Parade', 'En attitude Standard, Défensive ou Rapide, et en bénéficiant de l\'initiative, le personnage peut effectuer un jet d\'attaque contre une cible. Si le résultat est supérieur au jet d\'attaque de la cible, le personnage pare l\'attaque. Sinon, l\'attaque est résolue normalement', '2014-04-09 08:56:43', '2014-04-09 08:56:43', null, $book);
-        $this->fixtureObject($repo, 4, 0, 1, 'Archerie', 'Niveau 5 requis en Tir et Lancer. Le personnage agit en dernier pendant le tour où il utilise cet art, mais il dispose d\'un bonus de 2 en Tir & Lancer pour son attaque, et annule tout malus sur une cible en mouvement.', '2014-04-09 08:56:43', '2014-04-09 08:56:43', null, $book);
-        $this->fixtureObject($repo, 5, 1, 0, 'Cavalerie', 'Discipline Equitation nécessaire. Si le personnage sur une monture subit moins de 5 dégâts, il n\'est pas désarçonné. Le personnage peut également charger en premier round d\'un combat, et bénéficier d\'un bonus de +3 à son jet (+4 avec une lance)', '2014-04-09 08:56:43', '2014-04-09 08:56:43', null, $book);
+        $this->fixtureObject($repo, 1, 1, 0, 'Attaque sournoise', 'Si le personnage tient un ennemi en embuscade et qu\'il touche sa cible, il inflige +5 dégâts. Il ne doit utiliser qu\'une arme courte (dague, épée courte, couteau...)', $book);
+        $this->fixtureObject($repo, 2, 1, 1, 'Combat à 2 armes', 'La deuxième arme doit être courte ou de petite taille. Il ajoute +2 au bonus de son attitude de combat. Le bonus est au choix s\'il est en atittude standard. Sinon, le bonus s\'ajoute à l\'attaque en posture offensive, et en défense en posture défensive.', $book);
+        $this->fixtureObject($repo, 3, 1, 0, 'Parade', 'En attitude Standard, Défensive ou Rapide, et en bénéficiant de l\'initiative, le personnage peut effectuer un jet d\'attaque contre une cible. Si le résultat est supérieur au jet d\'attaque de la cible, le personnage pare l\'attaque. Sinon, l\'attaque est résolue normalement', $book);
+        $this->fixtureObject($repo, 4, 0, 1, 'Archerie', 'Niveau 5 requis en Tir et Lancer. Le personnage agit en dernier pendant le tour où il utilise cet art, mais il dispose d\'un bonus de 2 en Tir & Lancer pour son attaque, et annule tout malus sur une cible en mouvement.', $book);
+        $this->fixtureObject($repo, 5, 1, 0, 'Cavalerie', 'Discipline Equitation nécessaire. Si le personnage sur une monture subit moins de 5 dégâts, il n\'est pas désarçonné. Le personnage peut également charger en premier round d\'un combat, et bénéficier d\'un bonus de +3 à son jet (+4 avec une lance)', $book);
 
         $this->manager->flush();
     }
 
-    public function fixtureObject(EntityRepository $repo, $id, $melee, $ranged, $name, $description, $created, $updated, $deleted = null, $book)
+    public function fixtureObject(EntityRepository $repo, $id, $melee, $ranged, $name, $description, $book)
     {
         $obj       = null;
         $newObject = false;
@@ -82,9 +82,6 @@ class CombatArtsFixtures extends AbstractFixture implements OrderedFixtureInterf
                 ->setRanged($ranged)
                 ->setDescription($description)
                 ->setBook($book)
-                ->setCreated($created ? new \Datetime($created) : new \Datetime())
-                ->setUpdated($updated ? new \Datetime($updated) : null)
-                ->setDeleted($deleted ? new \Datetime($deleted) : null)
             ;
             if ($id) {
                 /** @var ClassMetadata $metadata */
