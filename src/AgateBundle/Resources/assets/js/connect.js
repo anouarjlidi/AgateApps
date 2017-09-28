d57566cdd2a88487a971ad173a26ef8367fe53d9
Alex Rock Ancelet <pierstoval@gmail.com>
2017-09-28 10:29:43 +0200

    Remove some domaines from global connect

--- src/AgateBundle/Resources/assets/js/connect.js
+++ src/AgateBundle/Resources/assets/js/connect.js
@@ -5,7 +5,6 @@
 
     if (paths && paths.constructor === Array && paths.length) {
         for (var i = 0, l = paths.length; i < l; i++) {
-            console.info('Connecting via '+paths[i]);
             $.ajax({
                 type: 'post',
                 url: paths[i],
