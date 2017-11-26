776f53c3cfce1f9d505148bc3788743efe452768
Alex Rock Ancelet <pierstoval@gmail.com>
2017-11-26 20:38:24 +0100

    Create a specific action in ApiMapsController for edit mode #2111 @0.1

--- src/EsterenMaps/Controller/Api/ApiMapsController.php
+++ src/EsterenMaps/Controller/Api/ApiMapsController.php
@@ -62,4 +62,41 @@ class ApiMapsController extends AbstractController
 
         return $response;
     }
+
+    /**
+     * @Route("/maps/{id}/edit-mode", name="esterenmaps_api_map_get_editmode", requirements={"id"="\d+"}, methods={"GET"})
+     */
+    public function getEditModeAction(int $id, Request $request): JsonResponse
+    {
+        if (!$this->isGranted('ROLE_ADMIN')) {
+            // We need 404 instead of 403 to avoid dirty hacks here.
+            throw $this->createNotFoundException();
+        }
+
+        $mapApi = $this->get('esterenmaps.api.map');
+
+        $response = new JsonResponse();
+
+        $response->setLastModified($mapApi->getLastUpdateTime($id));
+
+        if ($response->isNotModified($request)) {
+            return $response;
+        }
+
+        $data = $mapApi->getMap($id, true);
+
+        $response->setData($data);
+
+        if (!$this->getParameter('kernel.debug')) {
+            $response->setCache([
+                'etag'          => sha1('map'.$id.$this->getParameter('version_code')),
+                'last_modified' => new \DateTime($mapApi->getLastUpdateTime($id)),
+                'max_age'       => 600,
+                's_maxage'      => 600,
+                'public'        => true,
+            ]);
+        }
+
+        return $response;
+    }
 }
