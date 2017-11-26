d56f71315f01e348b3530e30343ac30fd6418737
Alex Rock Ancelet <pierstoval@gmail.com>
2017-11-26 17:26:16 +0100

    Allow Materialize JS components to be re-enabled manually with a JS func

--- assets/esteren/js/global.js
+++ assets/esteren/js/global.js
@@ -1,43 +1,48 @@
 (function($, d){$(function(){
 
-    // Automatically activate button collapse "side" navigation
-    if (d.querySelector('.button-collapse')) {
-        $('.button-collapse').sideNav();
-    }
+    var enableJsComponents = function (context) {
+        // Automatically activate button collapse "side" navigation
+        if (context.querySelector('.button-collapse')) {
+            $('.button-collapse').sideNav();
+        }
 
-    // Automatically add parallax when available
-    if (d.querySelector('.parallax')) {
-        $('.parallax').parallax();
-    }
+        // Automatically add parallax when available
+        if (context.querySelector('.parallax')) {
+            $('.parallax').parallax();
+        }
 
-    // Automatically activate tooltips
-    if (d.querySelector('.tooltipped')) {
-        $('.tooltipped').tooltip();
-    }
+        // Automatically activate tooltips
+        if (context.querySelector('.tooltipped')) {
+            $('.tooltipped').tooltip();
+        }
 
-    // Automatically activate modals
-    if (d.querySelector('.modal')) {
-        $('.modal').modal();
-    }
+        // Automatically activate modals
+        if (context.querySelector('.modal')) {
+            $('.modal').modal();
+        }
 
-    // Automatically activate materialize select tags
-    if (d.querySelector('select')) {
-        $('select').material_select();
-    }
+        // Automatically activate materialize select tags
+        if (context.querySelector('select')) {
+            $('select').material_select();
+        }
 
-    // Automatically activate dropdowns if there are some
-    if (d.querySelector('.dropdown-button')) {
-        $('.dropdown-button').dropdown();
-    }
+        // Automatically activate dropdowns if there are some
+        if (context.querySelector('.dropdown-button')) {
+            $('.dropdown-button').dropdown();
+        }
+    };
 
     // Manage the "disable tags" cookie CNIL requirement
-    if (d.querySelector('button.disable_tags')) {
-        d.addEventListener('click', function(e){
+    if (context.querySelector('button.disable_tags')) {
+        context.addEventListener('click', function(e){
             if (e.target.tagName.toLowerCase() === 'button' && e.target.className.match('disable_tags')) {
-                d.cookie = "disable_tags=1";
+                context.cookie = "disable_tags=1";
                 e.target.innerHTML = 'OK';
             }
         });
     }
 
+    enableJsComponents(d);
+
+    d.parentWindow.enableJsComponents = enableJsComponents;
 })})(jQuery, document);
