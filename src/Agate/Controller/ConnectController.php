a00b13139aa6ecfd7a389a75bcdcf4495de7489b
Alex Rock Ancelet <pierstoval@gmail.com>
2018-01-18 22:17:13 +0100

    Attempt a fix of the login & connect system

--- src/Agate/Controller/ConnectController.php
+++ src/Agate/Controller/ConnectController.php
@@ -2,32 +2,20 @@
 
 namespace Agate\Controller;
 
+use Agate\EventListener\MultidomainConnectSubscriber;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 
 class ConnectController extends Controller
 {
     /**
      * @Route("/connect/{id}", name="connect", methods={"POST"})
      */
-    public function connectAction($id, Request $request)
+    public function connectAction()
     {
-        $session = $request->getSession();
-
-        if ($session && $session->getId() === $id) {
-            // Means sessions are synchronized between domains
-            return new Response();
-        }
-
-        $storage = $this->get('session.storage');
-
-        $storage->clear();
-        $storage->save();
-        $storage->setId($id);
-        $storage->start();
-
-        return new Response();
+        throw new \RuntimeException(sprintf(
+            'This controller should not be called. This means that %s listener has not been executed correctly.',
+            MultidomainConnectSubscriber::class
+        ));
     }
 }
