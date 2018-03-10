966126880c38f145e3ef1f8fff416cee9bf933fb
Alex Rock Ancelet <pierstoval@gmail.com>
2018-03-10 14:23:53 +0100

    Add small deploy script

---
+++ bin/deploy.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cd ${DIR}/../
+
+composer ins --no-dev --optimize-autoloader --classmap-authoritative --prefer-dist
+
+php bin/console cache:clear --no-warmup
+php bin/console cache:warmup
