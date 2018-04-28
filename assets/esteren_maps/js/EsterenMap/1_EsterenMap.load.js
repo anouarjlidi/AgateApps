71ca241666ad24ba6b2876e8e689e61cf984f780
Alex Rock Ancelet <pierstoval@gmail.com>
2018-04-28 21:46:15 +0200

    Remove console log in EsterenMap._load #2111 @0.05

--- assets/esteren_maps/js/EsterenMap/1_EsterenMap.load.js
+++ assets/esteren_maps/js/EsterenMap/1_EsterenMap.load.js
@@ -63,8 +63,6 @@
             data: method === 'GET' ? data : JSON.stringify(data ? data : {})
         };
 
-        console.info(data, ajaxObject);
-
         // Apply the different callbacks
         if (typeof(callback) === 'function') {
             ajaxObject.success = function(response) {
