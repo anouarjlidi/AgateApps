5b372f4b24f4d499a8745001b28c8e208b1d6d8c
Alex Rock Ancelet <pierstoval@gmail.com>
2018-05-02 22:49:24 +0200

    Start working on tests for the CacheManager #2111 @5

---
+++ tests/EsterenMaps/Cache/CacheManagerTest.php
@@ -0,0 +1,89 @@
+<?php
+
+/**
+ * This file is part of the corahn_rin package.
+ *
+ * (c) Alexandre Rock Ancelet <alex@orbitale.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Tests\EsterenMaps\Cache;
+
+use Doctrine\ORM\EntityManagerInterface;
+use EsterenMaps\DataFixtures\ORM\RoutesFixtures;
+use EsterenMaps\Entity\Routes;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpKernel\DataCollector\LoggerDataCollector;
+use Symfony\Component\VarDumper\Cloner\Data;
+use Tests\WebTestCase as PiersTestCase;
+
+
+class CacheManagerTest extends WebTestCase
+{
+    use PiersTestCase;
+
+    public function testdUpdatingRouteShouldClearMapsCache()
+    {
+        static::resetDatabase();
+
+        /**
+         * Here we only change coordinates for greater distance.
+         * @see RoutesFixtures::getSimplerCanvasRoutes for route 701 details
+         */
+        $jsonPayload = json_encode([
+            'map' => 1,
+            'name' => 'From 0,0 to 0,10',
+            'coordinates' => '[{"lat":0,"lng":0},{"lat":10,"lng":10},{"lat":0,"lng":10}]',
+            'description' => 'Test route description',
+            'routeType' => 1,
+            'markerStart' => 700,
+            'markerEnd' => 701,
+            'faction' => null,
+            'guarded' => false,
+            'forcedDistance' => null,
+        ]);
+
+        $client = $this->getClient('api.esteren.dev', [], ['ROLE_ADMIN']);
+        $client->enableProfiler();
+
+        $client->request('POST', '/fr/routes/701', [], [], [], $jsonPayload);
+
+        /** @var Routes $route */
+        $route = $client
+            ->getContainer()
+            ->get(EntityManagerInterface::class)
+            ->getRepository(Routes::class)
+            ->findOneBy(['name' => 'From 0,0 to 0,10'])
+        ;
+        static::assertNotNull($route);
+        // FIXME => Check distance is different
+
+        $profile = $client->getProfile();
+
+        /** @var LoggerDataCollector $loggerDataCollector */
+        $loggerDataCollector = $profile->getCollector('logger');
+        $loggerDataCollector->lateCollect();
+
+        /*
+         * Retrieve log item from collector.
+         * This is the way we know whether CacheManager was triggered or not.
+         */
+        $logToCheck = null;
+        foreach ($loggerDataCollector->getLogs() as $log) {
+            if ($log instanceof Data) {
+                $log = $log->getValue(true);
+            }
+            if (($log['message'] ?? '') === 'Clearing doctrine cache') {
+                if ($logToCheck) {
+                    static::fail('Seems that cache clear log was fired twice or more.');
+                }
+
+                $logToCheck = $log;
+            }
+        }
+
+        static::assertNotNull($logToCheck, 'Log was not fired, it means that cache manager was not triggered.');
+    }
+}
