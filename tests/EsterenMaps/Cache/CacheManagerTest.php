05e9dcf4448ca7ac583c55c3e421748d21bd11e2
Alex Rock Ancelet <pierstoval@gmail.com>
2018-05-03 21:13:41 +0200

    Fix CacheManagerTest, now it works exactly as expected \o/ #2111 @2.25

--- tests/EsterenMaps/Cache/CacheManagerTest.php
+++ tests/EsterenMaps/Cache/CacheManagerTest.php
@@ -23,48 +23,55 @@ class CacheManagerTest extends WebTestCase
 {
     use PiersTestCase;
 
+    /**
+     * @runInSeparateProcess
+     */
     public function testdUpdatingRouteShouldClearMapsCache()
     {
         static::resetDatabase();
 
+        static::bootKernel();
+        $em = static::$kernel->getContainer()->get(EntityManagerInterface::class);
+
         /** @see RoutesFixtures::getSimplerCanvasRoutes for route 701 details */
         $routeId = 700;
-        $coordinates = '[{"lat":"0","lng":"0"},{"lat":"10","lng":"10"},{"lat":"0","lng":"10"}]';
 
-        /**
-         * Here we only change coordinates for greater distance.
-         */
-        $jsonPayload = json_encode(
-            [
-                'map'            => 1,
-                'name'           => 'From 0,0 to 0,10',
-                'coordinates'    => $coordinates,
-                'description'    => 'Test route description',
-                'routeType'      => 1,
-                'markerStart'    => 700,
-                'markerEnd'      => 701,
-                'faction'        => null,
-                'guarded'        => false,
-                'forcedDistance' => null,
-            ]
-        );
+        $coordinates = '[{"lat":"0","lng":"0"},{"lat":"10","lng":"10"},{"lat":"5","lng":"5"},{"lat":"0","lng":"5"},{"lat":"0","lng":"10"}]';
 
         $client = $this->getClient('api.esteren.dev', [], ['ROLE_ADMIN']);
         $client->enableProfiler();
 
-        $client->request('POST', "/fr/routes/$routeId", [], [], [], $jsonPayload);
-
         /** @var Routes $route */
-        $route = $client
-            ->getContainer()
-            ->get(EntityManagerInterface::class)
-            ->find(Routes::class, $routeId);
+        $route = $em->find(Routes::class, $routeId);
         static::assertNotNull($route);
-        static::assertSame(700, $route->getId());
-        static::assertSame($coordinates, $route->getCoordinates());
-        static::assertSame(120, $route->getDistance());
+        static::assertSame('[{"lat":"0","lng":"0"},{"lat":"0","lng":"10"}]', $route->getCoordinates());
+        static::assertSame(50, $route->getDistance());
+
+        /**
+         * Here we only change coordinates for greater distance.
+         */
+        $client->request('POST', "/fr/routes/$routeId", [], [], [], json_encode(
+            [
+                'map'            => $route->getMap()->getId(),
+                'name'           => $route->getName(),
+                'coordinates'    => $coordinates,
+                'description'    => $route->getDescription(),
+                'routeType'      => $route->getRouteType()->getId(),
+                'markerStart'    => $route->getMarkerStart()->getId(),
+                'markerEnd'      => $route->getMarkerEnd()->getId(),
+                'faction'        => $route->getFaction() ? $route->getFaction()->getId() : null,
+                'guarded'        => $route->isGuarded(),
+                'forcedDistance' => $route->getForcedDistance(),
+            ]
+        ));
+        $em = $client->getContainer()->get(EntityManagerInterface::class);
 
         $profile = $client->getProfile();
+
+        $route = $em->find(Routes::class, $routeId);
+        static::assertNotNull($route);
+        static::assertSame($coordinates, $route->getCoordinates());
+        static::assertSame(156, $route->getDistance());
 
         /** @var LoggerDataCollector $loggerDataCollector */
         $loggerDataCollector = $profile->getCollector('logger');
