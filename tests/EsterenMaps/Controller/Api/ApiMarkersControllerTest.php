93f5ad4ef854c3e9e57531003267b1701af2ef81
Alex Rock Ancelet <pierstoval@gmail.com>
2018-05-30 22:45:05 +0200

    Add tests for Markers & Zones API #2111 @0.5

---
+++ tests/EsterenMaps/Controller/Api/ApiMarkersControllerTest.php
@@ -0,0 +1,102 @@
+<?php
+
+/**
+ * This file is part of the corahn_rin package.
+ *
+ * (c) Alexandre Rock Ancelet <alex@orbitale.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Tests\EsterenMaps\Controller\Api;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Tests\WebTestCase as PiersTestCase;
+
+class ApiMarkersControllerTest extends WebTestCase
+{
+    use PiersTestCase;
+
+    public function testCreateWithCorrectData()
+    {
+        static::resetDatabase();
+
+        $client = $this->getClient('api.esteren.dev', [], 'ROLE_ADMIN');
+
+        $data = [
+            'name' => 'Test name',
+            'description' => 'Test description',
+            'latitude' => 20,
+            'longitude' => 20,
+            'altitude' => 0,
+            'map' => 1,
+            'markerType' => 1,
+            'faction' => null,
+        ];
+
+        $client->request('POST','/fr/markers', [], [], [], json_encode($data));
+
+        static::assertSame(200, $client->getResponse()->getStatusCode());
+        static::assertSame('application/json', $client->getResponse()->headers->get('Content-Type'));
+
+        // Add ID For assertion
+        $data['id'] = 704;
+        $responseData = json_decode($client->getResponse()->getContent(), true);
+        static::assertSame(ksort($data), ksort($responseData));
+    }
+
+    public function testCreateWithEmptyData()
+    {
+        static::resetDatabase();
+
+        $client = $this->getClient('api.esteren.dev', [], 'ROLE_ADMIN');
+
+        $client->request('POST','/fr/markers', [], [], [], '[]');
+
+        static::assertSame(400, $client->getResponse()->getStatusCode());
+        static::assertSame('application/json', $client->getResponse()->headers->get('Content-Type'));
+
+        $expectedResponse = [
+            'name' => 'Cette valeur ne doit pas être vide.',
+            'map' => 'Cette valeur ne doit pas être vide.',
+            'markerType' => 'Cette valeur ne doit pas être vide.',
+        ];
+
+        $responseData = json_decode($client->getResponse()->getContent(), true);
+
+        static::assertSame($expectedResponse, $responseData);
+    }
+
+    public function testCreateWithIncorrectData()
+    {
+        static::resetDatabase();
+
+        $client = $this->getClient('api.esteren.dev', [], 'ROLE_ADMIN');
+
+        $dataToSend = [
+            'name' => 'Test name',
+            'description' => 'Test description',
+            'latitude' => 20,
+            'longitude' => 20,
+            'altitude' => 0,
+            'map' => 9999999999,
+            'markerType' => 9999999999,
+            'faction' => 9999999999,
+        ];
+
+        $client->request('POST','/fr/markers', [], [], [], json_encode($dataToSend));
+
+        static::assertSame(400, $client->getResponse()->getStatusCode());
+        static::assertSame('application/json', $client->getResponse()->headers->get('Content-Type'));
+
+        $responseData = json_decode($client->getResponse()->getContent(), true);
+
+        $expectedResponse = [
+            'map' => 'Cette valeur ne doit pas être vide.',
+            'markerType' => 'Cette valeur ne doit pas être vide.',
+        ];
+
+        static::assertSame($expectedResponse, $responseData);
+    }
+}
---
+++ tests/EsterenMaps/Controller/Api/ApiZonesControllerTest.php
@@ -0,0 +1,124 @@
+<?php
+
+/**
+ * This file is part of the corahn_rin package.
+ *
+ * (c) Alexandre Rock Ancelet <alex@orbitale.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Tests\EsterenMaps\Controller\Api;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Tests\WebTestCase as PiersTestCase;
+
+class ApiZonesControllerTest extends WebTestCase
+{
+    use PiersTestCase;
+
+    public function testCreateWithCorrectData()
+    {
+        static::resetDatabase();
+
+        $client = $this->getClient('api.esteren.dev', [], 'ROLE_ADMIN');
+
+        $data = [
+            'name' => 'Test name',
+            'description' => 'Test description',
+            'coordinates' => '[{"lat":"0","lng":"10"}]',
+            'map' => 1,
+            'zoneType' => 1,
+            'faction' => null,
+        ];
+
+        $client->request('POST','/fr/zones', [], [], [], json_encode($data));
+
+        static::assertSame(200, $client->getResponse()->getStatusCode());
+        static::assertSame('application/json', $client->getResponse()->headers->get('Content-Type'));
+
+        // Add ID For assertion
+        $data['id'] = 704;
+        $responseData = json_decode($client->getResponse()->getContent(), true);
+        static::assertSame(ksort($data), ksort($responseData));
+    }
+
+    public function testCreationFlattensLatLngCorrectly()
+    {
+        static::resetDatabase();
+
+        $client = $this->getClient('api.esteren.dev', [], 'ROLE_ADMIN');
+
+        $data = [
+            'name' => 'Test zone to flatten coordinates',
+            'description' => 'Description',
+            'coordinates' => '[[{"lat":"0","lng":"10"}],[{"lat":"10","lng":"10"}]]',
+            'map' => 1,
+            'zoneType' => 1,
+            'faction' => null,
+        ];
+
+        $client->request('POST','/fr/zones', [], [], [], json_encode($data));
+
+        static::assertSame(200, $client->getResponse()->getStatusCode());
+        static::assertSame('application/json', $client->getResponse()->headers->get('Content-Type'));
+
+        // Add ID For assertion
+        $data['id'] = 704;
+        $responseData = json_decode($client->getResponse()->getContent(), true);
+        static::assertSame('[{"lat":0,"lng":10},{"lat":10,"lng":10}]', $responseData['coordinates']);
+    }
+
+    public function testCreateWithEmptyData()
+    {
+        static::resetDatabase();
+
+        $client = $this->getClient('api.esteren.dev', [], 'ROLE_ADMIN');
+
+        $client->request('POST','/fr/zones', [], [], [], '[]');
+
+        static::assertSame(400, $client->getResponse()->getStatusCode());
+        static::assertSame('application/json', $client->getResponse()->headers->get('Content-Type'));
+
+        $expectedResponse = [
+            'name' => 'Cette valeur ne doit pas être vide.',
+            'map' => 'Cette valeur ne doit pas être vide.',
+            'zoneType' => 'Cette valeur ne doit pas être vide.',
+        ];
+
+        $responseData = json_decode($client->getResponse()->getContent(), true);
+
+        static::assertSame($expectedResponse, $responseData);
+    }
+
+    public function testCreateWithIncorrectData()
+    {
+        static::resetDatabase();
+
+        $client = $this->getClient('api.esteren.dev', [], 'ROLE_ADMIN');
+
+        $dataToSend = [
+            'name' => 'Test name',
+            'description' => 'Test description',
+            'coordinates' => '[{"lat":"0","lng":"10"}]',
+            'map' => 9999999999,
+            'zoneType' => 9999999999,
+            'faction' => 9999999999,
+        ];
+
+        $client->request('POST','/fr/zones', [], [], [], json_encode($dataToSend));
+
+        static::assertSame(400, $client->getResponse()->getStatusCode());
+        static::assertSame('application/json', $client->getResponse()->headers->get('Content-Type'));
+
+        $responseData = json_decode($client->getResponse()->getContent(), true);
+
+        $expectedResponse = [
+            'map' => 'Cette valeur ne doit pas être vide.',
+            'zoneType' => 'Cette valeur ne doit pas être vide.',
+        ];
+
+        static::assertSame($expectedResponse, $responseData);
+    }
+}
