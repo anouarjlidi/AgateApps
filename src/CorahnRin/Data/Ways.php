f4d8565b878f3004a1d43675d2b1d0fc70fc51f5
Alex Rock Ancelet <pierstoval@gmail.com>
2018-06-14 23:06:49 +0200

    Refactored Data objects and moved Ways entity to static values #2430 @0.5

---
+++ src/CorahnRin/Data/Ways.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * This file is part of the corahn_rin package.
+ *
+ * (c) Alexandre Rock Ancelet <alex@orbitale.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace CorahnRin\Data;
+
+final class Ways
+{
+    public const COMBATIVENESS = 'combativeness';
+    public const CREATIVITY = 'creativity';
+    public const EMPATHY = 'empathy';
+    public const REASON = 'reason';
+    public const CONVICTION = 'conviction';
+
+    public const ALL = [
+        self::COMBATIVENESS => self::COMBATIVENESS.'.description',
+        self::CREATIVITY => self::CREATIVITY.'.description',
+        self::EMPATHY => self::EMPATHY.'.description',
+        self::REASON => self::REASON.'.description',
+        self::CONVICTION => self::CONVICTION.'.description',
+    ];
+}
