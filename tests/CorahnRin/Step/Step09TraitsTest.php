bc00b503d5ded2e684ed9fb2fe239b73e687f734
Alex Rock Ancelet <pierstoval@gmail.com>
2018-07-03 23:56:02 +0200

    Fix step 9 tests and add one for redirection based on ways #2430 @0.15

--- tests/CorahnRin/Step/Step09TraitsTest.php
+++ tests/CorahnRin/Step/Step09TraitsTest.php
@@ -13,7 +13,7 @@ namespace Tests\CorahnRin\Step;
 
 class Step09TraitsTest extends AbstractStepTest
 {
-    public function testWaysDependency()
+    public function test ways dependency redirects to generator()
     {
         $client = $this->getClient();
 
@@ -23,15 +23,40 @@ class Step09TraitsTest extends AbstractStepTest
         static::assertTrue($client->getResponse()->isRedirect('/fr/character/generate'));
     }
 
-    public function testValidSetbacks()
+    public function test invalid ways redirect to step 8()
+    {
+        $client = $this->getClient();
+
+        // Inject the Ways step dependency in the session to test it's redirecting properly
+        $session = $client->getContainer()->get('session');
+        $session->set('character.corahn_rin', [
+            '08_ways' => [
+                'wrong_way_for_combativeness' => 1,
+                'wrong_way_for_creativity' => 1,
+                'wrong_way_for_empathy' => 1,
+                'wrong_way_for_reason' => 1,
+                'wrong_way_for_conviction' => 1,
+            ],
+        ]);
+        $session->save();
+
+        $result = new StepActionTestResult($client->request('GET', '/fr/character/generate/'.$this->getStepName()), $client);
+
+        static::assertSame(302, $result->getResponse()->getStatusCode());
+        static::assertTrue($result->getResponse()->isRedirect('/fr/character/generate/08_ways'));
+        static::assertSame(['Traits coming from Step 08 Ways are not correct, please check them back.'], $result->getSession()->getFlashBag()->get('error'));
+    }
+
+
+    public function test valid setbacks()
     {
         $result = $this->submitAction([
             '08_ways' => [
-                1 => 1,
-                2 => 2,
-                3 => 3,
-                4 => 4,
-                5 => 5,
+                'ways.combativeness' => 1,
+                'ways.creativity' => 2,
+                'ways.empathy' => 3,
+                'ways.reason' => 4,
+                'ways.conviction' => 5,
             ],
         ], $values = [
             'quality' => 6,
@@ -43,15 +68,15 @@ class Step09TraitsTest extends AbstractStepTest
         static::assertSame($values, $result->getSession()->get('character.corahn_rin')[$this->getStepName()]);
     }
 
-    public function testInValidSetbacks()
+    public function test in valid setbacks()
     {
         $result = $this->submitAction([
             '08_ways' => [
-                1 => 1,
-                2 => 2,
-                3 => 3,
-                4 => 4,
-                5 => 5,
+                'ways.combativeness' => 1,
+                'ways.creativity' => 2,
+                'ways.empathy' => 3,
+                'ways.reason' => 4,
+                'ways.conviction' => 5,
             ],
         ], $values = [
             'quality' => 0,
