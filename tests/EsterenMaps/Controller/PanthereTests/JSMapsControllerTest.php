919b26957c185f5a72911b3ccb4f78f87ee2bd50
Alex Rock Ancelet <pierstoval@gmail.com>
2018-07-08 02:07:01 +0200

    Make another attempt to make Panthère work. Note: doesn't work on Docker

--- tests/EsterenMaps/Controller/PanthereTests/JSMapsControllerTest.php
+++ tests/EsterenMaps/Controller/PanthereTests/JSMapsControllerTest.php
@@ -13,29 +13,44 @@ namespace Tests\EsterenMaps\Controller\PanthereTests;
 
 use Panthere\Client;
 use Panthere\PanthereTestCase;
-use PHPUnit\Framework\AssertionFailedError;
+use Panthere\ProcessManager\ChromeManager;
 use Tests\WebTestCase as PiersTestCase;
 
 class JSMapsControllerTest extends PanthereTestCase
 {
     use PiersTestCase;
 
+    protected function screenshot(Client $client, string $testMethod, string $suffix)
+    {
+        $normalizedMethod = preg_replace('^tests_', '', str_replace(['\\'], '_', $testMethod));
+
+        $fileName = __DIR__.'/../../../../build/screenshots/'.$normalizedMethod.$suffix.'.png';
+
+        $client->takeScreenshot($fileName);
+    }
+
     public function testMapIndex()
     {
         static::startWebServer(null, '127.0.0.1', 9900);
-        $client = self::$panthereClient = Client::createChromeClient('chromedriver', null, ['binary' => 'chromium'], self::$baseUri);
 
-        $client->request('GET', 'http://maps.esteren.docker:9900/fr/login');
+        $chromeManager = new ChromeManager('chromedriver', []);
+        $client = self::$panthereClient = new Client($chromeManager, self::$baseUri);
+
+        $crawler = $client->request('GET', 'http://maps.esteren.docker:9900/fr/login');
+
+        $form = $crawler->filter('#form_login')->form();
+        $form->get('_username_or_email')->setValue('Pierstoval');
+        $form->get('_password')->setValue('admin');
+
+        $client->submit($form);
+
+        $this->screenshot($client, __METHOD__, 'login_response');
 
         $crawler = $client->request('GET', 'http://maps.esteren.docker:9900/fr/map-tri-kazel');
 
-        try {
-            static::assertSame(200, $client->getInternalResponse()->getStatus());
-            static::assertCount(1, $crawler->filter('#map_wrapper'), 'Map link does not redirect to map view, or map view is broken');
-        } catch (AssertionFailedError $exception) {
-            echo '>>>>';
-            var_dump($client->getInternalResponse());
-            throw $exception;
-        }
+        $this->screenshot($client, __METHOD__, 'map_view');
+
+        static::assertSame(200, $client->getInternalResponse()->getStatus());
+        static::assertCount(1, $crawler->filter('#map_wrapper'), 'Map link does not redirect to map view, or map view is broken');
     }
 }
