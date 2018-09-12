c3c836752c176fce323f574636ccbcfd7d7d1483
Alex Rock Ancelet <pierstoval@gmail.com>
2018-09-12 02:12:57 +0200

    Now that Panther is back alive, let's make it roar!🐆

--- public/index.php
+++ public/index.php
@@ -15,7 +15,7 @@ if (!isset($_SERVER['APP_ENV'])) {
     (new Dotenv())->load(__DIR__.'/../.env');
 }
 
-$env = $_SERVER['APP_ENV'] ?? 'dev';
+$env = $_SERVER['APP_ENV'] ?? $_ENV['APP_ENV'] ?? 'dev';
 $debug = (bool) ($_SERVER['APP_DEBUG'] ?? ('prod' !== $env));
 
 if ($debug) {
